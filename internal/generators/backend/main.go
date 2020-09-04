3b16804e68ae20dbcb01ee71b0a40ced4ab36a68
Gautam Botrel <gautam.botrel@consensys.net>
2020-09-04 15:23:14 -0500

    internal/: reorganizing template files

--- internal/generators/backend/main.go
+++ internal/generators/backend/main.go
@@ -1,45 +1,180 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
-	"github.com/consensys/gnark/internal/generators/backend/template/generator"
+	"github.com/consensys/bavard"
+	"github.com/consensys/gnark/internal/generators/backend/template"
+	"github.com/consensys/gnark/internal/generators/backend/template/algorithms"
+	"github.com/consensys/gnark/internal/generators/backend/template/representations"
+	"github.com/consensys/gnark/internal/generators/backend/template/zkpschemes"
 )
 
 //go:generate go run -tags debug main.go
 func main() {
 
-	bls377 := generator.GenerateData{
+	bls377 := templateData{
 		RootPath: "../../../internal/backend/bls377/",
 		Curve:    "BLS377",
 	}
-	bls381 := generator.GenerateData{
+	bls381 := templateData{
 		RootPath: "../../../internal/backend/bls381/",
 		Curve:    "BLS381",
 	}
-	bn256 := generator.GenerateData{
+	bn256 := templateData{
 		RootPath: "../../../internal/backend/bn256/",
 		Curve:    "BN256",
 	}
 
-	bw761 := generator.GenerateData{
+	bw761 := templateData{
 		RootPath: "../../../internal/backend/bw761/",
 		Curve:    "BW761",
 	}
 
-	datas := []generator.GenerateData{bls377, bls381, bn256, bw761}
+	datas := []templateData{bls377, bls381, bn256, bw761}
 
 	for _, d := range datas {
 		if err := os.MkdirAll(d.RootPath+"groth16", 0700); err != nil {
 			panic(err)
 		}
-		if err := generator.GenerateGroth16(d); err != nil {
+		if err := generateGroth16(d); err != nil {
 			panic(err)
 		}
 		d.RootPath = "../../../backend/r1cs/"
-		if err := generator.GenerateR1CSConvertor(d); err != nil {
+		if err := generateR1CSConvertor(d); err != nil {
 			panic(err)
 		}
 	}
 
+}
+
+type templateData struct {
+	RootPath string
+	Curve    string // GENERIC, BLS381, BLS377, BN256
+}
+
+func generateR1CSConvertor(d templateData) error {
+	if !strings.HasSuffix(d.RootPath, "/") {
+		d.RootPath += "/"
+	}
+	fmt.Println()
+	fmt.Println("generating r1cs convertor for ", d.Curve)
+	fmt.Println()
+	if d.Curve == "GENERIC" {
+		return nil
+	}
+
+	// generate r1cs_curve.go
+	src := []string{
+		template.ImportCurve,
+		representations.R1CSConvertor,
+	}
+	return bavard.Generate(d.RootPath+"r1cs_"+strings.ToLower(d.Curve)+".go", src, d,
+		bavard.Package("r1cs"),
+		bavard.Apache2("ConsenSys AG", 2020),
+		bavard.GeneratedBy("gnark/internal/generators"),
+	)
+}
+
+func generateGroth16(d templateData) error {
+	if !strings.HasSuffix(d.RootPath, "/") {
+		d.RootPath += "/"
+	}
+	fmt.Println()
+	fmt.Println("generating groth16 backend for ", d.Curve)
+	fmt.Println()
+	if d.Curve == "GENERIC" {
+		return nil
+	}
+
+	// generate R1CS.go
+	src := []string{
+		template.ImportCurve,
+		representations.R1CS,
+	}
+	if err := bavard.Generate(d.RootPath+"r1cs.go", src, d,
+		bavard.Package("backend_"+strings.ToLower(d.Curve)),
+		bavard.Apache2("ConsenSys AG", 2020),
+		bavard.GeneratedBy("gnark/internal/generators"),
+	); err != nil {
+		return err
+	}
+
+	// groth16
+	{
+		// setup
+		src := []string{
+			template.ImportCurve,
+			zkpschemes.Groth16Setup,
+		}
+		if err := bavard.Generate(d.RootPath+"groth16/setup.go", src, d,
+			bavard.Package("groth16"),
+			bavard.Apache2("ConsenSys AG", 2020),
+			bavard.GeneratedBy("gnark/internal/generators"),
+		); err != nil {
+			return err
+		}
+	}
+	{
+		// prove
+		src := []string{
+			template.ImportCurve,
+			zkpschemes.Groth16Prove,
+		}
+		if err := bavard.Generate(d.RootPath+"groth16/prove.go", src, d,
+			bavard.Package("groth16"),
+			bavard.Apache2("ConsenSys AG", 2020),
+			bavard.GeneratedBy("gnark/internal/generators"),
+		); err != nil {
+			return err
+		}
+	}
+
+	{
+		// verify
+		src := []string{
+			template.ImportCurve,
+			zkpschemes.Groth16Verify,
+		}
+		if err := bavard.Generate(d.RootPath+"groth16/verify.go", src, d,
+			bavard.Package("groth16"),
+			bavard.Apache2("ConsenSys AG", 2020),
+			bavard.GeneratedBy("gnark/internal/generators"),
+		); err != nil {
+			return err
+		}
+	}
+
+	{
+		// generate FFT
+		src := []string{
+			template.ImportCurve,
+			algorithms.FFT,
+		}
+		if err := bavard.Generate(d.RootPath+"fft.go", src, d,
+			bavard.Package("backend_"+strings.ToLower(d.Curve)),
+			bavard.Apache2("ConsenSys AG", 2020),
+			bavard.GeneratedBy("gnark/internal/generators"),
+		); err != nil {
+			return err
+		}
+	}
+
+	{
+		// tests
+		src := []string{
+			template.ImportCurve,
+			zkpschemes.Groth16Tests,
+		}
+		if err := bavard.Generate(d.RootPath+"groth16/groth16_test.go", src, d,
+			bavard.Package("groth16_test"),
+			bavard.Apache2("ConsenSys AG", 2020),
+			bavard.GeneratedBy("gnark/internal/generators"),
+		); err != nil {
+			return err
+		}
+	}
+	return nil
 }
