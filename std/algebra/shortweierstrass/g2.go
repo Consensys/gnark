6a75d2593add6d4efce1627754048bd909f4195b
Thomas Piellard <thomas.piellard@consensys.net>
2020-09-23 16:34:48 +0200

    changed std/../sw --> std/../shortweierstrass. Package sw is now shortweierstrass

--- std/algebra/shortweierstrass/g2.go
+++ std/algebra/shortweierstrass/g2.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package sw
+package shortweierstrass
 
 import (
 	"github.com/consensys/gnark/frontend"
