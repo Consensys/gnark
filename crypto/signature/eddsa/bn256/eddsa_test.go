0e709f3d5553c3ee034814940a0cc5e24e399197
Thomas Piellard <thomas.piellard@consensys.net>
2021-01-13 19:59:56 +0100

    cleaned eddsa for bn256, fixed error on size of message, tests OK

--- crypto/signature/eddsa/bn256/eddsa_test.go
+++ crypto/signature/eddsa/bn256/eddsa_test.go
@@ -15,13 +15,73 @@
 package eddsa
 
 import (
+	"crypto/sha256"
 	"testing"
 
 	"github.com/consensys/gnark/crypto/hash/mimc/bn256"
 	"github.com/consensys/gurvy/bn256/fr"
 )
 
-func TestEddsa(t *testing.T) {
+func TestSerialization(t *testing.T) {
+
+	var seed [32]byte
+	s := []byte("eddsa")
+	for i, v := range s {
+		seed[i] = v
+	}
+
+	pubKey, privKey := New(seed)
+	hFunc := sha256.New()
+	signature, err := Sign([]byte("message"), privKey, hFunc)
+	if err != nil {
+		t.Fatal("unexpected error when signing")
+	}
+
+	marshalPubKey := pubKey.Marshal()
+	marshalprivKey := privKey.Marshal()
+	marshalSignature := signature.Marshal()
+
+	var unMarshalPubKey PublicKey
+	var unMarshalPrivKey PrivateKey
+	var unMarshalSignature Signature
+
+	unMarshalPubKey.Unmarshal(marshalPubKey)
+	unMarshalPrivKey.Unmarshal(marshalprivKey)
+	unMarshalSignature.Unmarshal(marshalSignature)
+
+	// public key
+	if !unMarshalPubKey.A.Equal(&pubKey.A) {
+		t.Fatal("unmarshal(marshal(pubkey)) failed")
+	}
+
+	// signature
+	if !unMarshalSignature.R.Equal(&signature.R) {
+		t.Fatal("unmarshal(marshal(signature.R)) failed")
+	}
+	for i := 0; i < frSize; i++ {
+		if unMarshalSignature.S[i] != signature.S[i] {
+			t.Fatal("unmarshal(marshal(signature.S)) failed")
+		}
+	}
+
+	// private key
+	if !privKey.pubKey.A.Equal(&unMarshalPrivKey.pubKey.A) {
+		t.Fatal("unmarshal(marshal(privKey.pubkey)) failed")
+	}
+	for i := 0; i < 32; i++ {
+		if privKey.randSrc[i] != unMarshalPrivKey.randSrc[i] {
+			t.Fatal("unmarshal(marshal(privKey.randSrc)) failed")
+		}
+	}
+	for i := 0; i < frSize; i++ {
+		if privKey.scalar[i] != unMarshalPrivKey.scalar[i] {
+			t.Fatal("unmarshal(marshal(signature.scalar)) failed")
+		}
+	}
+
+}
+
+func TestEddsaMIMC(t *testing.T) {
 
 	var seed [32]byte
 	s := []byte("eddsa")
@@ -32,17 +92,17 @@ func TestEddsa(t *testing.T) {
 	hFunc := bn256.NewMiMC("seed")
 
 	// create eddsa obj and sign a message
-	pubKey, privKey := New(seed, hFunc)
+	pubKey, privKey := New(seed)
 	var frMsg fr.Element
 	frMsg.SetString("44717650746155748460101257525078853138837311576962212923649547644148297035978")
 	msgBin := frMsg.Bytes()
-	signature, err := Sign(msgBin[:], pubKey, privKey)
+	signature, err := Sign(msgBin[:], privKey, hFunc)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// verifies correct msg
-	res, err := Verify(signature, msgBin[:], pubKey)
+	res, err := Verify(signature, msgBin[:], pubKey, hFunc)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -53,7 +113,44 @@ func TestEddsa(t *testing.T) {
 	// verifies wrong msg
 	frMsg.SetString("44717650746155748460101257525078853138837311576962212923649547644148297035979")
 	msgBin = frMsg.Bytes()
-	res, err = Verify(signature, msgBin[:], pubKey)
+	res, err = Verify(signature, msgBin[:], pubKey, hFunc)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res {
+		t.Fatal("Verfiy wrong signature should be false")
+	}
+
+}
+
+func TestEddsaSHA256(t *testing.T) {
+
+	var seed [32]byte
+	s := []byte("eddsa")
+	for i, v := range s {
+		seed[i] = v
+	}
+
+	hFunc := sha256.New()
+
+	// create eddsa obj and sign a message
+	pubKey, privKey := New(seed)
+	signature, err := Sign([]byte("message"), privKey, hFunc)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// verifies correct msg
+	res, err := Verify(signature, []byte("message"), pubKey, hFunc)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !res {
+		t.Fatal("Verifiy correct signature should return true")
+	}
+
+	// verifies wrong msg
+	res, err = Verify(signature, []byte("wrong_message"), pubKey, hFunc)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -76,14 +173,14 @@ func BenchmarkVerify(b *testing.B) {
 	hFunc := bn256.NewMiMC("seed")
 
 	// create eddsa obj and sign a message
-	pubKey, privKey := New(seed, hFunc)
+	pubKey, privKey := New(seed)
 	var frMsg fr.Element
 	frMsg.SetString("44717650746155748460101257525078853138837311576962212923649547644148297035978")
 	msgBin := frMsg.Bytes()
-	signature, _ := Sign(msgBin[:], pubKey, privKey)
+	signature, _ := Sign(msgBin[:], privKey, hFunc)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		Verify(signature, msgBin[:], pubKey)
+		Verify(signature, msgBin[:], pubKey, hFunc)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,11 @@ module github.com/consensys/gnark
 go 1.15
 
 require (
-	github.com/consensys/bavard v0.1.7
-	github.com/consensys/gurvy v0.3.8-0.20210105143504-812ca66b4e5e
+	github.com/consensys/bavard v0.1.8-0.20210105233146-c16790d2aa8b
+	github.com/consensys/gurvy v0.3.8-0.20210112212430-d33a4b57f545
 	github.com/fxamacker/cbor/v2 v2.2.0
 	github.com/leanovate/gopter v0.2.9
 	github.com/stretchr/testify v1.6.1
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
-	golang.org/x/sys v0.0.0-20210104204734-6f8348627aad // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
