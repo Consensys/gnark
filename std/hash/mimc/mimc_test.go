ae096689db95cbcb2f43937157d0c6a22d4434b0
Thomas Piellard <thomas.piellard@consensys.net>
2021-01-15 15:20:38 +0100

    gathered mimc hash functions constructors in a single file to simplify imports, simplified std/../mimc_test.go

--- std/hash/mimc/mimc_test.go
+++ std/hash/mimc/mimc_test.go
@@ -17,22 +17,13 @@ limitations under the License.
 package mimc
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/consensys/gnark/backend/groth16"
-
+	"github.com/consensys/gnark/crypto/hash"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gurvy"
-
-	mimcbls377 "github.com/consensys/gnark/crypto/hash/mimc/bls377"
-	mimcbls381 "github.com/consensys/gnark/crypto/hash/mimc/bls381"
-	mimcbn256 "github.com/consensys/gnark/crypto/hash/mimc/bn256"
-	mimcbw761 "github.com/consensys/gnark/crypto/hash/mimc/bw761"
-
-	fr_bls377 "github.com/consensys/gurvy/bls377/fr"
-	fr_bls381 "github.com/consensys/gurvy/bls381/fr"
-	fr_bn256 "github.com/consensys/gurvy/bn256/fr"
-	fr_bw761 "github.com/consensys/gurvy/bw761/fr"
 )
 
 type mimcCircuit struct {
@@ -50,121 +41,40 @@ func (circuit *mimcCircuit) Define(curveID gurvy.ID, cs *frontend.ConstraintSyst
 	return nil
 }
 
-func TestMimcBN256(t *testing.T) {
+func TestMimcAll(t *testing.T) {
 	assert := groth16.NewAssert(t)
 
 	// input
-	var data fr_bn256.Element
-	data.SetString("7808462342289447506325013279997289618334122576263655295146895675168642919487")
+	var data big.Int
+	data.SetString("7808462342289447506325013279997289618334122576263655295146895675168642919487", 10)
 
-	// minimal cs res = hash(data)
-	var circuit, witness mimcCircuit
-	r1cs, err := frontend.Compile(gurvy.BN256, &circuit)
-	if err != nil {
-		t.Fatal(err)
+	curves := map[gurvy.ID]hash.Hash{
+		gurvy.BN256:  hash.MIMC_BN256,
+		gurvy.BLS381: hash.MIMC_BLS381,
+		gurvy.BLS377: hash.MIMC_BLS377,
+		gurvy.BW761:  hash.MIMC_BW761,
 	}
 
-	// running MiMC (Go)
-	dataBytes := data.Bytes()
-	b, err := mimcbn256.Sum("seed", dataBytes[:])
-	if err != nil {
-		t.Fatal(err)
+	for curve, hashFunc := range curves {
+
+		// minimal cs res = hash(data)
+		var circuit, witness mimcCircuit
+		r1cs, err := frontend.Compile(curve, &circuit)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// running MiMC (Go)
+		goMimc := hashFunc.New("seed")
+		goMimc.Write(data.Bytes())
+		b := goMimc.Sum(nil)
+
+		// prepare witness
+		witness.Data.Assign(data)
+		witness.ExpectedResult.Assign(b)
+
+		// Check correctness
+		assert.SolvingSucceeded(r1cs, &witness)
 	}
-	var tmp fr_bn256.Element
-	tmp.SetBytes(b)
-	witness.Data.Assign(data)
-	witness.ExpectedResult.Assign(tmp)
-
-	// creates r1cs
-	assert.SolvingSucceeded(r1cs, &witness)
-}
-
-func TestMimcBLS381(t *testing.T) {
-
-	assert := groth16.NewAssert(t)
-
-	// input
-	var data fr_bls381.Element
-	data.SetString("7808462342289447506325013279997289618334122576263655295146895675168642919487")
-
-	// minimal cs res = hash(data)
-	var circuit, witness mimcCircuit
-	r1cs, err := frontend.Compile(gurvy.BLS381, &circuit)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// running MiMC (Go)
-	dataBytes := data.Bytes()
-	b, err := mimcbls381.Sum("seed", dataBytes[:])
-	if err != nil {
-		t.Fatal(err)
-	}
-	var tmp fr_bls381.Element
-	tmp.SetBytes(b)
-	witness.Data.Assign(data)
-	witness.ExpectedResult.Assign(tmp)
-
-	assert.SolvingSucceeded(r1cs, &witness)
-
-}
-
-func TestMimcBLS377(t *testing.T) {
-
-	assert := groth16.NewAssert(t)
-
-	// input
-	var data fr_bls377.Element
-	data.SetString("7808462342289447506325013279997289618334122576263655295146895675168642919487")
-
-	// minimal cs res = hash(data)
-	var circuit, witness mimcCircuit
-	r1cs, err := frontend.Compile(gurvy.BLS377, &circuit)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// running MiMC (Go)
-	dataBytes := data.Bytes()
-	b, err := mimcbls377.Sum("seed", dataBytes[:])
-	if err != nil {
-		t.Fatal(err)
-	}
-	var tmp fr_bls377.Element
-	tmp.SetBytes(b)
-	witness.Data.Assign(data)
-	witness.ExpectedResult.Assign(tmp)
-
-	assert.SolvingSucceeded(r1cs, &witness)
-
-}
-
-func TestMimcBW761(t *testing.T) {
-
-	assert := groth16.NewAssert(t)
-
-	// input
-	var data fr_bw761.Element
-	data.SetString("7808462342289447506325013279997289618334122576263655295146895675168642919487")
-
-	// minimal cs res = hash(data)
-	var circuit, witness mimcCircuit
-	r1cs, err := frontend.Compile(gurvy.BW761, &circuit)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// running MiMC (Go)
-	dataBytes := data.Bytes()
-	b, err := mimcbw761.Sum("seed", dataBytes[:])
-	if err != nil {
-		t.Fatal(err)
-	}
-	var tmp fr_bw761.Element
-	tmp.SetBytes(b)
-	witness.Data.Assign(data)
-	witness.ExpectedResult.Assign(tmp)
-
-	assert.SolvingSucceeded(r1cs, &witness)
 
 }
