a0c27b2bdf5be42aa100eed65f75abbe42308233
Gautam Botrel <gautam.botrel@gmail.com>
2021-03-30 18:49:21 -0500

    fix: bn256 -> bn254

--- gnarkd/circuits/generate.go
+++ gnarkd/circuits/generate.go
@@ -6,7 +6,7 @@ import (
 	"github.com/consensys/gnark/backend"
 	"github.com/consensys/gnark/backend/groth16"
 	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/gnark/gnarkd/circuits/bn256/cubic"
+	"github.com/consensys/gnark/gnarkd/circuits/bn254/cubic"
 	"github.com/consensys/gurvy/ecc"
 )
 
@@ -16,19 +16,19 @@ func main() {
 	r1cs, _ := frontend.Compile(ecc.BN254, backend.GROTH16, &circuit)
 
 	{
-		f, _ := os.Create("bn256/cubic/cubic.r1cs")
+		f, _ := os.Create("bn254/cubic/cubic.r1cs")
 		r1cs.WriteTo(f)
 		f.Close()
 	}
 
 	pk, vk, _ := groth16.Setup(r1cs)
 	{
-		f, _ := os.Create("bn256/cubic/cubic.pk")
+		f, _ := os.Create("bn254/cubic/cubic.pk")
 		pk.WriteTo(f)
 		f.Close()
 	}
 	{
-		f, _ := os.Create("bn256/cubic/cubic.vk")
+		f, _ := os.Create("bn254/cubic/cubic.vk")
 		vk.WriteTo(f)
 		f.Close()
 	}
