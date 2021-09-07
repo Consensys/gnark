bc8ad3a38b7b16f4ba34a1ea5838129e203a2188
Gautam Botrel <gautam.botrel@gmail.com>
2021-09-07 13:51:12 -0500

    test: added test from #136

--- frontend/cs_api_test.go
+++ frontend/cs_api_test.go
@@ -740,3 +740,66 @@ func TestPrintln(t *testing.T) {
 
 	cs.Println(nil, 1, "a", new(big.Int), one)
 }
+
+// empty circuits
+type IsBool1 struct{}
+type IsBool2 struct{}
+type IsBool3 struct{}
+
+func TestIsBool1(t *testing.T) {
+
+	var circuit IsBool1
+
+	_, err := Compile(ecc.BN254, backend.GROTH16, &circuit)
+	if err != nil {
+		t.Fatal("compilation failed")
+	}
+}
+
+func TestIsBool2(t *testing.T) {
+
+	var circuit IsBool2
+
+	_, err := Compile(ecc.BN254, backend.GROTH16, &circuit)
+	if err != nil {
+		t.Fatal("compilation failed")
+	}
+}
+
+func TestIsBool3(t *testing.T) {
+
+	var circuit IsBool3
+
+	_, err := Compile(ecc.BN254, backend.GROTH16, &circuit)
+	if err != nil {
+		t.Fatal("compilation failed")
+	}
+}
+
+func (c *IsBool1) Define(curve ecc.ID, cs *ConstraintSystem) error {
+
+	zero := cs.Constant(0)
+	one := cs.Constant(1)
+	cs.AssertIsBoolean(zero)
+	cs.AssertIsBoolean(one)
+	return nil
+}
+
+func (c *IsBool2) Define(curve ecc.ID, cs *ConstraintSystem) error {
+
+	zero := cs.Constant(0)
+	one := cs.Constant(1)
+	sum := cs.Add(zero, one)
+	cs.AssertIsBoolean(sum)
+	return nil
+}
+
+func (c *IsBool3) Define(curve ecc.ID, cs *ConstraintSystem) error {
+
+	zero := cs.Constant(0)
+	one := cs.Constant(1)
+	prod := cs.Mul(zero, one)
+	cs.AssertIsBoolean(prod)
+
+	return nil
+}
