3398c6c90faefa811a6ae530b97cd3ddf75aac0a
Thomas Piellard <thomas.piellard@consensys.net>
2021-11-29 23:51:20 +0100

    fix: fixed Div

--- debug_test.go
+++ debug_test.go
@@ -91,7 +91,7 @@ func TestTraceDivBy0(t *testing.T) {
 	{
 		_, err := getPlonkTrace(&circuit, &witness)
 		assert.Error(err)
-		assert.Contains(err.Error(), "constraint is not satisfied: [div] 2/(-2 + 2) == <unsolved>")
+		assert.Contains(err.Error(), "constraint is not satisfied: [div] 2/0 == <unsolved>")
 		assert.Contains(err.Error(), "(*divBy0Trace).Define")
 		assert.Contains(err.Error(), "debug_test.go:")
 	}
@@ -128,7 +128,7 @@ func TestTraceNotEqual(t *testing.T) {
 	{
 		_, err := getPlonkTrace(&circuit, &witness)
 		assert.Error(err)
-		assert.Contains(err.Error(), "constraint is not satisfied: [assertIsEqual] 1 == (24 + 42)")
+		assert.Contains(err.Error(), "constraint is not satisfied: [assertIsEqual] 1 == 66")
 		assert.Contains(err.Error(), "(*notEqualTrace).Define")
 		assert.Contains(err.Error(), "debug_test.go:")
 	}
@@ -165,7 +165,7 @@ func TestTraceNotBoolean(t *testing.T) {
 	{
 		_, err := getPlonkTrace(&circuit, &witness)
 		assert.Error(err)
-		assert.Contains(err.Error(), "constraint is not satisfied: [assertIsBoolean] (24 + 42) == (0|1)")
+		assert.Contains(err.Error(), "constraint is not satisfied: [assertIsBoolean] 66 == (0|1)")
 		assert.Contains(err.Error(), "(*notBooleanTrace).Define")
 		assert.Contains(err.Error(), "debug_test.go:")
 	}
