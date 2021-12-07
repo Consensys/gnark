1b7b482eadd48c6ba44c649135509bf1600e09c4
Ivo Kubjas <ivo.kubjas@consensys.net>
2021-12-07 13:18:47 +0100

    refactor(lookup2): use multiple witnesses for tests

--- internal/backend/circuits/lookup2.go
+++ internal/backend/circuits/lookup2.go
@@ -1,8 +1,6 @@
 package circuits
 
 import (
-	"fmt"
-
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/frontend"
 )
@@ -21,21 +19,16 @@ func (c *lookup2Circuit) Define(api frontend.API) error {
 
 func init() {
 	v0, v1, v2, v3 := 0, 1, 2, 3
+	good := []frontend.Circuit{}
+	bad := []frontend.Circuit{}
 	for _, tc := range []struct {
 		b0, b1     int
 		expected   int
 		unexpected int
-	}{
-		{0, 0, 0, 1},
-		{1, 0, 1, 0},
-		{0, 1, 2, 0},
-		{1, 1, 3, 0}} {
-		addEntry(
-			fmt.Sprintf("lookup2-%d%d", tc.b0, tc.b1),
-			&lookup2Circuit{},
-			&lookup2Circuit{v0, v1, v2, v3, tc.b0, tc.b1, tc.expected},
-			&lookup2Circuit{v0, v1, v2, v3, tc.b0, tc.b1, tc.unexpected},
-			ecc.Implemented(),
-		)
+	}{{0, 0, 0, 1}, {1, 0, 1, 0}, {0, 1, 2, 0}, {1, 1, 3, 0}} {
+		good = append(good, &lookup2Circuit{v0, v1, v2, v3, tc.b0, tc.b1, tc.expected})
+		bad = append(bad, &lookup2Circuit{v0, v1, v2, v3, tc.b0, tc.b1, tc.unexpected})
 	}
+
+	addNewEntry("lookup2", &lookup2Circuit{}, good, bad, ecc.Implemented())
 }
