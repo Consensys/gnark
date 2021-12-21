f348cf75080833633d6298be7de9eb03c863ca56
Ivo Kubjas <ivo.kubjas@consensys.net>
2021-12-21 17:18:36 +0100

    feat(internal/backend): explicit type marshalling for hints

--- internal/backend/compiled/cs.go
+++ internal/backend/compiled/cs.go
@@ -3,10 +3,13 @@ package compiled
 import (
 	"fmt"
 	"io"
+	"math/big"
+	"reflect"
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend"
 	"github.com/consensys/gnark/backend/hint"
+	"github.com/fxamacker/cbor/v2"
 )
 
 // CS contains common element between R1CS and CS
@@ -52,6 +55,124 @@ const (
 type Hint struct {
 	ID     hint.ID       // hint function id
 	Inputs []interface{} // terms to inject in the hint function
+}
+
+func (h Hint) inputsCBORTags() (cbor.TagSet, error) {
+	defTagOpts := cbor.TagOptions{EncTag: cbor.EncTagRequired, DecTag: cbor.DecTagRequired}
+	tags := cbor.NewTagSet()
+	if err := tags.Add(defTagOpts, reflect.TypeOf(LinearExpression{}), 25443); err != nil {
+		return nil, fmt.Errorf("new LE tag: %w", err)
+	}
+	if err := tags.Add(defTagOpts, reflect.TypeOf(Variable{}), 25444); err != nil {
+		return nil, fmt.Errorf("new variable tag: %w", err)
+	}
+	if err := tags.Add(defTagOpts, reflect.TypeOf(Term(0)), 25445); err != nil {
+		return nil, fmt.Errorf("new term tag: %w", err)
+	}
+	return tags, nil
+}
+
+func (h Hint) MarshalCBOR() ([]byte, error) {
+	tags, err := h.inputsCBORTags()
+	if err != nil {
+		return nil, fmt.Errorf("cbor tags: %w", err)
+	}
+	enc, err := cbor.CoreDetEncOptions().EncModeWithTags(tags)
+	if err != nil {
+		return nil, err
+	}
+	// v of type vt is Hint but does not implement cbor.Marshaler
+	type vt Hint
+	inputs := make([]interface{}, len(h.Inputs))
+	// map big.Int to bytes
+	for i := range h.Inputs {
+		switch vit := h.Inputs[i].(type) {
+		case big.Int:
+			b := vit.Bytes()
+			bb, err := enc.Marshal(b)
+			if err != nil {
+				return nil, fmt.Errorf("marshal big int bytes: %w", err)
+			}
+			inputs[i] = cbor.RawTag{Number: 25446, Content: cbor.RawMessage(bb)}
+		case *big.Int:
+			b := vit.Bytes()
+			bb, err := enc.Marshal(b)
+			if err != nil {
+				return nil, fmt.Errorf("marshal big int bytes: %w", err)
+			}
+			inputs[i] = cbor.RawTag{Number: 25447, Content: cbor.RawMessage(bb)}
+		default:
+			_ = vit
+			// handled by tagset
+			inputs[i] = h.Inputs[i]
+		}
+	}
+	v := vt{ID: h.ID, Inputs: inputs}
+	return enc.Marshal(v)
+}
+
+func (h *Hint) UnmarshalCBOR(b []byte) error {
+	tags, err := h.inputsCBORTags()
+	if err != nil {
+		return fmt.Errorf("cbor tags: %w", err)
+	}
+	dec, err := cbor.DecOptions{}.DecModeWithTags(tags)
+	if err != nil {
+		return fmt.Errorf("decoder: %w", err)
+	}
+	// v of type vt is Hint but does not implement cbor.Marshaler
+	type vt struct {
+		ID     hint.ID
+		Inputs []cbor.RawTag
+	}
+	var v vt
+	if err := dec.Unmarshal(b, &v); err != nil {
+		return fmt.Errorf("unmarshal: %w", err)
+	}
+	inputs := make([]interface{}, len(v.Inputs))
+	for i, vin := range v.Inputs {
+		switch vin.Number {
+		case 25443:
+			var v []Term
+			if err := dec.Unmarshal(vin.Content, &v); err != nil {
+				return fmt.Errorf("unmarshal linear expression: %w", err)
+			}
+			inputs[i] = LinearExpression(v)
+		case 25444:
+			var v Variable
+			if err := dec.Unmarshal(vin.Content, &v); err != nil {
+				return fmt.Errorf("unmarshal variable: %w", err)
+			}
+			inputs[i] = v
+		case 25445:
+			var v Term
+			if err := dec.Unmarshal(vin.Content, &v); err != nil {
+				return fmt.Errorf("unmarshal term: %w", err)
+			}
+			inputs[i] = v
+		case 25446:
+			v := new(big.Int)
+			var bb []byte
+			if err := dec.Unmarshal(vin.Content, &bb); err != nil {
+				return fmt.Errorf("unmarshal big int bytes: %w", err)
+			}
+			v.SetBytes(bb)
+			inputs[i] = *v
+		case 25447:
+			v := new(big.Int)
+			var bb []byte
+			if err := dec.Unmarshal(vin.Content, &bb); err != nil {
+				return fmt.Errorf("unmarshal big int bytes: %w", err)
+			}
+			v.SetBytes(bb)
+			inputs[i] = v
+		default:
+			return fmt.Errorf("unknown tag %d", vin.Number)
+		}
+	}
+	h.ID = v.ID
+	h.Inputs = inputs
+	return nil
 }
 
 // GetNbVariables return number of internal, secret and public variables
