b688c2710c967dc453d677c13ecbeca471a8d998
Ivo Kubjas <ivo.kubjas@consensys.net>
2022-01-03 17:39:38 +0100

    feat(integration_test): add variable-input/output hint test

--- internal/backend/circuits/hint.go
+++ internal/backend/circuits/hint.go
@@ -33,23 +33,69 @@ func (circuit *hintCircuit) Define(api frontend.API) error {
 	return nil
 }
 
+type vectorDoubleCircuit struct {
+	A []frontend.Variable
+	B []frontend.Variable
+}
+
+func (c *vectorDoubleCircuit) Define(api frontend.API) error {
+	res, err := api.NewHint(dvHint, c.A...)
+	if err != nil {
+		return fmt.Errorf("double newhint: %w", err)
+	}
+	if len(res) != len(c.B) {
+		return fmt.Errorf("expected len %d, got %d", len(c.B), len(res))
+	}
+	for i := range res {
+		api.AssertIsEqual(api.Mul(2, c.A[i]), c.B[i])
+		api.AssertIsEqual(res[i], c.B[i])
+	}
+	return nil
+}
+
 func init() {
+	{
+		good := []frontend.Circuit{
+			&hintCircuit{
+				A: 42,
+				B: 42 * 7,
+			},
+		}
 
-	good := []frontend.Circuit{
-		&hintCircuit{
-			A: (42),
-			B: (42 * 7),
-		},
+		bad := []frontend.Circuit{
+			&hintCircuit{
+				A: 42,
+				B: 42,
+			},
+		}
+
+		addNewEntry("hint", &hintCircuit{}, good, bad, ecc.Implemented(), mulBy7, make3)
 	}
 
-	bad := []frontend.Circuit{
-		&hintCircuit{
-			A: (42),
-			B: (42),
-		},
-	}
+	{
+		good := []frontend.Circuit{
+			&vectorDoubleCircuit{
+				A: []frontend.Variable{
+					1, 2, 3, 4, 5, 6, 7, 8,
+				},
+				B: []frontend.Variable{
+					2, 4, 6, 8, 10, 12, 14, 16,
+				},
+			},
+		}
 
-	addNewEntry("hint", &hintCircuit{}, good, bad, ecc.Implemented(), mulBy7, make3)
+		bad := []frontend.Circuit{
+			&vectorDoubleCircuit{
+				A: []frontend.Variable{
+					1, 2, 3, 4, 5, 6, 7, 8,
+				},
+				B: []frontend.Variable{
+					1, 2, 3, 4, 5, 6, 7, 8,
+				},
+			},
+		}
+		addNewEntry("multi-output-hint", &vectorDoubleCircuit{A: make([]frontend.Variable, 8), B: make([]frontend.Variable, 8)}, good, bad, ecc.Implemented(), dvHint)
+	}
 }
 
 var mulBy7 = hint.NewStaticHint(func(curveID ecc.ID, inputs []*big.Int, result []*big.Int) error {
@@ -61,3 +107,31 @@ var make3 = hint.NewStaticHint(func(curveID ecc.ID, inputs []*big.Int, result []
 	result[0].SetUint64(3)
 	return nil
 }, 0, 1)
+
+var dvHint = &doubleVector{}
+
+type doubleVector struct{}
+
+func (dv *doubleVector) UUID() hint.ID {
+	return hint.UUID(dv.Call)
+}
+
+func (dv *doubleVector) Call(curveID ecc.ID, inputs []*big.Int, res []*big.Int) error {
+	two := big.NewInt(2)
+	for i := range inputs {
+		res[i].Mul(two, inputs[i])
+	}
+	return nil
+}
+
+func (dv *doubleVector) TotalInputs(curveID ecc.ID) (nInputs int) {
+	return -1
+}
+
+func (dv *doubleVector) TotalOutputs(curveID ecc.ID, nInputs int) (nOutputs int) {
+	return nInputs
+}
+
+func (dv *doubleVector) String() string {
+	return "double"
+}
