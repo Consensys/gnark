af8ae923465c317f4461022bcb22b6e7d1a62c4e
Youssef El Housni <youssef.housni21@gmail.com>
2022-04-14 15:44:33 +0200

    feat(std): KZG verifier gadget bls24-315 (with static witness)

--- std/kzg_bls12377/verifier_test.go
+++ std/kzg_bls12377/verifier_test.go
@@ -17,13 +17,11 @@ limitations under the License.
 package kzg_bls12377
 
 import (
-	"math/big"
 	"testing"
 
 	"github.com/consensys/gnark-crypto/ecc"
-	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr"
-	kzg_bls12377 "github.com/consensys/gnark-crypto/ecc/bls12-377/fr/kzg"
 	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
 	"github.com/consensys/gnark/test"
 )
 
@@ -31,7 +29,7 @@ type verifierCircuit struct {
 	VerifKey VK
 	Proof    OpeningProof
 	Com      Digest
-	S        Point
+	S        frontend.Variable
 }
 
 func (circuit *verifierCircuit) Define(api frontend.API) error {
@@ -46,36 +44,25 @@ func TestVerifier(t *testing.T) {
 
 	var circuit, witness verifierCircuit
 
-	// create a polynomial
-	f := randomPolynomial(60)
+	// static witness
+	witness.Com.X = "145429059828629443506099208441019164249918805265766585069511130101715300037889375544644493566733059056337445574142"
+	witness.Com.Y = "7748648670212409231552941907406345586179813940682493172078407968203200311849395869785335293628955566021478572791"
 
-	// commit the polynomial
-	digest, err := kzg_bls12377.Commit(f, testSRS)
-	if err != nil {
-		t.Fatal(err)
-	}
+	witness.Proof.H.X = "142546216630759857020142552653688574597188212934274836451979072858880695115513802425442488457664742720974070355453"
+	witness.Proof.H.Y = "51742728231756961100409716107519203689800988928890924645730616869717553365749083029986151526811552917856555146906"
 
-	// compute opening proof at a random point
-	var point fr.Element
-	point.SetString("4321")
-	proof, err := kzg_bls12377.Open(f, point, testSRS)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// verify the claimed valued
-	expected := eval(f, point)
-	if !proof.ClaimedValue.Equal(&expected) {
-		t.Fatal("inconsistant claimed value")
-	}
-
-	witness.Proof.H.Assign(&proof.H)
-	witness.Proof.ClaimedValue = proof.ClaimedValue
-	witness.Com.Assign(&digest)
-	witness.S = point
-	witness.VerifKey.G1.Assign(&testSRS.G1[0])
-	witness.VerifKey.G2[0].Assign(&testSRS.G2[0])
-	witness.VerifKey.G2[1].Assign(&testSRS.G2[1])
+	witness.Proof.ClaimedValue = "7211341386127354417397285211336133449231039596179023429378585109196698597268"
+	witness.S = "4321"
+	witness.VerifKey.G1.X = "81937999373150964239938255573465948239988671502647976594219695644855304257327692006745978603320413799295628339695"
+	witness.VerifKey.G1.Y = "241266749859715473739788878240585681733927191168601896383759122102112907357779751001206799952863815012735208165030"
+	witness.VerifKey.G2[0].X.A0 = "233578398248691099356572568220835526895379068987715365179118596935057653620464273615301663571204657964920925606294"
+	witness.VerifKey.G2[0].X.A1 = "140913150380207355837477652521042157274541796891053068589147167627541651775299824604154852141315666357241556069118"
+	witness.VerifKey.G2[0].Y.A0 = "63160294768292073209381361943935198908131692476676907196754037919244929611450776219210369229519898517858833747423"
+	witness.VerifKey.G2[0].Y.A1 = "149157405641012693445398062341192467754805999074082136895788947234480009303640899064710353187729182149407503257491"
+	witness.VerifKey.G2[1].X.A0 = "123747009012703414871739433259892117784672459657097139998749475279099125411579029748101735145753812822027512995199"
+	witness.VerifKey.G2[1].X.A1 = "62735868045337090199933301723513128455431585854943778977190757050206710789139082141526891028732261537358701287808"
+	witness.VerifKey.G2[1].Y.A0 = "212548833831227473592895134150456464278558858278752454560645447355770538424096804613692943525553353783189853308160"
+	witness.VerifKey.G2[1].Y.A1 = "123051654588413991319606911619099872563646143639520520553172600449178549047186983142138529976243874838154671706124"
 
 	// cs values
 	assert := test.NewAssert(t)
@@ -83,32 +70,14 @@ func TestVerifier(t *testing.T) {
 
 }
 
-// utils
-// testSRS re-used accross tests of the KZG scheme
-var testSRS *kzg_bls12377.SRS
+// bench
+var ccsBench frontend.CompiledConstraintSystem
 
-func init() {
-	const srsSize = 230
-	testSRS, _ = kzg_bls12377.NewSRS(ecc.NextPowerOfTwo(srsSize), new(big.Int).SetInt64(42))
-}
-
-// samples a random polynomial
-func randomPolynomial(size int) []fr.Element {
-	f := make([]fr.Element, size)
-	for i := 0; i < size; i++ {
-		f[i].SetRandom()
+func BenchmarkVerifyKZG(b *testing.B) {
+	var c verifierCircuit
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ccsBench, _ = frontend.Compile(ecc.BW6_761, r1cs.NewBuilder, &c)
 	}
-	return f
-}
-
-// eval returns p(point) where p is interpreted as a polynomial
-// ∑_{i<len(p)}p[i]Xⁱ
-func eval(p []fr.Element, point fr.Element) fr.Element {
-	var res fr.Element
-	n := len(p)
-	res.Set(&p[n-1])
-	for i := n - 2; i >= 0; i-- {
-		res.Mul(&res, &point).Add(&res, &p[i])
-	}
-	return res
+	b.Log("groth16", ccsBench.GetNbConstraints())
 }
--- std/kzg_bls24315/verifier.go
+++ std/kzg_bls24315/verifier.go
@@ -14,28 +14,28 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package kzg_bls12377 provides a ZKP-circuit function to verify BLS12_377 KZG inside a BW6_761 circuit.
-package kzg_bls12377
+// Package kzg_bls24315 provides a ZKP-circuit function to verify BLS24_315 KZG inside a BW6_633 circuit.
+package kzg_bls24315
 
 import (
 	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/gnark/std/algebra/fields_bls12377"
-	"github.com/consensys/gnark/std/algebra/sw_bls12377"
+	"github.com/consensys/gnark/std/algebra/fields_bls24315"
+	"github.com/consensys/gnark/std/algebra/sw_bls24315"
 )
 
 // Digest commitment of a polynomial.
-type Digest = sw_bls12377.G1Affine
+type Digest = sw_bls24315.G1Affine
 
 // VK verification key (G2 part of SRS)
 type VK struct {
-	G1 sw_bls12377.G1Affine    // G₁
-	G2 [2]sw_bls12377.G2Affine // [G₂, [α]G₂]
+	G1 sw_bls24315.G1Affine    // G₁
+	G2 [2]sw_bls24315.G2Affine // [G₂, [α]G₂]
 }
 
 // OpeningProof KZG proof for opening at a single point.
 type OpeningProof struct {
 	// H quotient polynomial (f - f(z))/(x-z)
-	H sw_bls12377.G1Affine
+	H sw_bls24315.G1Affine
 
 	// ClaimedValue purported value
 	ClaimedValue frontend.Variable
@@ -45,32 +45,32 @@ type OpeningProof struct {
 func Verify(api frontend.API, commitment Digest, proof OpeningProof, point frontend.Variable, srs VK) {
 
 	// [f(a)]G₁
-	var claimedValueG1Aff sw_bls12377.G1Affine
+	var claimedValueG1Aff sw_bls24315.G1Affine
 	claimedValueG1Aff.ScalarMul(api, srs.G1, proof.ClaimedValue)
 
 	// [f(α) - f(a)]G₁
-	var fminusfaG1 sw_bls12377.G1Affine
+	var fminusfaG1 sw_bls24315.G1Affine
 	fminusfaG1.Neg(api, claimedValueG1Aff)
 	fminusfaG1.AddAssign(api, commitment)
 
 	// [-H(α)]G₁
-	var negH sw_bls12377.G1Affine
+	var negH sw_bls24315.G1Affine
 	negH.Neg(api, proof.H)
 
 	// [α-a]G₂
-	var alphaMinusaG2 sw_bls12377.G2Affine
+	var alphaMinusaG2 sw_bls24315.G2Affine
 	alphaMinusaG2.ScalarMul(api, srs.G2[0], point).
 		Neg(api, alphaMinusaG2).
 		AddAssign(api, srs.G2[1])
 
 	// e([f(α) - f(a)]G₁, G₂).e([-H(α)]G₁, [α-a]G₂) ==? 1
-	resPairing, _ := sw_bls12377.Pair(
+	resPairing, _ := sw_bls24315.Pair(
 		api,
-		[]sw_bls12377.G1Affine{fminusfaG1, negH},
-		[]sw_bls12377.G2Affine{srs.G2[0], alphaMinusaG2},
+		[]sw_bls24315.G1Affine{fminusfaG1, negH},
+		[]sw_bls24315.G2Affine{srs.G2[0], alphaMinusaG2},
 	)
 
-	var one fields_bls12377.E12
+	var one fields_bls24315.E24
 	one.SetOne()
 	resPairing.AssertIsEqual(api, one)
 
