b93e9369fdd9138ac4305d60b4a24a2ee0c97787
Gautam Botrel <gautam.botrel@gmail.com>
2022-07-22 10:23:15 -0500

    fix: fix version test to work on CI using git ls-remote

--- version_test.go
+++ version_test.go
@@ -11,12 +11,17 @@ import (
 
 func TestVersion(t *testing.T) {
 	assert := require.New(t)
-	// git describe --abbrev=0
-	cmd := exec.Command("git", "describe", "--abbrev=0")
+	// git describe --abbrev=0 --> doesn't work on CI
+	// git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags https://github.com/consensys/gnark-crypto '*.*.*'
+	cmd := exec.Command("git", "-c", "versionsort.suffix=-", "ls-remote", "--exit-code", "--refs", "--sort=version:refname",
+		"--tags", "https://github.com/ConsenSys/gnark", "*.*.*")
 	stdout, err := cmd.Output()
 	assert.NoError(err)
 
-	lastTag := strings.TrimSpace(string(stdout))
+	splitted := strings.Split(string(stdout), "/")
+	lastTag := splitted[len(splitted)-1]
+
+	// lastTag := strings.TrimSpace(string(stdout))
 	lastVersion, err := semver.ParseTolerant(lastTag)
 	assert.NoError(err)
 
