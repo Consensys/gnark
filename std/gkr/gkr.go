8fafec41282ed0c4a983db53621d5c0d82480621
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2022-09-05 12:48:46 -0400

    feat!: copy paste gkr verifier from gnark-crypto

---
+++ std/gkr/gkr.go
@@ -0,0 +1,74 @@
+package gkr
+
+import (
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/sumcheck"
+)
+
+type Gate interface {
+	Evaluate(frontend.API, ...frontend.Variable) frontend.Variable
+	Degree() int
+}
+
+type Wire struct {
+	Gate       Gate
+	Inputs     []*Wire // if there are no Inputs, the wire is assumed an input wire
+	NumOutputs int     // number of other wires using it as input, not counting doubles (i.e. providing two inputs to the same gate counts as one). By convention, equal to 1 for output wires
+}
+
+type CircuitLayer []Wire
+
+// TODO: Constructor so that user doesn't have to give layers explicitly.
+type Circuit []CircuitLayer
+
+func (w *Wire) IsInput() bool {
+	return len(w.Inputs) == 0
+}
+
+func (c Circuit) Size() int { //TODO: Worth caching?
+	res := len(c[0])
+	for i := range c {
+		res += len(c[i])
+	}
+	return res
+}
+
+// WireAssignment is assignment of values to the same wire across many instances of the circuit
+type WireAssignment map[*Wire]sumcheck.MultiLin
+
+type Proof [][]sumcheck.Proof // for each layer, for each wire, a sumcheck (for each variable, a polynomial)
+
+type eqTimesGateEvalSumcheckLazyClaims struct {
+	wire               *Wire
+	evaluationPoints   [][]frontend.Variable
+	claimedEvaluations []frontend.Variable
+	manager            *claimsManager // WARNING: Circular references
+}
+
+func (e *eqTimesGateEvalSumcheckLazyClaims) ClaimsNum() int {
+	return len(e.evaluationPoints)
+}
+
+func (e *eqTimesGateEvalSumcheckLazyClaims) VarsNum() int {
+	return len(e.evaluationPoints[0])
+}
+
+func (e *eqTimesGateEvalSumcheckLazyClaims) CombinedSum(a fr.Element) fr.Element {
+	evalsAsPoly := polynomial.Polynomial(e.claimedEvaluations)
+	return evalsAsPoly.Eval(&a)
+}
+
+func (e *eqTimesGateEvalSumcheckLazyClaims) Degree(int) int {
+	return 1 + e.wire.Gate.Degree()
+}
+
+type Verifier struct {
+	Assignment interface{}
+	Proof      interface{}
+	Transcript sumcheck.ArithmeticTranscript
+}
+
+func (v *Verifier) Define(api frontend.API) error {
+	//TODO implement me
+	panic("implement me")
+}
