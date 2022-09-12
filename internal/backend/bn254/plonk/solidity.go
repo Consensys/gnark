22bcffc105b5afaaced6597c3924febdfe32328a
Gautam Botrel <gautam.botrel@gmail.com>
2022-09-12 13:03:31 -0500

    feat: change solidity ver for plonk

--- internal/backend/bn254/plonk/solidity.go
+++ internal/backend/bn254/plonk/solidity.go
@@ -7,7 +7,7 @@ const solidityTemplate = `
 // It has not been audited and is provided as-is, we make no guarantees or warranties to its safety and reliability. 
 // 
 // According to https://eprint.iacr.org/archive/2019/953/1585767119.pdf
-pragma solidity >=0.5.0 <0.7.0;
+pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
 library PairingsBn254 {
