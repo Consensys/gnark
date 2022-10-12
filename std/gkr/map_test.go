ef885e48e374de17ed3496f24678265e36b1e20f
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2022-10-12 14:22:57 -0400

    test: doubleMap works

--- std/gkr/map_test.go
+++ std/gkr/map_test.go
@@ -1,9 +1,12 @@
 package gkr_test
 
 import (
+	"fmt"
 	"github.com/consensys/gnark/backend"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/test"
+	"github.com/stretchr/testify/assert"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -41,6 +44,7 @@ func (m Map) Get(api frontend.API, key frontend.Variable) frontend.Variable {
 	return res
 }
 
+// The keys in a DoubleMap must be constant. i.e. known at setup time
 type DoubleMap struct {
 	keys1  []frontend.Variable
 	keys2  []frontend.Variable
@@ -48,7 +52,7 @@ type DoubleMap struct {
 }
 
 // Get is very inefficient. Do not use outside testing
-func (m DoubleMap) Get(api frontend.API, key1, key2 frontend.API) frontend.Variable {
+func (m DoubleMap) Get(api frontend.API, key1, key2 frontend.Variable) frontend.Variable {
 	deltas1 := make([]frontend.Variable, len(m.keys1))
 	deltas2 := make([]frontend.Variable, len(m.keys2))
 
@@ -64,9 +68,11 @@ func (m DoubleMap) Get(api frontend.API, key1, key2 frontend.API) frontend.Varia
 
 	for i := range deltas1 {
 		for j := range deltas2 {
-
-			deltaIJ := api.Mul(deltas1[i], deltas2[j], m.values[i][j])
-			res = api.Add(res, deltaIJ)
+			if m.values[i][j] != nil {
+				api.Println(m.keys1[i], m.keys2[j])
+				deltaIJ := api.Mul(deltas1[i], deltas2[j], m.values[i][j])
+				res = api.Add(res, deltaIJ)
+			}
 		}
 	}
 
@@ -77,6 +83,14 @@ func register[K comparable](m map[K]int, key K) {
 	if _, ok := m[key]; !ok {
 		m[key] = len(m)
 	}
+}
+
+func orderKeys[K comparable](order map[K]int) (ordered []K) {
+	ordered = make([]K, len(order))
+	for k, i := range order {
+		ordered[i] = k
+	}
+	return
 }
 
 func ReadMap(in map[string]interface{}) (Map, DoubleMap) {
@@ -119,8 +133,8 @@ func ReadMap(in map[string]interface{}) (Map, DoubleMap) {
 	}
 
 	double := DoubleMap{
-		keys1:  toVariableSlice(getKeys(keys1)),
-		keys2:  toVariableSlice(getKeys(keys2)),
+		keys1:  toVariableSlice(orderKeys(keys1)),
+		keys2:  toVariableSlice(orderKeys(keys2)),
 		values: vals,
 	}
 
@@ -188,4 +202,115 @@ func TestSingleMap(t *testing.T) {
 
 	assert := test.NewAssert(t)
 	assert.ProverSucceeded(&circuit, &assignment, test.WithBackends(backend.GROTH16))
+}
+
+type TestDoubleMapCircuit struct {
+	M      DoubleMap `gnark:"-"`
+	Values []frontend.Variable
+	Keys1  []frontend.Variable `gnark:"-"`
+	Keys2  []frontend.Variable `gnark:"-"`
+}
+
+func (c *TestDoubleMapCircuit) Define(api frontend.API) error {
+
+	for i := range c.Keys1 {
+		v := c.M.Get(api, c.Keys1[i], c.Keys2[i])
+		api.AssertIsEqual(v, c.Values[i])
+	}
+
+	return nil
+}
+
+func toMap(keys1, keys2, values []frontend.Variable) map[string]interface{} {
+	res := make(map[string]interface{}, len(keys1))
+	for i := range keys1 {
+		str := strconv.Itoa(keys1[i].(int)) + "," + strconv.Itoa(keys2[i].(int))
+		res[str] = values[i].(int)
+	}
+	return res
+}
+
+func TestReadDoubleMap(t *testing.T) {
+	keys1 := []frontend.Variable{1, 2}
+	keys2 := []frontend.Variable{1, 0}
+	values := []frontend.Variable{3, 1}
+
+	for i := 0; i < 100; i++ {
+		m := toMap(keys1, keys2, values)
+		_, double := ReadMap(m)
+		valuesOrdered := [][]frontend.Variable{{3, nil}, {nil, 1}}
+
+		assert.True(t, double.keys1[0] == "1" && double.keys1[1] == "2" || double.keys1[0] == "2" && double.keys1[1] == "1")
+		assert.True(t, double.keys2[0] == "1" && double.keys2[1] == "0" || double.keys2[0] == "0" && double.keys2[1] == "1")
+
+		if double.keys1[0] != "1" {
+			valuesOrdered[0], valuesOrdered[1] = valuesOrdered[1], valuesOrdered[0]
+		}
+
+		if double.keys2[0] != "1" {
+			valuesOrdered[0][0], valuesOrdered[0][1] = valuesOrdered[0][1], valuesOrdered[0][0]
+			valuesOrdered[1][0], valuesOrdered[1][1] = valuesOrdered[1][1], valuesOrdered[1][0]
+		}
+
+		assert.True(t, slice2Eq(valuesOrdered, double.values))
+
+	}
+
+}
+
+func slice2Eq(s1, s2 [][]frontend.Variable) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := range s1 {
+		if !sliceEq(s1[i], s2[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func sliceEq(s1, s2 []frontend.Variable) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := range s1 {
+		if s1[i] != s2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestDoubleMap(t *testing.T) {
+	keys1 := []frontend.Variable{1, 5, 5, 3}
+	keys2 := []frontend.Variable{1, -5, 4, 4}
+	values := []frontend.Variable{0, 2, 3, 0}
+
+	m := toMap(keys1, keys2, values)
+	_, double := ReadMap(m)
+
+	fmt.Println(double)
+
+	assignment := TestDoubleMapCircuit{
+		M:      double,
+		Values: values,
+		Keys1:  keys1,
+		Keys2:  keys2,
+	}
+
+	circuit := TestDoubleMapCircuit{
+		M:      double,
+		Keys1:  keys1,
+		Keys2:  keys2,
+		Values: make([]frontend.Variable, len(m)), // Okay to use the same object?
+	}
+
+	test.NewAssert(t).ProverSucceeded(&circuit, &assignment, test.WithBackends(backend.GROTH16))
+}
+
+func TestDoubleMapManyTimes(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		TestDoubleMap(t)
+	}
 }
