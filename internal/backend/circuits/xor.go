5c421d2814cf7dbb0ef71a61c107f61afb4685fa
Thomas Piellard <thomas.piellard@consensys.net>
2022-10-12 19:24:49 +0200

    fix: simplified xor in r1cs, variables multiplied by 0 are removed from lienarExpression

--- internal/backend/circuits/xor.go
+++ internal/backend/circuits/xor.go
@@ -2,9 +2,58 @@ package circuits
 
 import (
 	"github.com/consensys/gnark"
+	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/frontend"
 )
 
+// one input is constant
+type XorCircuitVarCst struct {
+	Op      frontend.Variable
+	XorOne  frontend.Variable `gnark:“,public”`
+	XorZero frontend.Variable `gnark:“,public”`
+}
+
+func (circuit *XorCircuitVarCst) Define(api frontend.API) error {
+	a := api.Xor(circuit.Op, 1)
+	b := api.Xor(circuit.Op, 0)
+	api.AssertIsEqual(a, circuit.XorOne)
+	api.AssertIsEqual(b, circuit.XorZero)
+	return nil
+}
+
+func init() {
+
+	good := []frontend.Circuit{
+		&XorCircuitVarCst{
+			Op:      (1),
+			XorOne:  (0),
+			XorZero: (1),
+		},
+		&XorCircuitVarCst{
+			Op:      (0),
+			XorOne:  (1),
+			XorZero: (1),
+		},
+	}
+
+	bad := []frontend.Circuit{
+		&XorCircuitVarCst{
+			Op:      (0),
+			XorOne:  (0),
+			XorZero: (1),
+		},
+		&XorCircuitVarCst{
+			Op:      (1),
+			XorOne:  (1),
+			XorZero: (1),
+		},
+	}
+
+	addNewEntry("xorCstVar", &xorCircuit{}, good, bad, []ecc.ID{ecc.BN254})
+
+}
+
+// both inputs are variable
 type xorCircuit struct {
 	Op1, Op2, Res frontend.Variable
 }
