64bd20ef01356900e612fbbe6ad7bd2d9bb06876
zhiqiangxu <652732310@qq.com>
2022-11-16 00:23:03 +0800

    use fixed bytes

--- std/decompose.go
+++ std/decompose.go
@@ -5,14 +5,17 @@ import "math/big"
 func Decompose(rawBytes []byte, modulos *big.Int) (decomposed []byte) {
 	raw := big.NewInt(0).SetBytes(rawBytes)
 
+	var chunk [32]byte
 	decomposed = make([]byte, 0, len(rawBytes))
 	for raw.Cmp(modulos) >= 0 {
 		mod := big.NewInt(0).Mod(raw, modulos)
-		decomposed = append(decomposed, mod.Bytes()...)
+		mod.FillBytes(chunk[:])
+		decomposed = append(decomposed, chunk[:]...)
 
 		raw.Div(raw, modulos)
 	}
 
-	decomposed = append(decomposed, raw.Bytes()...)
+	raw.FillBytes(chunk[:])
+	decomposed = append(decomposed, chunk[:]...)
 	return decomposed
 }
