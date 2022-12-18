7eac0f388e134a7579b35048f77f7f90e3120d1c
SherLzp <lzp.sher@gmail.com>
2022-12-18 19:49:32 +0800

    [Sher] pairing related e6 operations

--- std/algebra/pairing_bw6761/e6_pairing.go
+++ std/algebra/pairing_bw6761/e6_pairing.go
@@ -17,3 +17,151 @@
  */
 
 package pairing_bw6761
+
+func (e ext6) nSquare(z *E6, n int) {
+	for i := 0; i < n; i++ {
+		z = e.CyclotomicSquare(z)
+	}
+}
+
+func (e ext6) nSquareCompressed(z *E6, n int) {
+	for i := 0; i < n; i++ {
+		z = e.CyclotomicSquareCompressed(z)
+	}
+}
+
+// Expt set z to x^t in *E6 and return z
+func (e ext6) Expt(x *E6) *E6 {
+	// const tAbsVal uint64 = 9586122913090633729
+	// tAbsVal in binary: 1000010100001000110000000000000000000000000000000000000000000001
+	// drop the low 46 bits (all 0 except the least significant bit): 100001010000100011 = 136227
+	// Shortest addition chains can be found at https://wwwhomes.uni-bielefeld.de/achim/addition_chain.html
+
+	// a shortest addition chain for 136227
+	result := &E6{
+		B0: x.B0,
+		B1: x.B1,
+	}
+	e.nSquare(result, 5)
+	result = e.Mul(result, x)
+	x33 := &E6{
+		B0: result.B0,
+		B1: result.B1,
+	}
+	e.nSquare(result, 7)
+	result = e.Mul(result, x33)
+	e.nSquare(result, 4)
+	result = e.Mul(result, x)
+	result = e.CyclotomicSquare(result)
+	result = e.Mul(result, x)
+
+	// the remaining 46 bits
+	e.nSquareCompressed(result, 46)
+	result = e.DecompressKarabina(result)
+	result = e.Mul(result, x)
+
+	return &E6{
+		B0: result.B0,
+		B1: result.B1,
+	}
+}
+
+// Expc2 set z to x^c2 in *E6 and return z
+// ht, hy = 13, 9
+// c1 = ht+hy = 22 (10110)
+func (e ext6) Expc2(x *E6) *E6 {
+
+	result := e.CyclotomicSquare(x)
+	result = e.CyclotomicSquare(result)
+	result = e.Mul(result, x)
+	result = e.CyclotomicSquare(result)
+	result = e.Mul(result, x)
+	result = e.CyclotomicSquare(result)
+
+	return &E6{
+		B0: result.B0,
+		B1: result.B1,
+	}
+}
+
+// Expc1 set z to x^c1 in *E6 and return z
+// ht, hy = 13, 9
+// c1 = ht**2+3*hy**2 = 412 (110011100)
+func (e ext6) Expc1(x *E6) *E6 {
+
+	result := e.CyclotomicSquare(x)
+	result = e.Mul(result, x)
+	result = e.CyclotomicSquare(result)
+	result = e.CyclotomicSquare(result)
+	result = e.CyclotomicSquare(result)
+	result = e.Mul(result, x)
+	result = e.CyclotomicSquare(result)
+	result = e.Mul(result, x)
+	result = e.CyclotomicSquare(result)
+	result = e.Mul(result, x)
+	result = e.CyclotomicSquare(result)
+	result = e.CyclotomicSquare(result)
+
+	return &E6{
+		B0: result.B0,
+		B1: result.B1,
+	}
+}
+
+// MulBy034 multiplication by sparse element (c0,0,0,c3,c4,0)
+func (e ext6) MulBy034(z *E6, c0, c3, c4 *baseField) *E6 {
+
+	a := e.ext3.MulByElement(&z.B0, c0)
+
+	b := &E3{
+		A0: z.B1.A0,
+		A1: z.B1.A1,
+		A2: z.B1.A2,
+	}
+	b = e.ext3.MulBy01(b, c3, c4)
+
+	c0 = e.fp.Add(c0, c3)
+	d := e.ext3.Add(&z.B0, &z.B1)
+	d = e.ext3.MulBy01(d, c0, c4)
+
+	z.B1 = *e.ext3.Add(a, b)
+	z.B1 = *e.ext3.Neg(&z.B1)
+	z.B1 = *e.ext3.Add(&z.B1, d)
+	z.B0 = *e.ext3.MulByNonResidue(b)
+	z.B0 = *e.ext3.Add(&z.B0, a)
+
+	return z
+}
+
+// Mul034By034 multiplication of sparse element (c0,0,0,c3,c4,0) by sparse element (d0,0,0,d3,d4,0)
+func (e ext6) Mul034By034(d0, d3, d4, c0, c3, c4 *baseField) *E6 {
+	x0 := e.fp.Mul(c0, d0)
+	x3 := e.fp.Mul(c3, d3)
+	x4 := e.fp.Mul(c4, d4)
+	tmp := e.fp.Add(c0, c4)
+	x04 := e.fp.Add(d0, d4)
+	x04 = e.fp.Mul(x04, tmp)
+	x04 = e.fp.Sub(x04, x0)
+	x04 = e.fp.Sub(x04, x4)
+	tmp = e.fp.Add(c0, c3)
+	x03 := e.fp.Add(d0, d3)
+	x03 = e.fp.Mul(x03, tmp)
+	x03 = e.fp.Sub(x03, x0)
+	x03 = e.fp.Sub(x03, x3)
+	tmp = e.fp.Add(c3, c4)
+	x34 := e.fp.Add(d3, d4)
+	x34 = e.fp.Mul(x34, tmp)
+	x34 = e.fp.Sub(x34, x3)
+	x34 = e.fp.Sub(x34, x4)
+
+	var z E6
+	z.B0.A0 = *MulByNonResidue(e.fp, x4)
+	z.B0.A0 = *e.fp.Add(&z.B0.A0, x0)
+	z.B0.A1 = *x3
+	z.B0.A2 = *x34
+	z.B1.A0 = *x03
+	z.B1.A1 = *x04
+	z.B1.A2 = *e.fp.Zero()
+
+	return &z
+}
