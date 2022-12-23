a664c387aa297974db75f523054831e423919392
SherLzp <lzp.sher@gmail.com>
2022-12-23 14:01:24 +0800

    [Sher] String for debug

--- std/math/emulated/debug.go
+++ std/math/emulated/debug.go
@@ -17,3 +17,37 @@
  */
 
 package emulated
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func (f *Field[T]) String(a *Element[T]) (string, error) {
+	// for debug only, if is not test engine then no-op
+	var fp T
+	blimbs := make([]*big.Int, len(a.Limbs))
+	for i, v := range a.Limbs {
+		switch vv := v.(type) {
+		case *big.Int:
+			blimbs[i] = vv
+		case big.Int:
+			blimbs[i] = &vv
+		case int:
+			blimbs[i] = new(big.Int)
+			blimbs[i].SetInt64(int64(vv))
+		case uint:
+			blimbs[i] = new(big.Int)
+			blimbs[i].SetUint64(uint64(vv))
+		default:
+			return "", fmt.Errorf("not big int")
+		}
+	}
+	res := new(big.Int)
+	err := recompose(blimbs, fp.BitsPerLimb(), res)
+	if err != nil {
+		return "", fmt.Errorf("recompose: %w", err)
+	}
+	reduced := new(big.Int).Mod(res, fp.Modulus())
+	return reduced.String(), nil
+}
