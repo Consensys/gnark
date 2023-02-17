c2f401d0421877d0e15535367b761737f4f207b1
Ivo Kubjas <ivo.kubjas@consensys.net>
2023-02-17 19:39:01 +0100

    docs: add package documentation and example

---
+++ std/algebra/pairing_bn254/doc.go
@@ -0,0 +1,7 @@
+// Package pairing_bn254 implements pairing over BN254 curve.
+//
+// The implementation follows very closely the implementation of its out-circuit
+// counterpart in [gnark-crypto].
+//
+// [gnark-crypto]: https://github.com/ConsenSys/gnark-crypto/tree/master/ecc/bn254
+package pairing_bn254
