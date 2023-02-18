3b7d7b2bb6a791654df3664d6c019c627ab34c5f
aybehrouz <behrouz_ayati@yahoo.com>
2023-02-18 14:00:37 +0330

    Refactor multiplexer test

--- std/gadgets/multiplexer_test.go
+++ std/gadgets/multiplexer_test.go
@@ -23,18 +23,15 @@ func (c *muxCircuit) Define(api frontend.API) error {
 	return nil
 }
 
-// The output of this circuit is ignored and the only way its proof can fail is by invalid inputs.
-type zeroedMuxCircuit struct {
+// The output of this circuit is ignored and the only way its proof can fail is by providing invalid inputs.
+type ignoredOutputMuxCircuit struct {
 	SEL        frontend.Variable
 	I0, I1, I2 frontend.Variable
 }
 
-func (c *zeroedMuxCircuit) Define(api frontend.API) error {
-
-	out := gadgets.Mux(api, c.SEL, c.I0, c.I1, c.I2)
-
-	// a dummy constraint
-	api.AssertIsEqual(api.Mul(out, 0), 0)
+func (c *ignoredOutputMuxCircuit) Define(api frontend.API) error {
+	// We ignore the output
+	_ = gadgets.Mux(api, c.SEL, c.I0, c.I1, c.I2)
 
 	return nil
 }
@@ -46,24 +43,20 @@ func TestMux(t *testing.T) {
 
 	assert.ProverSucceeded(&muxCircuit{}, &muxCircuit{SEL: 0, I0: 10, I1: 11, I2: 12, I3: 13, I4: 14, OUT: 10})
 
-	assert.ProverSucceeded(&muxCircuit{}, &muxCircuit{SEL: 4, I0: 10, I1: 11, I2: 12, I3: 13, I4: 14, OUT: 14})
-
-	assert.ProverSucceeded(&muxCircuit{}, &muxCircuit{SEL: 3, I0: 20, I1: 21, I2: 22, I3: 23, I4: 24, OUT: 23})
+	assert.ProverSucceeded(&muxCircuit{}, &muxCircuit{SEL: 4, I0: 20, I1: 21, I2: 22, I3: 23, I4: 24, OUT: 24})
 
 	// Failures
 	assert.ProverFailed(&muxCircuit{}, &muxCircuit{SEL: 5, I0: 20, I1: 21, I2: 22, I3: 23, I4: 24, OUT: 24})
 
 	assert.ProverFailed(&muxCircuit{}, &muxCircuit{SEL: 0, I0: 20, I1: 21, I2: 22, I3: 23, I4: 24, OUT: 21})
 
-	// Zeroed circuit:
-	assert.ProverSucceeded(&zeroedMuxCircuit{}, &zeroedMuxCircuit{SEL: 0, I0: 0, I1: 1, I2: 2})
+	// Ignoring the circuit's output:
+	assert.ProverSucceeded(&ignoredOutputMuxCircuit{}, &ignoredOutputMuxCircuit{SEL: 0, I0: 0, I1: 1, I2: 2})
 
-	assert.ProverSucceeded(&zeroedMuxCircuit{}, &zeroedMuxCircuit{SEL: 2, I0: 0, I1: 1, I2: 2})
+	assert.ProverSucceeded(&ignoredOutputMuxCircuit{}, &ignoredOutputMuxCircuit{SEL: 2, I0: 0, I1: 1, I2: 2})
 
 	// Failures
-	assert.ProverFailed(&zeroedMuxCircuit{}, &zeroedMuxCircuit{SEL: 3, I0: 0, I1: 1, I2: 2})
-	assert.SolvingFailed(&zeroedMuxCircuit{}, &zeroedMuxCircuit{SEL: 3, I0: 0, I1: 1, I2: 2})
+	assert.ProverFailed(&ignoredOutputMuxCircuit{}, &ignoredOutputMuxCircuit{SEL: 3, I0: 0, I1: 1, I2: 2})
 
-	assert.ProverFailed(&zeroedMuxCircuit{}, &zeroedMuxCircuit{SEL: -1, I0: 0, I1: 1, I2: 2})
-	assert.SolvingFailed(&zeroedMuxCircuit{}, &zeroedMuxCircuit{SEL: -1, I0: 0, I1: 1, I2: 2})
+	assert.ProverFailed(&ignoredOutputMuxCircuit{}, &ignoredOutputMuxCircuit{SEL: -1, I0: 0, I1: 1, I2: 2})
 }
