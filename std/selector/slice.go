7608209c5d5bf80b3855f90965b9030317c56bce
aybehrouz <behrouz_ayati@yahoo.com>
2023-02-23 23:44:36 +0330

    docs: add missing docs

--- std/selector/slice.go
+++ std/selector/slice.go
@@ -12,7 +12,7 @@ func init() {
 }
 
 // Partition selects left or right side of the input array, with respect to the pivotPosition.
-// More precisely when rightSide is false, for each i:
+// More precisely when rightSide is false, for each i we have:
 //
 //	if i < pivotPosition
 //	    out[i] = input[i]
@@ -28,8 +28,8 @@ func init() {
 //
 // We must have pivotPosition >= 1 and pivotPosition <= len(input), otherwise a proof cannot be generated.
 func Partition(api frontend.API, pivotPosition frontend.Variable, rightSide bool,
-	input []frontend.Variable) []frontend.Variable {
-	out := make([]frontend.Variable, len(input))
+	input []frontend.Variable) (out []frontend.Variable) {
+	out = make([]frontend.Variable, len(input))
 	var mask []frontend.Variable
 	// we create a mask of len(input)+1 to be able to handle the case pivotPosition == len(input)
 	if rightSide {
@@ -40,22 +40,29 @@ func Partition(api frontend.API, pivotPosition frontend.Variable, rightSide bool
 	for i := 0; i < len(out); i++ {
 		out[i] = api.Mul(mask[i], input[i])
 	}
-	return out
+	return
 }
 
+// StepMask generates a step like function into an output array of a given length.
+// The output is an array of length outputLen,
+// such that its first stepPosition elements are equal to startValue and the remaining elements are equal to
+// endValue. Note that outputLen cannot be a circuit variable.
+//
+// We must have stepPosition >= 1 and stepPosition <= outputLen, otherwise a proof cannot be generated.
+// This function panics when outputLen is less than 2.
 func StepMask(api frontend.API, outputLen int,
 	stepPosition, startValue, endValue frontend.Variable) []frontend.Variable {
 	if outputLen < 2 {
 		panic("output len must be >= 2")
 	}
-	// Get the output as a hint
+	// get the output as a hint
 	out, _ := api.Compiler().NewHint(StepOutput, outputLen, stepPosition, startValue, endValue)
 
-	// Add boundary constraints
+	// add boundary constraints
 	api.AssertIsEqual(out[0], startValue)
 	api.AssertIsEqual(out[len(out)-1], endValue)
 
-	// Add constraints for the correct form of a step function that steps at the stepPosition
+	// add constraints for the correct form of a step function that steps at the stepPosition
 	for i := 1; i < len(out); i++ {
 		// (out[i] - out[i-1]) * (i - stepPosition) == 0
 		api.AssertIsEqual(api.Mul(api.Sub(out[i], out[i-1]), api.Sub(i, stepPosition)), 0)
