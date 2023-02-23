bc54b6bc0536690e978834d8897c1b3365b0833a
aybehrouz <behrouz_ayati@yahoo.com>
2023-02-24 00:08:53 +0330

    test: add more tests

--- std/selector/slice_test.go
+++ std/selector/slice_test.go
@@ -8,21 +8,34 @@ import (
 )
 
 type partitionerCircuit struct {
-	Pivot frontend.Variable
-	In    [6]frontend.Variable
-	Left  [6]frontend.Variable
-	Right [6]frontend.Variable
+	Pivot     frontend.Variable    `gnark:",public"`
+	In        [6]frontend.Variable `gnark:",public"`
+	WantLeft  [6]frontend.Variable `gnark:",public"`
+	WantRight [6]frontend.Variable `gnark:",public"`
 }
 
 func (c *partitionerCircuit) Define(api frontend.API) error {
-	left := selector.Partition(api, c.Pivot, false, c.In[:])
-	for i, want := range c.Left {
-		api.AssertIsEqual(want, left[i])
+	gotLeft := selector.Partition(api, c.Pivot, false, c.In[:])
+	for i, want := range c.WantLeft {
+		api.AssertIsEqual(gotLeft[i], want)
 	}
-	right := selector.Partition(api, c.Pivot, true, c.In[:])
-	for i, want := range c.Right {
-		api.AssertIsEqual(want, right[i])
+
+	gotRight := selector.Partition(api, c.Pivot, true, c.In[:])
+	for i, want := range c.WantRight {
+		api.AssertIsEqual(gotRight[i], want)
 	}
+
+	return nil
+}
+
+type ignoredOutputPartitionerCircuit struct {
+	Pivot frontend.Variable    `gnark:",public"`
+	In    [2]frontend.Variable `gnark:",public"`
+}
+
+func (c *ignoredOutputPartitionerCircuit) Define(api frontend.API) error {
+	_ = selector.Partition(api, c.Pivot, false, c.In[:])
+	_ = selector.Partition(api, c.Pivot, true, c.In[:])
 	return nil
 }
 
@@ -30,45 +43,61 @@ func TestPartition(t *testing.T) {
 	assert := test.NewAssert(t)
 
 	assert.ProverSucceeded(&partitionerCircuit{}, &partitionerCircuit{
-		Pivot: 3,
-		In:    [6]frontend.Variable{10, 20, 30, 40, 50, 60},
-		Left:  [6]frontend.Variable{10, 20, 30, 0, 0, 0},
-		Right: [6]frontend.Variable{0, 0, 0, 40, 50, 60},
+		Pivot:     3,
+		In:        [6]frontend.Variable{10, 20, 30, 40, 50, 60},
+		WantLeft:  [6]frontend.Variable{10, 20, 30, 0, 0, 0},
+		WantRight: [6]frontend.Variable{0, 0, 0, 40, 50, 60},
 	})
 
 	assert.ProverSucceeded(&partitionerCircuit{}, &partitionerCircuit{
-		Pivot: 1,
-		In:    [6]frontend.Variable{10, 20, 30, 40, 50, 60},
-		Left:  [6]frontend.Variable{10, 0, 0, 0, 0, 0},
-		Right: [6]frontend.Variable{0, 20, 30, 40, 50, 60},
+		Pivot:     1,
+		In:        [6]frontend.Variable{10, 20, 30, 40, 50, 60},
+		WantLeft:  [6]frontend.Variable{10, 0, 0, 0, 0, 0},
+		WantRight: [6]frontend.Variable{0, 20, 30, 40, 50, 60},
 	})
 
 	assert.ProverSucceeded(&partitionerCircuit{}, &partitionerCircuit{
-		Pivot: 5,
-		In:    [6]frontend.Variable{10, 20, 30, 40, 50, 60},
-		Left:  [6]frontend.Variable{10, 20, 30, 40, 50, 0},
-		Right: [6]frontend.Variable{0, 0, 0, 0, 0, 60},
+		Pivot:     6,
+		In:        [6]frontend.Variable{10, 20, 30, 40, 50, 60},
+		WantLeft:  [6]frontend.Variable{10, 20, 30, 40, 50, 60},
+		WantRight: [6]frontend.Variable{0, 0, 0, 0, 0, 0},
 	})
 
-	assert.ProverSucceeded(&partitionerCircuit{}, &partitionerCircuit{
-		Pivot: 6,
-		In:    [6]frontend.Variable{10, 20, 30, 40, 50, 60},
-		Left:  [6]frontend.Variable{10, 20, 30, 40, 50, 60},
-		Right: [6]frontend.Variable{0, 0, 0, 0, 0, 0},
+	assert.ProverFailed(&partitionerCircuit{}, &partitionerCircuit{
+		Pivot:     5,
+		In:        [6]frontend.Variable{10, 20, 30, 40, 50, 60},
+		WantLeft:  [6]frontend.Variable{10, 20, 30, 40, 0, 0},
+		WantRight: [6]frontend.Variable{0, 0, 0, 0, 0, 0},
 	})
 
 	// Pivot is outside and the prover fails: (todo: this doesn't work. why?)
 	assert.ProverFailed(&partitionerCircuit{}, &partitionerCircuit{
-		Pivot: 7,
-		In:    [6]frontend.Variable{10, 20, 30, 40, 50, 60},
-		Left:  [6]frontend.Variable{10, 20, 30, 40, 50, 60},
-		Right: [6]frontend.Variable{0, 0, 0, 0, 0, 0},
+		Pivot:     7,
+		In:        [6]frontend.Variable{10, 20, 30, 40, 50, 60},
+		WantLeft:  [6]frontend.Variable{10, 20, 30, 40, 50, 60},
+		WantRight: [6]frontend.Variable{0, 0, 0, 0, 0, 0},
 	})
 
 	assert.ProverFailed(&partitionerCircuit{}, &partitionerCircuit{
+		Pivot:     0,
+		In:        [6]frontend.Variable{10, 20, 30, 40, 50, 60},
+		WantLeft:  [6]frontend.Variable{0, 0, 0, 0, 0, 0},
+		WantRight: [6]frontend.Variable{10, 20, 30, 40, 50, 60},
+	})
+
+	// tests by ignoring the output:
+	assert.ProverSucceeded(&ignoredOutputPartitionerCircuit{}, &ignoredOutputPartitionerCircuit{
+		Pivot: 1,
+		In:    [2]frontend.Variable{10, 20},
+	})
+
+	assert.ProverFailed(&ignoredOutputPartitionerCircuit{}, &ignoredOutputPartitionerCircuit{
 		Pivot: 0,
-		In:    [6]frontend.Variable{10, 20, 30, 40, 50, 60},
-		Left:  [6]frontend.Variable{0, 0, 0, 0, 0, 0},
-		Right: [6]frontend.Variable{10, 20, 30, 40, 50, 60},
+		In:    [2]frontend.Variable{10, 20},
+	})
+
+	assert.ProverFailed(&ignoredOutputPartitionerCircuit{}, &ignoredOutputPartitionerCircuit{
+		Pivot: 3,
+		In:    [2]frontend.Variable{10, 20},
 	})
 }
