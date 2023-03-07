5a038fa134927788c7f7103e809b72eabe5ec329
Gautam Botrel <gautam.botrel@gmail.com>
2023-03-07 13:47:40 -0600

    feat: gnark/profile now filter frontend private method for clarity and return a tree as txt repr

--- profile/profile_test.go
+++ profile/profile_test.go
@@ -4,11 +4,13 @@ package profile_test
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/r1cs"
 	"github.com/consensys/gnark/profile"
+	"golang.org/x/exp/slices"
 )
 
 type Circuit struct {
@@ -29,12 +31,27 @@ func Example() {
 	_, _ = frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &Circuit{})
 	p.Stop()
 
+	const expectedOutput = `Showing nodes accounting for 2, 100% of 2 total
+----------------------------------------------------------+-------------
+      flat  flat%   sum%        cum   cum%   calls calls% + context              
+----------------------------------------------------------+-------------
+                                                 1   100% |   profile_test.(*Circuit).Define profile/profile_test.go:21
+         1 50.00% 50.00%          1 50.00%                | r1cs.(*builder).AssertIsEqual frontend/cs/r1cs/api_assertions.go:37
+----------------------------------------------------------+-------------
+                                                 1   100% |   profile_test.(*Circuit).Define profile/profile_test.go:21
+         1 50.00%   100%          1 50.00%                | r1cs.(*builder).Mul frontend/cs/r1cs/api.go:221
+----------------------------------------------------------+-------------
+         0     0%   100%          2   100%                | profile_test.(*Circuit).Define profile/profile_test.go:21
+                                                 1 50.00% |   r1cs.(*builder).AssertIsEqual frontend/cs/r1cs/api_assertions.go:37
+                                                 1 50.00% |   r1cs.(*builder).Mul frontend/cs/r1cs/api.go:221
+----------------------------------------------------------+-------------`
+
+	a := strings.Fields(p.Top())
+	b := strings.Fields(expectedOutput)
+
 	fmt.Println(p.NbConstraints())
-	fmt.Println(p.Top())
+	fmt.Println(slices.Equal(a, b))
 	// Output:
 	// 2
-	// Showing nodes accounting for 2, 100% of 2 total
-	//       flat  flat%   sum%        cum   cum%
-	//          1 50.00% 50.00%          2   100%  profile_test.(*Circuit).Define profile/profile_test.go:19
-	//          1 50.00%   100%          1 50.00%  r1cs.(*builder).AssertIsEqual frontend/cs/r1cs/api_assertions.go:37
+	// true
 }
