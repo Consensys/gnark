35c079431401f0ac276eb9357bc86154839f8cce
SherLzp <lzp.sher@gmail.com>
2023-03-20 17:04:58 +0800

    delete comment

--- std/algebra/emulated/sw_bw6761/pairing_test.go
+++ std/algebra/emulated/sw_bw6761/pairing_test.go
@@ -20,7 +20,6 @@ package sw_bw6761
 
 import (
 	"crypto/rand"
-	"fmt"
 	"github.com/consensys/gnark-crypto/ecc"
 	bw6761 "github.com/consensys/gnark-crypto/ecc/bw6-761"
 	"github.com/consensys/gnark-crypto/ecc/bw6-761/fr"
@@ -56,34 +55,34 @@ func (circuit *millerLoopBW6761) Define(api frontend.API) error {
 
 func TestMillerLoopBW6761(t *testing.T) {
 	assert := test.NewAssert(t)
-	for i := 0; i < 1; i++ {
-		fmt.Println("round:", i)
-		// witness values
-		var (
-			a     bw6761.G1Affine
-			b     bw6761.G2Affine
-			c     bw6761.GT
-			r1, _ = rand.Int(rand.Reader, fr.Modulus())
-			r2, _ = rand.Int(rand.Reader, fr.Modulus())
-		)
-		_, _, g1, g2 := bw6761.Generators()
+	// witness values
+	var (
+		a     bw6761.G1Affine
+		b     bw6761.G2Affine
+		c     bw6761.GT
+		r1, _ = rand.Int(rand.Reader, fr.Modulus())
+		r2, _ = rand.Int(rand.Reader, fr.Modulus())
+	)
+	_, _, g1, g2 := bw6761.Generators()
 
-		a.ScalarMultiplication(&g1, r1)
-		b.ScalarMultiplication(&g2, r2)
-		c, err := bw6761.MillerLoop([]bw6761.G1Affine{a}, []bw6761.G2Affine{b})
-		if err != nil {
-			panic(err)
-		}
-
-		witness := millerLoopBW6761{
-			A: NewG1Affine(a),
-			B: NewG2Affine(b),
-			C: fields_bw6761.NewE6(c),
-		}
-
-		err = test.IsSolved(&millerLoopBW6761{}, &witness, testCurve.ScalarField())
-		assert.NoError(err)
+	a.ScalarMultiplication(&g1, r1)
+	b.ScalarMultiplication(&g2, r2)
+	c, err := bw6761.MillerLoop([]bw6761.G1Affine{a}, []bw6761.G2Affine{b})
+	if err != nil {
+		panic(err)
 	}
+
+	witness := millerLoopBW6761{
+		A: NewG1Affine(a),
+		B: NewG2Affine(b),
+		C: fields_bw6761.NewE6(c),
+	}
+
+	err = test.IsSolved(&millerLoopBW6761{}, &witness, testCurve.ScalarField())
+	assert.NoError(err)
+
+	_, err = frontend.Compile(testCurve.ScalarField(), r1cs.NewBuilder, &millerLoopBW6761{}, frontend.IgnoreUnconstrainedInputs())
+	assert.NoError(err)
 }
 
 type tripleMillerLoopBW6761 struct {
@@ -109,46 +108,43 @@ func (circuit *tripleMillerLoopBW6761) Define(api frontend.API) error {
 
 func TestDoubleMillerLoopBW6761(t *testing.T) {
 	assert := test.NewAssert(t)
-	for i := 0; i < 1; i++ {
-		fmt.Println("round:", i)
-		// witness values
-		var (
-			a1, a2, a3 bw6761.G1Affine
-			b1, b2, b3 bw6761.G2Affine
-			c          bw6761.GT
-			r1, _      = rand.Int(rand.Reader, fr.Modulus())
-			r2, _      = rand.Int(rand.Reader, fr.Modulus())
-			r3, _      = rand.Int(rand.Reader, fr.Modulus())
-			r4, _      = rand.Int(rand.Reader, fr.Modulus())
-			r5, _      = rand.Int(rand.Reader, fr.Modulus())
-			r6, _      = rand.Int(rand.Reader, fr.Modulus())
-		)
-		_, _, g1, g2 := bw6761.Generators()
+	// witness values
+	var (
+		a1, a2, a3 bw6761.G1Affine
+		b1, b2, b3 bw6761.G2Affine
+		c          bw6761.GT
+		r1, _      = rand.Int(rand.Reader, fr.Modulus())
+		r2, _      = rand.Int(rand.Reader, fr.Modulus())
+		r3, _      = rand.Int(rand.Reader, fr.Modulus())
+		r4, _      = rand.Int(rand.Reader, fr.Modulus())
+		r5, _      = rand.Int(rand.Reader, fr.Modulus())
+		r6, _      = rand.Int(rand.Reader, fr.Modulus())
+	)
+	_, _, g1, g2 := bw6761.Generators()
 
-		a1.ScalarMultiplication(&g1, r1)
-		a2.ScalarMultiplication(&g1, r2)
-		a3.ScalarMultiplication(&g1, r3)
-		b1.ScalarMultiplication(&g2, r4)
-		b2.ScalarMultiplication(&g2, r5)
-		b3.ScalarMultiplication(&g2, r6)
-		c, err := bw6761.MillerLoop([]bw6761.G1Affine{a1, a2, a3}, []bw6761.G2Affine{b1, b2, b3})
-		if err != nil {
-			panic(err)
-		}
-
-		witness := tripleMillerLoopBW6761{
-			A1: NewG1Affine(a1),
-			A2: NewG1Affine(a2),
-			A3: NewG1Affine(a3),
-			B1: NewG2Affine(b1),
-			B2: NewG2Affine(b2),
-			B3: NewG2Affine(b3),
-			C:  fields_bw6761.NewE6(c),
-		}
-
-		err = test.IsSolved(&tripleMillerLoopBW6761{}, &witness, testCurve.ScalarField())
-		assert.NoError(err)
+	a1.ScalarMultiplication(&g1, r1)
+	a2.ScalarMultiplication(&g1, r2)
+	a3.ScalarMultiplication(&g1, r3)
+	b1.ScalarMultiplication(&g2, r4)
+	b2.ScalarMultiplication(&g2, r5)
+	b3.ScalarMultiplication(&g2, r6)
+	c, err := bw6761.MillerLoop([]bw6761.G1Affine{a1, a2, a3}, []bw6761.G2Affine{b1, b2, b3})
+	if err != nil {
+		panic(err)
 	}
+
+	witness := tripleMillerLoopBW6761{
+		A1: NewG1Affine(a1),
+		A2: NewG1Affine(a2),
+		A3: NewG1Affine(a3),
+		B1: NewG2Affine(b1),
+		B2: NewG2Affine(b2),
+		B3: NewG2Affine(b3),
+		C:  fields_bw6761.NewE6(c),
+	}
+
+	err = test.IsSolved(&tripleMillerLoopBW6761{}, &witness, testCurve.ScalarField())
+	assert.NoError(err)
 }
 
 type finalExponentiationBW6761 struct {
@@ -198,6 +194,9 @@ func TestFinalExponentiationBW6761(t *testing.T) {
 	}
 
 	err = test.IsSolved(&finalExponentiationBW6761{}, &witness, testCurve.ScalarField())
+	assert.NoError(err)
+
+	_, err = frontend.Compile(testCurve.ScalarField(), r1cs.NewBuilder, &finalExponentiationBW6761{}, frontend.IgnoreUnconstrainedInputs())
 	assert.NoError(err)
 }
 
