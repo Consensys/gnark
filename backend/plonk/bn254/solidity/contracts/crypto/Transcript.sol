57ab51b10fe87294f180420b4c4942a9d8969e65
Thomas Piellard <thomas.piellard@consensys.net>
2023-04-04 19:19:22 +0200

    feat: updated transcript

--- backend/plonk/bn254/solidity/contracts/crypto/Transcript.sol
+++ backend/plonk/bn254/solidity/contracts/crypto/Transcript.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.8.19;
 pragma experimental ABIEncoderV2;
 
+import {Bn254} from '.Bn254.sol'
+
 library TranscriptLibrary {
  
     struct Transcript {
@@ -22,15 +24,15 @@ library TranscriptLibrary {
         self.bindings = abi.encodePacked(self.bindings, value);
     }
 
-    function update_with_fr(Transcript memory self, PairingsBn254.Fr memory value) internal pure {
+    function update_with_fr(Transcript memory self, uint254 value) internal pure {
         self.bindings = abi.encodePacked(self.bindings, value.value);
     }
 
-    function update_with_g1(Transcript memory self, PairingsBn254.G1Point memory p) internal pure {
+    function update_with_g1(Transcript memory self, Bn254.G1Point memory p) internal pure {
         self.bindings = abi.encodePacked(self.bindings, p.X, p.Y);
     }
 
-    function get_challenge(Transcript memory self) internal pure returns(PairingsBn254.Fr memory challenge) {
+    function get_challenge(Transcript memory self) internal pure returns(uint256 challenge) {
         bytes32 query;
         if (self.challenge_counter != 0) {
             query = sha256(abi.encodePacked(self.name, self.previous_randomness, self.bindings));
@@ -39,7 +41,7 @@ library TranscriptLibrary {
         }
         self.challenge_counter += 1;
         self.previous_randomness = query;
-        challenge = PairingsBn254.Fr({value: uint256(query) % PairingsBn254.r_mod});
+        challenge = query % Bn254.r_mod;
         self.bindings = "";
     }
 }
