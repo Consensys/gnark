6d83f5d056b70c5db274f2a1825576ac675295ea
Ivo Kubjas <ivo.kubjas@consensys.net>
2023-04-05 18:18:02 +0200

    fix: ECPair takes multiple input pairs

--- std/evmprecompiles/08-bnpairing.go
+++ std/evmprecompiles/08-bnpairing.go
@@ -8,12 +8,12 @@ import (
 // ECPair implements [ALT_BN128_PAIRING_CHECK] precompile contract at address 0x08.
 //
 // [ALT_BN128_PAIRING_CHECK]: https://ethereum.github.io/execution-specs/autoapi/ethereum/paris/vm/precompiled_contracts/alt_bn128/index.html#alt-bn128-pairing-check
-func ECPair(api frontend.API, P *sw_bn254.G1Affine, Q *sw_bn254.G2Affine) {
+func ECPair(api frontend.API, P []*sw_bn254.G1Affine, Q []*sw_bn254.G2Affine) {
 	pair, err := sw_bn254.NewPairing(api)
 	if err != nil {
 		panic(err)
 	}
-	if err := pair.PairingCheck([]*sw_bn254.G1Affine{P}, []*sw_bn254.G2Affine{Q}); err != nil {
+	if err := pair.PairingCheck(P, Q); err != nil {
 		panic(err)
 	}
 }
