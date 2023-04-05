c214616a437f29101481b0680dcc30ddea0563a6
Ivo Kubjas <ivo.kubjas@consensys.net>
2023-04-05 18:15:38 +0200

    fix: ECPair performs pairing check

--- std/evmprecompiles/08-bnpairing.go
+++ std/evmprecompiles/08-bnpairing.go
@@ -8,14 +8,12 @@ import (
 // ECPair implements [ALT_BN128_PAIRING_CHECK] precompile contract at address 0x08.
 //
 // [ALT_BN128_PAIRING_CHECK]: https://ethereum.github.io/execution-specs/autoapi/ethereum/paris/vm/precompiled_contracts/alt_bn128/index.html#alt-bn128-pairing-check
-func ECPair(api frontend.API, P *sw_bn254.G1Affine, Q *sw_bn254.G2Affine) *sw_bn254.GTEl {
+func ECPair(api frontend.API, P *sw_bn254.G1Affine, Q *sw_bn254.G2Affine) {
 	pair, err := sw_bn254.NewPairing(api)
 	if err != nil {
 		panic(err)
 	}
-	R, err := pair.Pair([]*sw_bn254.G1Affine{P}, []*sw_bn254.G2Affine{Q})
-	if err != nil {
+	if err := pair.PairingCheck([]*sw_bn254.G1Affine{P}, []*sw_bn254.G2Affine{Q}); err != nil {
 		panic(err)
 	}
-	return R
 }
