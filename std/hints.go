ebc600136fc5d9cecfd0fe3aa98c713a51bed55b
Ivo Kubjas <ivo.kubjas@consensys.net>
2023-04-05 15:43:44 +0200

    feat: add generic log-derivative arg

--- std/hints.go
+++ std/hints.go
@@ -7,6 +7,7 @@ import (
 	"github.com/consensys/gnark/std/algebra/native/sw_bls12377"
 	"github.com/consensys/gnark/std/algebra/native/sw_bls24315"
 	"github.com/consensys/gnark/std/evmprecompiles"
+	"github.com/consensys/gnark/std/internal/logderivarg"
 	"github.com/consensys/gnark/std/math/bits"
 	"github.com/consensys/gnark/std/math/emulated"
 	"github.com/consensys/gnark/std/rangecheck"
@@ -37,4 +38,5 @@ func registerHints() {
 	solver.RegisterHint(emulated.GetHints()...)
 	solver.RegisterHint(rangecheck.CountHint, rangecheck.DecomposeHint)
 	solver.RegisterHint(evmprecompiles.GetHints()...)
+	solver.RegisterHint(logderivarg.GetHints()...)
 }
