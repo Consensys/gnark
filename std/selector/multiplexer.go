174e5ada129a2d3f72e8aed65780853f6e641f25
aybehrouz <behrouz_ayati@yahoo.com>
2023-05-04 19:26:31 +0330

    style: wrap comment lines on 80

--- std/selector/multiplexer.go
+++ std/selector/multiplexer.go
@@ -33,25 +33,29 @@ func GetHints() []solver.Hint {
 	return []solver.Hint{stepOutput, muxIndicators, mapIndicators}
 }
 
-// Map is a key value associative array: the output will be values[i] such that keys[i] == queryKey. If keys does not
-// contain queryKey, no proofs can be generated. If keys has more than one key that equals to queryKey, the output will
-// be undefined, and the output could be any linear combination of all the corresponding values with that queryKey.
+// Map is a key value associative array: the output will be values[i] such that
+// keys[i] == queryKey. If keys does not contain queryKey, no proofs can be
+// generated. If keys has more than one key that equals to queryKey, the output
+// will be undefined, and the output could be any linear combination of all the
+// corresponding values with that queryKey.
 //
-// In case keys and values do not have the same length, this function will panic.
+// In case keys and values do not have the same length, this function will
+// panic.
 func Map(api frontend.API, queryKey frontend.Variable,
 	keys []frontend.Variable, values []frontend.Variable) frontend.Variable {
-	// we don't need this check, but we added it to produce more informative errors and disallow
-	// len(keys) < len(values) which is supported by generateSelector.
+	// we don't need this check, but we added it to produce more informative errors
+	// and disallow len(keys) < len(values) which is supported by generateSelector.
 	if len(keys) != len(values) {
 		panic("The number of keys and values must be equal")
 	}
 	return dotProduct(api, values, generateIndicators(api, false, queryKey, keys))
 }
 
-// Mux is an n to 1 multiplexer: out = inputs[sel]. In other words, it selects exactly one of its
-// inputs based on sel. The index of inputs starts from zero.
+// Mux is an n to 1 multiplexer: out = inputs[sel]. In other words, it selects
+// exactly one of its inputs based on sel. The index of inputs starts from zero.
 //
-// sel needs to be between 0 and n - 1 (inclusive), where n is the number of inputs, otherwise the proof will fail.
+// sel needs to be between 0 and n - 1 (inclusive), where n is the number of
+// inputs, otherwise the proof will fail.
 func Mux(api frontend.API, sel frontend.Variable, inputs ...frontend.Variable) frontend.Variable {
 	if log := math.Log2(float64(len(inputs))); log == math.Trunc(log) {
 		selBits := bits.ToBinary(api, sel, bits.WithNbDigits(int(log)))
@@ -90,8 +94,9 @@ func Decoder(api frontend.API, n int, sel frontend.Variable) []frontend.Variable
 	return generateIndicators(api, true, sel, make([]frontend.Variable, n))
 }
 
-// generateSelector generates a circuit for a multiplexer or an associative array (map). If wantMux is true, a
-// multiplexer is generated and keys are ignored. If wantMux is false, a map is generated, and we must have
+// generateSelector generates a circuit for a multiplexer or an associative
+// array (map). If wantMux is true, a multiplexer is generated and keys are
+// ignored. If wantMux is false, a map is generated, and we must have
 // len(keys) <= len(values), or it panics.
 func generateIndicators(api frontend.API, wantMux bool, sel frontend.Variable,
 	keys []frontend.Variable) []frontend.Variable {
@@ -119,8 +124,8 @@ func generateIndicators(api frontend.API, wantMux bool, sel frontend.Variable,
 		}
 		indicatorsSum = api.Add(indicatorsSum, indicators[i])
 	}
-	// We need to check that the indicator of the selected input is exactly 1. We used a sum constraint, because usually
-	// it is cheap.
+	// We need to check that the indicator of the selected input is exactly 1. We
+	// used a sum constraint, because usually it is cheap.
 	api.AssertIsEqual(indicatorsSum, 1)
 	return indicators
 }
@@ -139,8 +144,8 @@ func dotProduct(api frontend.API, a, b []frontend.Variable) frontend.Variable {
 func muxIndicators(_ *big.Int, inputs []*big.Int, results []*big.Int) error {
 	sel := inputs[0]
 	for i := 0; i < len(results); i++ {
-		// i is an int which can be int32 or int64. We convert i to int64 then to bigInt, which is safe. We should
-		// not convert sel to int64.
+		// i is an int which can be int32 or int64. We convert i to int64 then to
+		// bigInt, which is safe. We should not convert sel to int64.
 		if sel.Cmp(big.NewInt(int64(i))) == 0 {
 			results[i].SetUint64(1)
 		} else {
