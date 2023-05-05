f0b797b4d7bcc124d961e4108edc52a0d53829d8
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2023-05-05 17:21:47 -0500

    fix: no commit test passes

--- backend/plonk/bn254/verify.go
+++ backend/plonk/bn254/verify.go
@@ -124,9 +124,9 @@ func Verify(proof *Proof, vk *VerifyingKey, publicWitness fr.Vector) error {
 			den.Sub(&zeta, &wPowI) // ζ-wⁱ
 
 			lagrange.SetOne().
-				Sub(&zeta, &lagrange).       // ζ-1
-				Mul(&lagrange, &wPowI).      // wⁱ(ζ-1)
-				Div(&lagrange, &den).        // wⁱ(ζ-1)/(ζ-wⁱ)
+				Sub(&zeta, &lagrange). // ζ-1
+				Mul(&lagrange, &wPowI). // wⁱ(ζ-1)
+				Div(&lagrange, &den). // wⁱ(ζ-1)/(ζ-wⁱ)
 				Mul(&lagrange, &lagrangeOne) // wⁱ/n (ζⁿ-1)/(ζ-wⁱ)
 
 			xiLi.Mul(&lagrange, &hashRes[0])
@@ -146,7 +146,7 @@ func Verify(proof *Proof, vk *VerifyingKey, publicWitness fr.Vector) error {
 	o := proof.BatchedProof.ClaimedValues[4]
 	s1 := proof.BatchedProof.ClaimedValues[5]
 	s2 := proof.BatchedProof.ClaimedValues[6]
-	qC := proof.BatchedProof.ClaimedValues[7]
+	qC := proof.BatchedProof.ClaimedValues[7:]
 
 	_s1.Mul(&s1, &beta).Add(&_s1, &l).Add(&_s1, &gamma) // (l(ζ)+β*s1(ζ)+γ)
 	_s2.Mul(&s2, &beta).Add(&_s2, &r).Add(&_s2, &gamma) // (r(ζ)+β*s2(ζ)+γ)
@@ -161,8 +161,8 @@ func Verify(proof *Proof, vk *VerifyingKey, publicWitness fr.Vector) error {
 		Mul(&alphaSquareLagrange, &alpha) // α²*L₁(ζ)
 
 	linearizedPolynomialZeta.
-		Add(&linearizedPolynomialZeta, &pi).                 // linearizedpolynomial + pi(zeta)
-		Add(&linearizedPolynomialZeta, &_s1).                // linearizedpolynomial+pi(zeta)+α*(Z(μζ))*(l(ζ)+s1(ζ)+γ)*(r(ζ)+s2(ζ)+γ)*(o(ζ)+γ)
+		Add(&linearizedPolynomialZeta, &pi). // linearizedpolynomial + pi(zeta)
+		Add(&linearizedPolynomialZeta, &_s1). // linearizedpolynomial+pi(zeta)+α*(Z(μζ))*(l(ζ)+s1(ζ)+γ)*(r(ζ)+s2(ζ)+γ)*(o(ζ)+γ)
 		Sub(&linearizedPolynomialZeta, &alphaSquareLagrange) // linearizedpolynomial+pi(zeta)+α*(Z(μζ))*(l(ζ)+s1(ζ)+γ)*(r(ζ)+s2(ζ)+γ)*(o(ζ)+γ)-α²*L₁(ζ)
 
 	// Compute H(ζ) using the previous result: H(ζ) = prev_result/(ζⁿ-1)
@@ -220,10 +220,10 @@ func Verify(proof *Proof, vk *VerifyingKey, publicWitness fr.Vector) error {
 		vk.S[2], proof.Z, // second & third part
 	)
 
-	scalars := []fr.Element{
-		l, r, rl, o, one /* TODO Perf @Tabaie Consider just adding Qk instead */, qC, // first part
+	scalars := append(qC,
+		l, r, rl, o, one /* TODO Perf @Tabaie Consider just adding Qk instead */, // first part
 		_s1, _s2, // second & third part
-	}
+	)
 	if _, err := linearizedPolynomialDigest.MultiExp(points, scalars, ecc.MultiExpConfig{}); err != nil {
 		return err
 	}
