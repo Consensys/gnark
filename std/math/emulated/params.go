aef4a328d3b643cb36dcdcffd6dc1e0d9c1eb716
Ivo Kubjas <ivo.kubjas@consensys.net>
2023-05-18 12:47:35 +0200

    docs: expand FieldParams doc

--- std/math/emulated/params.go
+++ std/math/emulated/params.go
@@ -6,7 +6,17 @@ import (
 	"github.com/consensys/gnark/std/math/emulated/emulationparams"
 )
 
-// FieldParams describes the emulated field characteristics
+// FieldParams describes the emulated field characteristics. For a list of
+// included built-in emulation params refer to the [emulationparams] package.
+// For backwards compatibility, the current package contains the following
+// parameters:
+//   - [Goldilocks]
+//   - [Secp256k1Fp] and [Secp256k1Fr]
+//   - [BN254Fp] and [BN254Fr]
+//   - [BLS12377Fp]
+//   - [BLS12381Fp] and [BLS12381Fr]
+//
+// Any new parameters will be included in [emulationparams] package, not here.
 type FieldParams interface {
 	NbLimbs() uint     // number of limbs to represent field element
 	BitsPerLimb() uint // number of bits per limb. Top limb may contain less than limbSize bits.
