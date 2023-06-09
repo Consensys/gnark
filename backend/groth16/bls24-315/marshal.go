ede869d0ff77aa8d1bfdc9444ab7b67bd9d19204
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2023-06-09 12:59:40 -0500

    revert: special case for empty slice

--- backend/groth16/bls24-315/marshal.go
+++ backend/groth16/bls24-315/marshal.go
@@ -357,9 +357,5 @@ func (pk *ProvingKey) readFrom(r io.Reader, decOptions ...func(*curve.Decoder)) 
 		}
 	}
 
-	if pk.G1.Z == nil {
-		pk.G1.Z = []curve.G1Affine{}
-	}
-
 	return n + dec.BytesRead(), nil
 }
