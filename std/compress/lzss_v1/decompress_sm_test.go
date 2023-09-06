c12eac11fed7d228a50ba0c24e343a59ed9f8bc2
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2023-09-06 13:26:01 -0500

    test: more, failing "8zerosAfterNonzero"

--- std/compress/lzss_v1/decompress_sm_test.go
+++ std/compress/lzss_v1/decompress_sm_test.go
@@ -2,6 +2,7 @@ package lzss_v1
 
 import (
 	"github.com/stretchr/testify/require"
+	"os"
 	"testing"
 )
 
@@ -40,7 +41,41 @@ func TestTwoConsecutiveBackrefs(t *testing.T) {
 }
 func Test300ZerosSm(t *testing.T) { // probably won't happen in our calldata
 	testCompressionRoundTripSm(t, 1, make([]byte, 300))
-	//testCompressionRoundTripSm(t, 2, make([]byte, 300))
+	testCompressionRoundTripSm(t, 2, make([]byte, 300))
+}
+
+func TestNoCompressionSm(t *testing.T) {
+	testCompressionRoundTripSm(t, 1, []byte{'h', 'i'})
+	testCompressionRoundTripSm(t, 2, []byte{'h', 'i'})
+}
+
+func Test8ZerosAfterNonzeroSm(t *testing.T) { // probably won't happen in our calldata
+	testCompressionRoundTripSm(t, 1, append([]byte{1}, make([]byte, 8)...))
+	testCompressionRoundTripSm(t, 2, append([]byte{1}, make([]byte, 8)...))
+}
+
+func Test300ZerosAfterNonzeroSm(t *testing.T) { // probably won't happen in our calldata
+	testCompressionRoundTripSm(t, 1, append([]byte{'h', 'i'}, make([]byte, 300)...))
+	testCompressionRoundTripSm(t, 2, append([]byte{'h', 'i'}, make([]byte, 300)...))
+}
+
+func TestRepeatedNonzeroSm(t *testing.T) {
+	testCompressionRoundTripSm(t, 1, []byte{'h', 'i', 'h', 'i', 'h', 'i'})
+	testCompressionRoundTripSm(t, 2, []byte{'h', 'i', 'h', 'i', 'h', 'i'})
+}
+
+func TestCalldataSm(t *testing.T) {
+	t.Parallel()
+	folders := []string{
+		"3c2943",
+	}
+	for _, folder := range folders {
+		d, err := os.ReadFile("../" + folder + "/data.bin")
+		require.NoError(t, err)
+		t.Run(folder, func(t *testing.T) {
+			testCompressionRoundTripSm(t, 2, d)
+		})
+	}
 }
 
 func testCompressionRoundTripSm(t *testing.T, nbBytesOffset uint, d []byte) {
