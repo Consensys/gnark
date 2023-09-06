3dbeef8b1268d26438f82f81d66e94c8cf02b592
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2023-09-06 14:08:59 -0500

    test: twoBackrefsAfterNonzero fails

--- std/compress/lzss_v1/decompress_sm_test.go
+++ std/compress/lzss_v1/decompress_sm_test.go
@@ -22,22 +22,7 @@ func Test8ZerosSm(t *testing.T) {
 }
 
 func TestTwoConsecutiveBackrefs(t *testing.T) {
-	c := make([]byte, 9)
-	d := c[:2]
-	dBack := make([]byte, 4)
-	settings := Settings{
-		BackRefSettings: BackRefSettings{
-			NbBytesAddress: 1,
-			NbBytesLength:  1,
-			Symbol:         0,
-		},
-		Logger:   nil,
-		LogHeads: new([]LogHeads),
-	}
-
-	dLen, err := decompressStateMachine(c, 6, dBack, settings)
-	require.NoError(t, err)
-	require.Equal(t, d, dBack[:dLen])
+	testDecompressionSm(t, 1, make([]byte, 6), []byte{0, 0})
 }
 func Test300ZerosSm(t *testing.T) { // probably won't happen in our calldata
 	testCompressionRoundTripSm(t, 1, make([]byte, 300))
@@ -62,6 +47,10 @@ func TestTwoZerosAfterNonzeroSm(t *testing.T) {
 func Test8ZerosAfterNonzeroSm(t *testing.T) {
 	testCompressionRoundTripSm(t, 1, append([]byte{1}, make([]byte, 8)...))
 	testCompressionRoundTripSm(t, 2, append([]byte{1}, make([]byte, 8)...))
+}
+
+func TestTwoBackrefsAfterNonzero(t *testing.T) {
+	testDecompressionSm(t, 1, []byte{1, 0, 1, 0, 0, 0, 0}, []byte{1, 0, 0})
 }
 
 func Test257ZerosAfterNonzeroSm(t *testing.T) { // probably won't happen in our calldata
@@ -107,8 +96,21 @@ func testCompressionRoundTripSm(t *testing.T, nbBytesOffset uint, d []byte) {
 	}
 
 	c, err := Compress(d, settings)
-	c = append(c, make([]byte, 2*len(c))...)
 	require.NoError(t, err)
+	testDecompressionSm(t, nbBytesOffset, c, d)
+}
+
+func testDecompressionSm(t *testing.T, nbBytesOffset uint, c []byte, d []byte) {
+	settings := Settings{
+		BackRefSettings: BackRefSettings{
+			NbBytesAddress: nbBytesOffset,
+			NbBytesLength:  1,
+			Symbol:         0,
+		},
+	}
+
+	c = append(c, make([]byte, 2*len(c))...)
+
 	dBack := make([]byte, len(d)*2)
 
 	T := func(padCoeff int) {
