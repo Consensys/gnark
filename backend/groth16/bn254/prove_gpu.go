d7d3218d80eee90dbc0a0f0953c17c4a5ac4b551
Jeremy Felder <jeremy.felder1@gmail.com>
2023-09-26 16:00:29 +0300

    Fix order of MSMs

--- backend/groth16/bn254/prove_gpu.go
+++ backend/groth16/bn254/prove_gpu.go
@@ -277,9 +277,9 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...b
 	<-chHDone
 
 	// schedule our proof part computations
-	computeKRS()
 	computeAR1()
 	computeBS1()
+	computeKRS()
 	if err := computeBS2(); err != nil {
 		return nil, err
 	}
