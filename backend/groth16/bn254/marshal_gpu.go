7aec04473e661f8e28693db400bff292cec8a455
Jeremy Felder <jeremy.felder1@gmail.com>
2023-09-27 12:37:21 +0300

    Update gpu files with changes from upstream

--- backend/groth16/bn254/marshal_gpu.go
+++ backend/groth16/bn254/marshal_gpu.go
@@ -58,6 +58,13 @@ func (proof *Proof) writeTo(w io.Writer, raw bool) (int64, error) {
 	if err := enc.Encode(&proof.Krs); err != nil {
 		return enc.BytesWritten(), err
 	}
+	if err := enc.Encode(proof.Commitments); err != nil {
+		return enc.BytesWritten(), err
+	}
+	if err := enc.Encode(&proof.CommitmentPok); err != nil {
+		return enc.BytesWritten(), err
+	}
+
 	return enc.BytesWritten(), nil
 }
 
@@ -74,6 +81,12 @@ func (proof *Proof) ReadFrom(r io.Reader) (n int64, err error) {
 		return dec.BytesRead(), err
 	}
 	if err := dec.Decode(&proof.Krs); err != nil {
+		return dec.BytesRead(), err
+	}
+	if err := dec.Decode(&proof.Commitments); err != nil {
+		return dec.BytesRead(), err
+	}
+	if err := dec.Decode(&proof.CommitmentPok); err != nil {
 		return dec.BytesRead(), err
 	}
 
