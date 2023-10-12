a4606498b04e8915a3c37883476d06459807d017
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2023-10-12 16:01:20 -0500

    chore: clean up comments and prints

--- std/compress/lzss_v1/snark.go
+++ std/compress/lzss_v1/snark.go
@@ -56,11 +56,9 @@ func Decompress(api frontend.API, c []frontend.Variable, d []frontend.Variable, 
 
 	for outI := range d {
 
-		api.Println("outI", outI, "inI", inI)
 		curr := readC(inI, 1)[0]
 		brLen := api.Add(readLittleEndian(api, readC(api.Add(inI, 1+settings.NbBytesAddress), int(settings.NbBytesLength))), 1)
 		brOffsetMinusOne := brOffsetTable.Lookup(inI)[0]
-		api.Println("curr", curr, "brLen", brLen, "brOffsetMinusOne", brOffsetMinusOne)
 
 		isSymb := api.IsZero(api.Sub(curr, int(settings.Symbol)))
 
@@ -68,21 +66,17 @@ func Decompress(api frontend.API, c []frontend.Variable, d []frontend.Variable, 
 		copyI = api.Sub(outI+brLengthRange-1, brOffsetMinusOne)
 		copyLen = api.Select(copyLen01, api.Mul(isSymb, brLen), api.Sub(copyLen, 1))
 		copyLen01 = isBit(copyLen)
-		// copying = copyLen01 ? copyLen==1 : 1
+		// copying = copyLen01 ? copyLen==1 : 1			either from previous iterations or starting a new copy
 		// copying = copyLen01 ? copyLen : 1
-		//copying := api.Add(1, api.MulAcc(api.Neg(copyLen01), copyLen01, copyLen)) // either from previous iterations or starting a new copy
 		copying := api.(*scs.Builder).NewCombination(copyLen01, copyLen, -1, 0, 1, 1)
 
 		// TODO Remove this if populating the entire negative address space
 		copyI = api.Select(copying, copyI, 0) // to keep it in range in case we read nonsensical backref data when not copying
 
-		api.Println("copyI", copyI)
 		toCopy := dTable.Lookup(copyI)[0]
-		api.Println("toCopy", toCopy)
 
 		// write to output
-
-		d[outI] = api.MulAcc(curr, copying, toCopy) // TODO full-on ite for the case where symb != 0
+		d[outI] = api.MulAcc(curr, copying, toCopy)
 		// WARNING: curr modified by MulAcc
 		dTable.Insert(d[outI])
 
@@ -92,23 +86,18 @@ func Decompress(api frontend.API, c []frontend.Variable, d []frontend.Variable, 
 			// inIDelta = - copying + copying*copyLen01*(backrefCodeLen) + 1
 			inIDelta := api.(*scs.Builder).NewCombination(copying, copyLen01, -1, 0, 1+int(settings.NbBytesAddress+settings.NbBytesLength), 1)
 
-			api.Println("copying", copying, "copyLen01", copyLen01, "inIDelta", inIDelta)
-
 			// TODO Try removing this check and requiring the user to pad the input with nonzeros
 			// TODO Change inner to mulacc once https://github.com/Consensys/gnark/pull/859 is merged
-			// inI = api.Add(inI, api.Mul(inIDelta, api.Sub(1, eof)))
+			// inI = inI + inIDelta * (1 - eof)
 			if eof == 0 {
 				inI = api.Add(inI, inIDelta)
 			} else {
 				inI = api.Add(inI, api.(*scs.Builder).NewCombination(inIDelta, eof, 1, 0, -1, 0)) // if eof, stay put
 			}
 
-			// TODO Try "isNonZero" for eofNow and eof
 			eofNow := api.IsZero(api.Sub(inI, cLength))
-			api.Println("eofNow", eofNow, "eof", eof)
 
 			dLength = api.Add(dLength, api.Mul(api.Sub(eofNow, eof), outI+1)) // if eof, don't advance dLength
-			api.Println("dLength", dLength)
 			eof = eofNow
 		}()
 	}
