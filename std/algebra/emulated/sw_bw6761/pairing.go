0978fbce79f465012c4f726020cdd42d31d10e7a
Youssef El Housni <youssef.housni21@gmail.com>
2023-10-23 16:18:07 -0400

    feat: add fixed pairing for bw6-761

--- std/algebra/emulated/sw_bw6761/pairing.go
+++ std/algebra/emulated/sw_bw6761/pairing.go
@@ -15,6 +15,7 @@ type Pairing struct {
 	api frontend.API
 	*fields_bw6761.Ext6
 	curveF *emulated.Field[emulated.BW6761Fp]
+	lines  [4][189]emulated.Element[emulated.BW6761Fp]
 }
 
 type GTEl = fields_bw6761.E6
@@ -43,6 +44,7 @@ func NewPairing(api frontend.API) (*Pairing, error) {
 		api:    api,
 		Ext6:   fields_bw6761.NewExt6(api),
 		curveF: ba,
+		lines:  getPrecomputedLines(),
 	}, nil
 }
 
@@ -503,4 +505,267 @@ func (pr Pairing) tangentCompute(p1 *G2Affine) *lineEvaluation {
 
 	return &line
 
+}
+
+// ----------------------------
+//	  Fixed-argument pairing
+// ----------------------------
+//
+// The second argument Q is g2 the fixed canonical generator of G2.
+//
+// g2.X = 0x110133241d9b816c852a82e69d660f9d61053aac5a7115f4c06201013890f6d26b41c5dab3da268734ec3f1f09feb58c5bbcae9ac70e7c7963317a300e1b6bace6948cb3cd208d700e96efbc2ad54b06410cf4fe1bf995ba830c194cd025f1c
+// g2.Y = 0x17c3357761369f8179eb10e4b6d2dc26b7cf9acec2181c81a78e2753ffe3160a1d86c80b95a59c94c97eb733293fef64f293dbd2c712b88906c170ffa823003ea96fcd504affc758aa2d3a3c5a02a591ec0594f9eac689eb70a16728c73b61
+
+// MillerLoopFixed computes the single Miller loop
+// fᵢ_{x₀+1+λ(x₀³-x₀²-x₀),Qᵢ}(Pᵢ), where g2 is fixed.
+func (pr Pairing) MillerLoopFixedQ(P *G1Affine) (*GTEl, error) {
+
+	// P and Q are supposed to be on G1 and G2 respectively of prime order r.
+	// The point (x,0) is of order 2. But this function does not check
+	// subgroup membership.
+	yInv := pr.curveF.Inverse(&P.Y)
+	xNegOverY := pr.curveF.MulMod(&P.X, yInv)
+	xNegOverY = pr.curveF.Neg(xNegOverY)
+
+	// f_{x₀+1+λ(x₀³-x₀²-x₀),Q}(P)
+	result := pr.Ext6.One()
+
+	// i = 188, separately to avoid an E6 Square
+	// (Square(res) = 1² = 1)
+	// and avoid MulBy014 (res × ℓ)
+	// (assign line to res)
+	result = &fields_bw6761.E6{
+		B0: fields_bw6761.E3{
+			A0: *pr.curveF.MulMod(&pr.lines[1][188], yInv),
+			A1: *pr.curveF.MulMod(&pr.lines[0][188], xNegOverY),
+			A2: result.B0.A2,
+		},
+		B1: fields_bw6761.E3{
+			A0: result.B1.A0,
+			A1: *pr.curveF.One(),
+			A2: result.B1.A2,
+		},
+	}
+
+	for i := 187; i >= 1; i-- {
+		// mutualize the square among n Miller loops
+		// (∏ᵢfᵢ)²
+		result = pr.Square(result)
+
+		result = pr.MulBy014(result,
+			pr.curveF.MulMod(&pr.lines[1][i], yInv),
+			pr.curveF.MulMod(&pr.lines[0][i], xNegOverY),
+		)
+
+		if loopCounter2[i]*3+loopCounter1[i] != 0 {
+			result = pr.MulBy014(result,
+				pr.curveF.MulMod(&pr.lines[3][i], yInv),
+				pr.curveF.MulMod(&pr.lines[2][i], xNegOverY),
+			)
+		}
+	}
+
+	// i = 0
+	result = pr.Square(result)
+	result = pr.MulBy014(result,
+		pr.curveF.MulMod(&pr.lines[1][0], yInv),
+		pr.curveF.MulMod(&pr.lines[0][0], xNegOverY),
+	)
+
+	return result, nil
+}
+
+// DoubleMillerLoopFixedQ computes the double Miller loop
+// fᵢ_{u,g2}(T) * fᵢ_{u,Q}(P), where g2 is fixed.
+func (pr Pairing) DoubleMillerLoopFixedQ(P [2]*G1Affine, Q *G2Affine) (*GTEl, error) {
+
+	// P and Q are supposed to be on G1 and G2 respectively of prime order r.
+	// The point (x,0) is of order 2. But this function does not check
+	// subgroup membership.
+	yInv := make([]*emulated.Element[emulated.BW6761Fp], 2)
+	xNegOverY := make([]*emulated.Element[emulated.BW6761Fp], 2)
+	yInv[1] = pr.curveF.Inverse(&P[1].Y)
+	xNegOverY[1] = pr.curveF.MulMod(&P[1].X, yInv[1])
+	xNegOverY[1] = pr.curveF.Neg(xNegOverY[1])
+	yInv[0] = pr.curveF.Inverse(&P[0].Y)
+	xNegOverY[0] = pr.curveF.MulMod(&P[0].X, yInv[0])
+	xNegOverY[0] = pr.curveF.Neg(xNegOverY[0])
+	// negQ = -Q = (x, -y)
+	negQ := &G1Affine{X: Q.X, Y: *pr.curveF.Neg(&Q.Y)}
+	// imQ = (w*x, -y)
+	imQ := &G1Affine{X: *pr.curveF.MulMod(&Q.X, &thirdRootOne), Y: negQ.Y}
+	// imQneg = (w*x, y)
+	imQneg := &G1Affine{X: imQ.X, Y: Q.Y}
+	// point accumulator initialized to imQ
+	accQ := imQ
+
+	// f_{x₀+1+λ(x₀³-x₀²-x₀),Q}(P[1])
+	result := pr.Ext6.One()
+	var l0, l1 *lineEvaluation
+
+	for i := 188; i >= 1; i-- {
+		// mutualize the square among n Miller loops
+		// (∏ᵢfᵢ)²
+		result = pr.Square(result)
+
+		j := loopCounter2[i]*3 + loopCounter1[i]
+
+		switch j {
+		// cases -4, -2, 2 and 4 are omitted as they do not occur given the
+		// static loop counters.
+		case -3:
+			accQ, l0, l1 = pr.doubleAndAddStep(accQ, imQneg)
+			l0 = &lineEvaluation{
+				R0: *pr.curveF.MulMod(&l0.R0, xNegOverY[1]),
+				R1: *pr.curveF.MulMod(&l0.R1, yInv[0]),
+			}
+			result = pr.MulBy014(result, &l0.R1, &l0.R0)
+			l1 = &lineEvaluation{
+				R0: *pr.curveF.MulMod(&l1.R0, xNegOverY[1]),
+				R1: *pr.curveF.MulMod(&l1.R1, yInv[0]),
+			}
+			result = pr.MulBy014(result, &l1.R1, &l1.R0)
+
+			// fixed-argument
+			result = pr.MulBy014(result,
+				pr.curveF.MulMod(&pr.lines[1][i], yInv[0]),
+				pr.curveF.MulMod(&pr.lines[0][i], xNegOverY[0]),
+			)
+			result = pr.MulBy014(result,
+				pr.curveF.MulMod(&pr.lines[3][i], yInv[0]),
+				pr.curveF.MulMod(&pr.lines[2][i], xNegOverY[0]),
+			)
+		case -1:
+			accQ, l0, l1 = pr.doubleAndAddStep(accQ, negQ)
+			l0 = &lineEvaluation{
+				R0: *pr.curveF.MulMod(&l0.R0, xNegOverY[1]),
+				R1: *pr.curveF.MulMod(&l0.R1, yInv[0]),
+			}
+			result = pr.MulBy014(result, &l0.R1, &l0.R0)
+			l1 = &lineEvaluation{
+				R0: *pr.curveF.MulMod(&l1.R0, xNegOverY[1]),
+				R1: *pr.curveF.MulMod(&l1.R1, yInv[0]),
+			}
+			result = pr.MulBy014(result, &l1.R1, &l1.R0)
+
+			// fixed-argument
+			result = pr.MulBy014(result,
+				pr.curveF.MulMod(&pr.lines[1][i], yInv[0]),
+				pr.curveF.MulMod(&pr.lines[0][i], xNegOverY[0]),
+			)
+			result = pr.MulBy014(result,
+				pr.curveF.MulMod(&pr.lines[3][i], yInv[0]),
+				pr.curveF.MulMod(&pr.lines[2][i], xNegOverY[0]),
+			)
+		case 0:
+			accQ, l0 = pr.doubleStep(accQ)
+			l0 = &lineEvaluation{
+				R0: *pr.curveF.MulMod(&l0.R0, xNegOverY[1]),
+				R1: *pr.curveF.MulMod(&l0.R1, yInv[0]),
+			}
+			result = pr.MulBy014(result, &l0.R1, &l0.R0)
+
+			// fixed-argument
+			result = pr.MulBy014(result,
+				pr.curveF.MulMod(&pr.lines[1][i], yInv[0]),
+				pr.curveF.MulMod(&pr.lines[0][i], xNegOverY[0]),
+			)
+		case 1:
+			accQ, l0, l1 = pr.doubleAndAddStep(accQ, Q)
+			l0 = &lineEvaluation{
+				R0: *pr.curveF.MulMod(&l0.R0, xNegOverY[1]),
+				R1: *pr.curveF.MulMod(&l0.R1, yInv[0]),
+			}
+			result = pr.MulBy014(result, &l0.R1, &l0.R0)
+			l1 = &lineEvaluation{
+				R0: *pr.curveF.MulMod(&l1.R0, xNegOverY[1]),
+				R1: *pr.curveF.MulMod(&l1.R1, yInv[0]),
+			}
+			result = pr.MulBy014(result, &l1.R1, &l1.R0)
+
+			// fixed-argument
+			result = pr.MulBy014(result,
+				pr.curveF.MulMod(&pr.lines[1][i], yInv[0]),
+				pr.curveF.MulMod(&pr.lines[0][i], xNegOverY[0]),
+			)
+			result = pr.MulBy014(result,
+				pr.curveF.MulMod(&pr.lines[3][i], yInv[0]),
+				pr.curveF.MulMod(&pr.lines[2][i], xNegOverY[0]),
+			)
+		case 3:
+			accQ, l0, l1 = pr.doubleAndAddStep(accQ, imQ)
+			l0 = &lineEvaluation{
+				R0: *pr.curveF.MulMod(&l0.R0, xNegOverY[1]),
+				R1: *pr.curveF.MulMod(&l0.R1, yInv[0]),
+			}
+			result = pr.MulBy014(result, &l0.R1, &l0.R0)
+			l1 = &lineEvaluation{
+				R0: *pr.curveF.MulMod(&l1.R0, xNegOverY[1]),
+				R1: *pr.curveF.MulMod(&l1.R1, yInv[0]),
+			}
+			result = pr.MulBy014(result, &l1.R1, &l1.R0)
+
+			// fixed-argument
+			result = pr.MulBy014(result,
+				pr.curveF.MulMod(&pr.lines[1][i], yInv[0]),
+				pr.curveF.MulMod(&pr.lines[0][i], xNegOverY[0]),
+			)
+			result = pr.MulBy014(result,
+				pr.curveF.MulMod(&pr.lines[3][i], yInv[0]),
+				pr.curveF.MulMod(&pr.lines[2][i], xNegOverY[0]),
+			)
+		default:
+			return nil, errors.New("invalid loopCounter")
+		}
+	}
+
+	// i = 0, j = -3
+	// The resulting accumulator point is the infinity point because
+	// [(x₀+1) + λ(x₀³-x₀²-x₀)]Q = [3(x₀-1)² ⋅ r]Q = ∞
+	// since we're using affine coordinates, the addStep in the last iteration
+	// (j=-3) will fail as the slope of a vertical line in indefinite. But in
+	// projective coordinates, vertinal lines meet at (0:1:0) so the result
+	// should be unchanged if we ommit the addStep in this case. Moreover we
+	// just compute before the tangent line and not the full doubleStep as we
+	// only care about the Miller loop result in Fp6 and not the point itself.
+	result = pr.Square(result)
+	l0 = pr.tangentCompute(accQ)
+	l0 = &lineEvaluation{
+		R0: *pr.curveF.MulMod(&l0.R0, xNegOverY[1]),
+		R1: *pr.curveF.MulMod(&l0.R1, yInv[0]),
+	}
+	result = pr.MulBy014(result, &l0.R1, &l0.R0)
+	// fixed-argument
+	result = pr.MulBy014(result,
+		pr.curveF.MulMod(&pr.lines[1][0], yInv[0]),
+		pr.curveF.MulMod(&pr.lines[0][0], xNegOverY[0]),
+	)
+
+	return result, nil
+}
+
+// PairFixedQ calculates the reduced pairing for a set of points
+// e(P, g2), where g2 is fixed.
+//
+// This function doesn't check that the inputs are in the correct subgroups.
+func (pr Pairing) PairFixedQ(P *G1Affine) (*GTEl, error) {
+	res, err := pr.MillerLoopFixedQ(P)
+	if err != nil {
+		return nil, fmt.Errorf("miller loop: %w", err)
+	}
+	res = pr.FinalExponentiation(res)
+	return res, nil
+}
+
+// DoublePairFixedQ calculates the reduced pairing for a set of points
+// e(P, Q) * e(T, g2), where g2 is fixed.
+//
+// This function doesn't check that the inputs are in the correct subgroups.
+func (pr Pairing) DoublePairFixedQ(P [2]*G1Affine, Q *G2Affine) (*GTEl, error) {
+	res, err := pr.DoubleMillerLoopFixedQ(P, Q)
+	if err != nil {
+		return nil, fmt.Errorf("double miller loop: %w", err)
+	}
+	res = pr.FinalExponentiation(res)
+	return res, nil
 }
