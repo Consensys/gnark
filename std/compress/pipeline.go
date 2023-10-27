5b18645afbacc81cfb0116d77396f62769f10fc7
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2023-10-27 12:02:17 -0500

    feat: r/w num, (un)marshal for stream

--- std/compress/pipeline.go
+++ std/compress/pipeline.go
@@ -1,5 +1,10 @@
 package compress
 
+import (
+	"bytes"
+	"github.com/icza/bitio"
+)
+
 // Streams and pipelines are inefficient data structures used for easy experimentation with compression algorithms.
 // They make it easy to swap modules in and out.
 
@@ -8,11 +13,11 @@ type Stream struct {
 	NbSymbs int
 }
 
-func (s Stream) Len() int {
+func (s *Stream) Len() int {
 	return len(s.D)
 }
 
-func (s Stream) RunLen(i int) int {
+func (s *Stream) RunLen(i int) int {
 	runLen := 1
 	for i+runLen < len(s.D) && s.D[i+runLen] == 0 {
 		runLen++
@@ -20,7 +25,7 @@ func (s Stream) RunLen(i int) int {
 	return runLen
 }
 
-func (s Stream) At(i int) int {
+func (s *Stream) At(i int) int {
 	return s.D[i]
 }
 
@@ -39,4 +44,90 @@ func (pipeline Pipeline) Run(in Stream) Stream {
 		in = f(in)
 	}
 	return in
+}
+
+func (s *Stream) WriteNum(r int, nbWords int) *Stream {
+	for i := 0; i < nbWords; i++ {
+		s.D = append(s.D, r%s.NbSymbs)
+		r /= s.NbSymbs
+	}
+	if r != 0 {
+		panic("overflow")
+	}
+	return s
+}
+
+func (s *Stream) ReadNum(start, nbWords int) int {
+	res := 0
+	for j := nbWords - 1; j >= 0; j-- {
+		res *= s.NbSymbs
+		res += s.D[start+j]
+	}
+	return res
+}
+
+func bitLen(n int) int {
+	bitLen := 0
+	for 1<<bitLen < n {
+		bitLen++
+	}
+	return bitLen
+}
+
+func (s *Stream) Marshal() []byte {
+	wordLen := bitLen(s.NbSymbs)
+
+	nbBytes := (len(s.D)*wordLen + 7) / 8
+	encodeLen := false
+	if s.NbSymbs <= 128 {
+		nbBytes++
+		encodeLen = true
+	}
+	bb := bytes.NewBuffer(make([]byte, 0, nbBytes))
+
+	w := bitio.NewWriter(bb)
+	for i := range s.D {
+		if err := w.WriteBits(uint64(s.D[i]), uint8(wordLen)); err != nil {
+			panic(err)
+		}
+	}
+	if err := w.Close(); err != nil {
+		panic(err)
+	}
+
+	if encodeLen {
+		nbWordsInLastByte := len(s.D) - ((nbBytes-2)*8+wordLen-1)/wordLen
+		bb.WriteByte(byte(nbWordsInLastByte))
+	}
+
+	return bb.Bytes()
+}
+
+func (s *Stream) Unmarshal(b []byte) *Stream {
+	wordLen := bitLen(s.NbSymbs)
+
+	var nbWords int
+	if s.NbSymbs <= 128 {
+		nbWordsNotEntirelyInLastByte := ((len(b)-2)*8 + wordLen - 1) / wordLen
+		nbWords = nbWordsNotEntirelyInLastByte + int(b[len(b)-1])
+		b = b[:len(b)-1]
+	} else {
+		nbWords = (len(b) * 8) / wordLen
+	}
+
+	if cap(s.D) < nbWords {
+		s.D = make([]int, nbWords)
+	}
+	s.D = s.D[:nbWords]
+
+	r := bitio.NewReader(bytes.NewReader(b))
+	for i := range s.D {
+		if n, err := r.ReadBits(uint8(wordLen)); err != nil {
+			panic(err)
+		} else {
+			s.D[i] = int(n)
+		}
+	}
+
+	return s
 }
