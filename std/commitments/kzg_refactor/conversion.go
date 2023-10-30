addc4fd87802f1ef90a0a1a809c445fdcc6904ac
Thomas Piellard <thomas.piellard@consensys.net>
2023-10-30 17:24:55 +0100

    feat: test kzg, error Modulus method missing

---
+++ std/commitments/kzg_refactor/conversion.go
@@ -0,0 +1,204 @@
+package kzg_refactor
+
+import (
+	"fmt"
+
+	fr_bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
+	fr_bn254 "github.com/consensys/gnark-crypto/ecc/bn254/fr"
+	fr_bw6761 "github.com/consensys/gnark-crypto/ecc/bw6-761/fr"
+
+	bls12377 "github.com/consensys/gnark-crypto/ecc/bls12-377"
+	bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381"
+	bls24315 "github.com/consensys/gnark-crypto/ecc/bls24-315"
+	"github.com/consensys/gnark-crypto/ecc/bn254"
+	bw6761 "github.com/consensys/gnark-crypto/ecc/bw6-761"
+
+	kzg_bls12377 "github.com/consensys/gnark-crypto/ecc/bls12-377/kzg"
+	kzg_bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381/kzg"
+	kzg_bls24315 "github.com/consensys/gnark-crypto/ecc/bls24-315/kzg"
+	kzg_bn254 "github.com/consensys/gnark-crypto/ecc/bn254/kzg"
+	kzg_bw6761 "github.com/consensys/gnark-crypto/ecc/bw6-761/kzg"
+
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bls12381"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bw6761"
+	"github.com/consensys/gnark/std/algebra/native/sw_bls12377"
+	"github.com/consensys/gnark/std/algebra/native/sw_bls24315"
+	"github.com/consensys/gnark/std/math/emulated"
+)
+
+// ValueOfScalar initializes a scalar in a witness from a native scalar (Fr) point.
+// The scalars are always emulated.
+func ValueOfScalar[S emulated.FieldParams](scalar any) (emulated.Element[S], error) {
+	var ret emulated.Element[S]
+	switch s := any(&ret).(type) {
+	case *emulated.Element[emulated.BN254Fr]:
+		tScalar, ok := scalar.(fr_bn254.Element)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, tScalar)
+		}
+		*s = sw_bn254.NewScalar(tScalar)
+	case *emulated.Element[emulated.BW6761Fr]:
+		tScalar, ok := scalar.(fr_bw6761.Element)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, tScalar)
+		}
+		*s = sw_bw6761.NewScalar(tScalar)
+	case *emulated.Element[emulated.BLS12381Fr]:
+		tScalar, ok := scalar.(fr_bls12381.Element)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, tScalar)
+		}
+		*s = sw_bls12381.NewScalar(tScalar)
+	default:
+		return ret, fmt.Errorf("unknown type parametrization")
+	}
+	return ret, nil
+
+}
+
+// ValueOfCommitment initializes a KZG commitment witness from a native
+// commitment. It returns an error if there is a conflict between the type
+// parameters and provided native commitment type.
+func ValueOfCommitment[G1El any](cmt any) (Commitment[G1El], error) {
+	var ret Commitment[G1El]
+	switch s := any(&ret).(type) {
+	case *Commitment[sw_bn254.G1Affine]:
+		tCmt, ok := cmt.(bn254.G1Affine)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, cmt)
+		}
+		s.G1El = sw_bn254.NewG1Affine(tCmt)
+	case *Commitment[sw_bls12377.G1Affine]:
+		tCmt, ok := cmt.(bls12377.G1Affine)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, cmt)
+		}
+		s.G1El = sw_bls12377.NewG1Affine(tCmt)
+	case *Commitment[sw_bls12381.G1Affine]:
+		tCmt, ok := cmt.(bls12381.G1Affine)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, cmt)
+		}
+		s.G1El = sw_bls12381.NewG1Affine(tCmt)
+	case *Commitment[sw_bw6761.G1Affine]:
+		tCmt, ok := cmt.(bw6761.G1Affine)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, cmt)
+		}
+		s.G1El = sw_bw6761.NewG1Affine(tCmt)
+	case *Commitment[sw_bls24315.G1Affine]:
+		tCmt, ok := cmt.(bls24315.G1Affine)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, cmt)
+		}
+		s.G1El = sw_bls24315.NewG1Affine(tCmt)
+	default:
+		return ret, fmt.Errorf("unknown type parametrization")
+	}
+	return ret, nil
+}
+
+// ValueOfOpeningProof initializes an opening proof from the given proof and
+// point. It returns an error if there is a mismatch between the type parameters
+// and types of the provided point and proof.
+func ValueOfOpeningProof[S emulated.FieldParams, G1El any](proof any) (OpeningProof[S, G1El], error) {
+	var ret OpeningProof[S, G1El]
+	switch s := any(&ret).(type) {
+	case *OpeningProof[emulated.BN254Fr, sw_bn254.G1Affine]:
+		tProof, ok := proof.(kzg_bn254.OpeningProof)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, proof)
+		}
+		s.Quotient = sw_bn254.NewG1Affine(tProof.H)
+		s.ClaimedValue = sw_bn254.NewScalar(tProof.ClaimedValue)
+
+	// TODO implement BLS12377FR emulated
+	// case *OpeningProof[emulated.BLS12377FR, sw_bls12377.G1Affine]:
+	// 	tProof, ok := proof.(kzg_bls12377.OpeningProof)
+	// 	if !ok {
+	// 		return ret, fmt.Errorf("mismatching types %T %T", ret, proof)
+	// 	}
+	// 	s.Quotient = sw_bls12377.NewG1Affine(tProof.H)
+	// 	s.ClaimedValue = tProof.ClaimedValue.String()
+	case *OpeningProof[emulated.BLS12381Fr, sw_bls12381.G1Affine]:
+		tProof, ok := proof.(kzg_bls12381.OpeningProof)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, proof)
+		}
+		s.Quotient = sw_bls12381.NewG1Affine(tProof.H)
+		s.ClaimedValue = sw_bls12381.NewScalar(tProof.ClaimedValue)
+	case *OpeningProof[emulated.BW6761Fr, sw_bw6761.G1Affine]:
+		tProof, ok := proof.(kzg_bw6761.OpeningProof)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, proof)
+		}
+		s.Quotient = sw_bw6761.NewG1Affine(tProof.H)
+		s.ClaimedValue = sw_bw6761.NewScalar(tProof.ClaimedValue)
+
+	// TODO implement BLS24315FR emulated
+	// case *OpeningProof[emulated.BLS24315, sw_bls24315.G1Affine]:
+	// 	tProof, ok := proof.(kzg_bls24315.OpeningProof)
+	// 	if !ok {
+	// 		return ret, fmt.Errorf("mismatching types %T %T", ret, proof)
+	// 	}
+	// 	s.Quotient = sw_bls24315.NewG1Affine(tProof.H)
+	// 	s.ClaimedValue = tProof.ClaimedValue.String()
+
+	default:
+		return ret, fmt.Errorf("unknown type parametrization")
+	}
+	return ret, nil
+}
+
+// ValueOfVerifyingKey initializes verifying key witness from the native
+// verifying key. It returns an error if there is a mismatch between the type
+// parameters and the provided verifying key type.
+func ValueOfVerifyingKey[G1El, G2El any](vk any) (VerifyingKey[G1El, G2El], error) {
+	var ret VerifyingKey[G1El, G2El]
+	switch s := any(&ret).(type) {
+	case *VerifyingKey[sw_bn254.G1Affine, sw_bn254.G2Affine]:
+		tVk, ok := vk.(kzg_bn254.VerifyingKey)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, vk)
+		}
+		s.G1 = sw_bn254.NewG1Affine(tVk.G1)
+		s.G2[0] = sw_bn254.NewG2Affine(tVk.G2[0])
+		s.G2[1] = sw_bn254.NewG2Affine(tVk.G2[1])
+	case *VerifyingKey[sw_bls12377.G1Affine, sw_bls12377.G2Affine]:
+		tVk, ok := vk.(kzg_bls12377.VerifyingKey)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, vk)
+		}
+		s.G1 = sw_bls12377.NewG1Affine(tVk.G1)
+		s.G2[0] = sw_bls12377.NewG2Affine(tVk.G2[0])
+		s.G2[1] = sw_bls12377.NewG2Affine(tVk.G2[1])
+	case *VerifyingKey[sw_bls12381.G1Affine, sw_bls12381.G2Affine]:
+		tVk, ok := vk.(kzg_bls12381.VerifyingKey)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, vk)
+		}
+		s.G1 = sw_bls12381.NewG1Affine(tVk.G1)
+		s.G2[0] = sw_bls12381.NewG2Affine(tVk.G2[0])
+		s.G2[1] = sw_bls12381.NewG2Affine(tVk.G2[1])
+	case *VerifyingKey[sw_bw6761.G1Affine, sw_bw6761.G2Affine]:
+		tVk, ok := vk.(kzg_bw6761.VerifyingKey)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, vk)
+		}
+		s.G1 = sw_bw6761.NewG1Affine(tVk.G1)
+		s.G2[0] = sw_bw6761.NewG2Affine(tVk.G2[0])
+		s.G2[1] = sw_bw6761.NewG2Affine(tVk.G2[1])
+	case *VerifyingKey[sw_bls24315.G1Affine, sw_bls24315.G2Affine]:
+		tVk, ok := vk.(kzg_bls24315.VerifyingKey)
+		if !ok {
+			return ret, fmt.Errorf("mismatching types %T %T", ret, vk)
+		}
+		s.G1 = sw_bls24315.NewG1Affine(tVk.G1)
+		s.G2[0] = sw_bls24315.NewG2Affine(tVk.G2[0])
+		s.G2[1] = sw_bls24315.NewG2Affine(tVk.G2[1])
+	default:
+		return ret, fmt.Errorf("unknown type parametrization")
+	}
+	return ret, nil
+}
---
+++ std/commitments/kzg_refactor/verifier_test.go
@@ -0,0 +1,397 @@
+package kzg_refactor
+
+import (
+	"crypto/rand"
+	"fmt"
+	"testing"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	fr_bn254 "github.com/consensys/gnark-crypto/ecc/bn254/fr"
+	kzg_bn254 "github.com/consensys/gnark-crypto/ecc/bn254/kzg"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
+	"github.com/consensys/gnark/std/math/emulated"
+	"github.com/consensys/gnark/test"
+)
+
+const (
+	kzgSize        = 128
+	polynomialSize = 100
+)
+
+type KZGVerificationCircuit[S emulated.FieldParams, G1El, G2El, GTEl any] struct {
+	Vk     VerifyingKey[G1El, G2El]
+	Digest Commitment[G1El]
+	Proof  OpeningProof[S, G1El]
+	Point  emulated.Element[S]
+}
+
+func (c *KZGVerificationCircuit[S, G1El, G2El, GTEl]) Define(api frontend.API) error {
+	_, err := NewVerifier[S, G1El, G2El, GTEl](api)
+	if err != nil {
+		return fmt.Errorf("get pairing: %w", err)
+	}
+	// if err := verifier.CheckOpeningProof(c.Digest, c.Proof, c.Point, c.Vk); err != nil {
+	// 	return fmt.Errorf("assert proof: %w", err)
+	// }
+	return nil
+}
+
+func TestKZGVerificationEmulated(t *testing.T) {
+	assert := test.NewAssert(t)
+
+	alpha, err := rand.Int(rand.Reader, ecc.BN254.ScalarField())
+	assert.NoError(err)
+	srs, err := kzg_bn254.NewSRS(kzgSize, alpha)
+	assert.NoError(err)
+
+	f := make([]fr_bn254.Element, polynomialSize)
+	for i := range f {
+		f[i].SetRandom()
+	}
+
+	com, err := kzg_bn254.Commit(f, srs.Pk)
+	assert.NoError(err)
+
+	var point fr_bn254.Element
+	point.SetRandom()
+	proof, err := kzg_bn254.Open(f, point, srs.Pk)
+	assert.NoError(err)
+
+	if err = kzg_bn254.Verify(&com, &proof, point, srs.Vk); err != nil {
+		t.Fatal("verify proof", err)
+	}
+
+	wCmt, err := ValueOfCommitment[sw_bn254.G1Affine](com)
+	assert.NoError(err)
+	wProof, err := ValueOfOpeningProof[emulated.BN254Fr, sw_bn254.G1Affine](proof)
+	assert.NoError(err)
+	wVk, err := ValueOfVerifyingKey[sw_bn254.G1Affine, sw_bn254.G2Affine](srs.Vk)
+	assert.NoError(err)
+
+	// wPoint, err := ValueOfScalar[emulated.BN254Fr](point)
+	wPoint, err := ValueOfScalar[emulated.BN254Fr](point)
+	assert.NoError(err)
+
+	_ = KZGVerificationCircuit[emulated.BN254Fr, sw_bn254.G1Affine, sw_bn254.G2Affine, sw_bn254.GTEl]{
+		Vk:     wVk,
+		Digest: wCmt,
+		Proof:  wProof,
+		Point:  wPoint,
+	}
+	// assert.CheckCircuit(&KZGVerificationCircuit[emulated.BN254Fr, sw_bn254.G1Affine, sw_bn254.G2Affine, sw_bn254.GTEl]{}, test.WithValidAssignment(&assignment))
+}
+
+// func TestKZGVerificationEmulated2(t *testing.T) {
+// 	assert := test.NewAssert(t)
+
+// 	alpha, err := rand.Int(rand.Reader, ecc.BLS12_381.ScalarField())
+// 	assert.NoError(err)
+// 	srs, err := kzg_bls12381.NewSRS(kzgSize, alpha)
+// 	assert.NoError(err)
+
+// 	f := make([]fr_bls12381.Element, polynomialSize)
+// 	for i := range f {
+// 		f[i].SetRandom()
+// 	}
+
+// 	com, err := kzg_bls12381.Commit(f, srs.Pk)
+// 	assert.NoError(err)
+
+// 	var point fr_bls12381.Element
+// 	point.SetRandom()
+// 	proof, err := kzg_bls12381.Open(f, point, srs.Pk)
+// 	assert.NoError(err)
+
+// 	if err = kzg_bls12381.Verify(&com, &proof, point, srs.Vk); err != nil {
+// 		t.Fatal("verify proof", err)
+// 	}
+
+// 	wCmt, err := ValueOfCommitment[sw_bls12381.G1Affine](com)
+// 	assert.NoError(err)
+// 	wProof, err := ValueOfOpeningProof[sw_bls12381.Scalar, sw_bls12381.G1Affine](point, proof)
+// 	assert.NoError(err)
+// 	wVk, err := ValueOfVerifyingKey[sw_bls12381.G2Affine](srs.Vk)
+// 	assert.NoError(err)
+
+// 	assignment := KZGVerificationCircuit[sw_bls12381.Scalar, sw_bls12381.G1Affine, sw_bls12381.G2Affine, sw_bls12381.GTEl]{
+// 		VerifyingKey: wVk,
+// 		Commitment:   wCmt,
+// 		OpeningProof: wProof,
+// 	}
+// 	assert.CheckCircuit(&KZGVerificationCircuit[sw_bls12381.Scalar, sw_bls12381.G1Affine, sw_bls12381.G2Affine, sw_bls12381.GTEl]{}, test.WithValidAssignment(&assignment))
+// }
+
+// func TestKZGVerificationEmulated3(t *testing.T) {
+// 	assert := test.NewAssert(t)
+
+// 	alpha, err := rand.Int(rand.Reader, ecc.BW6_761.ScalarField())
+// 	assert.NoError(err)
+// 	srs, err := kzg_bw6761.NewSRS(kzgSize, alpha)
+// 	assert.NoError(err)
+
+// 	f := make([]fr_bw6761.Element, polynomialSize)
+// 	for i := range f {
+// 		f[i].SetRandom()
+// 	}
+
+// 	com, err := kzg_bw6761.Commit(f, srs.Pk)
+// 	assert.NoError(err)
+
+// 	var point fr_bw6761.Element
+// 	point.SetRandom()
+// 	proof, err := kzg_bw6761.Open(f, point, srs.Pk)
+// 	assert.NoError(err)
+
+// 	if err = kzg_bw6761.Verify(&com, &proof, point, srs.Vk); err != nil {
+// 		t.Fatal("verify proof", err)
+// 	}
+
+// 	wCmt, err := ValueOfCommitment[sw_bw6761.G1Affine](com)
+// 	assert.NoError(err)
+// 	wProof, err := ValueOfOpeningProof[sw_bw6761.Scalar, sw_bw6761.G1Affine](point, proof)
+// 	assert.NoError(err)
+// 	wVk, err := ValueOfVerifyingKey[sw_bw6761.G2Affine](srs.Vk)
+// 	assert.NoError(err)
+
+// 	assignment := KZGVerificationCircuit[sw_bw6761.Scalar, sw_bw6761.G1Affine, sw_bw6761.G2Affine, sw_bw6761.GTEl]{
+// 		VerifyingKey: wVk,
+// 		Commitment:   wCmt,
+// 		OpeningProof: wProof,
+// 	}
+// 	assert.CheckCircuit(&KZGVerificationCircuit[sw_bw6761.Scalar, sw_bw6761.G1Affine, sw_bw6761.G2Affine, sw_bw6761.GTEl]{}, test.WithValidAssignment(&assignment), test.WithCurves(ecc.BN254))
+// }
+
+// func TestKZGVerificationTwoChain(t *testing.T) {
+// 	assert := test.NewAssert(t)
+
+// 	alpha, err := rand.Int(rand.Reader, ecc.BLS12_377.ScalarField())
+// 	assert.NoError(err)
+// 	srs, err := kzg_bls12377.NewSRS(kzgSize, alpha)
+// 	assert.NoError(err)
+
+// 	f := make([]fr_bls12377.Element, polynomialSize)
+// 	for i := range f {
+// 		f[i].SetRandom()
+// 	}
+
+// 	com, err := kzg_bls12377.Commit(f, srs.Pk)
+// 	assert.NoError(err)
+
+// 	var point fr_bls12377.Element
+// 	point.SetRandom()
+// 	proof, err := kzg_bls12377.Open(f, point, srs.Pk)
+// 	assert.NoError(err)
+
+// 	if err = kzg_bls12377.Verify(&com, &proof, point, srs.Vk); err != nil {
+// 		t.Fatal("verify proof", err)
+// 	}
+
+// 	wCmt, err := ValueOfCommitment[sw_bls12377.G1Affine](com)
+// 	assert.NoError(err)
+// 	wProof, err := ValueOfOpeningProof[sw_bls12377.Scalar, sw_bls12377.G1Affine](point, proof)
+// 	assert.NoError(err)
+// 	wVk, err := ValueOfVerifyingKey[sw_bls12377.G2Affine](srs.Vk)
+// 	assert.NoError(err)
+
+// 	assignment := KZGVerificationCircuit[sw_bls12377.Scalar, sw_bls12377.G1Affine, sw_bls12377.G2Affine, sw_bls12377.GT]{
+// 		VerifyingKey: wVk,
+// 		Commitment:   wCmt,
+// 		OpeningProof: wProof,
+// 	}
+
+// 	assert.CheckCircuit(&KZGVerificationCircuit[sw_bls12377.Scalar, sw_bls12377.G1Affine, sw_bls12377.G2Affine, sw_bls12377.GT]{}, test.WithValidAssignment(&assignment), test.WithCurves(ecc.BW6_761))
+// }
+
+// func TestKZGVerificationTwoChain2(t *testing.T) {
+// 	assert := test.NewAssert(t)
+
+// 	alpha, err := rand.Int(rand.Reader, ecc.BLS24_315.ScalarField())
+// 	assert.NoError(err)
+// 	srs, err := kzg_bls24315.NewSRS(kzgSize, alpha)
+// 	assert.NoError(err)
+
+// 	f := make([]fr_bls24315.Element, polynomialSize)
+// 	for i := range f {
+// 		f[i].SetRandom()
+// 	}
+
+// 	com, err := kzg_bls24315.Commit(f, srs.Pk)
+// 	assert.NoError(err)
+
+// 	var point fr_bls24315.Element
+// 	point.SetRandom()
+// 	proof, err := kzg_bls24315.Open(f, point, srs.Pk)
+// 	assert.NoError(err)
+
+// 	if err = kzg_bls24315.Verify(&com, &proof, point, srs.Vk); err != nil {
+// 		t.Fatal("verify proof", err)
+// 	}
+
+// 	wCmt, err := ValueOfCommitment[sw_bls24315.G1Affine](com)
+// 	assert.NoError(err)
+// 	wProof, err := ValueOfOpeningProof[sw_bls24315.Scalar, sw_bls24315.G1Affine](point, proof)
+// 	assert.NoError(err)
+// 	wVk, err := ValueOfVerifyingKey[sw_bls24315.G2Affine](srs.Vk)
+// 	assert.NoError(err)
+
+// 	assignment := KZGVerificationCircuit[sw_bls24315.Scalar, sw_bls24315.G1Affine, sw_bls24315.G2Affine, sw_bls24315.GT]{
+// 		VerifyingKey: wVk,
+// 		Commitment:   wCmt,
+// 		OpeningProof: wProof,
+// 	}
+
+// 	assert.CheckCircuit(&KZGVerificationCircuit[sw_bls24315.Scalar, sw_bls24315.G1Affine, sw_bls24315.G2Affine, sw_bls24315.GT]{}, test.WithValidAssignment(&assignment), test.WithCurves(ecc.BW6_633))
+// }
+
+// func TestValueOfCommitment(t *testing.T) {
+// 	assert := test.NewAssert(t)
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, G1, _ := bn254.Generators()
+// 		assignment, err := ValueOfCommitment[sw_bn254.G1Affine](G1)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bn254")
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, G1, _ := bls12377.Generators()
+// 		assignment, err := ValueOfCommitment[sw_bls12377.G1Affine](G1)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bls12377")
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, G1, _ := bls12381.Generators()
+// 		assignment, err := ValueOfCommitment[sw_bls12381.G1Affine](G1)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bls12381")
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, G1, _ := bw6761.Generators()
+// 		assignment, err := ValueOfCommitment[sw_bw6761.G1Affine](G1)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bw6761")
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, G1, _ := bls24315.Generators()
+// 		assignment, err := ValueOfCommitment[sw_bls24315.G1Affine](G1)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bls24315")
+// }
+
+// func TestValueOfOpeningProof(t *testing.T) {
+// 	assert := test.NewAssert(t)
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, G1, _ := bn254.Generators()
+// 		var value, point fr_bn254.Element
+// 		value.SetRandom()
+// 		point.SetRandom()
+// 		proof := kzg_bn254.OpeningProof{
+// 			H:            G1,
+// 			ClaimedValue: value,
+// 		}
+// 		assignment, err := ValueOfOpeningProof[sw_bn254.Scalar, sw_bn254.G1Affine](point, proof)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bn254")
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, G1, _ := bls12377.Generators()
+// 		var value, point fr_bls12377.Element
+// 		value.SetRandom()
+// 		point.SetRandom()
+// 		proof := kzg_bls12377.OpeningProof{
+// 			H:            G1,
+// 			ClaimedValue: value,
+// 		}
+// 		assignment, err := ValueOfOpeningProof[sw_bls12377.Scalar, sw_bls12377.G1Affine](point, proof)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bls12377")
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, G1, _ := bls12381.Generators()
+// 		var value, point fr_bls12381.Element
+// 		value.SetRandom()
+// 		point.SetRandom()
+// 		proof := kzg_bls12381.OpeningProof{
+// 			H:            G1,
+// 			ClaimedValue: value,
+// 		}
+// 		assignment, err := ValueOfOpeningProof[sw_bls12381.Scalar, sw_bls12381.G1Affine](point, proof)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bls12381")
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, G1, _ := bw6761.Generators()
+// 		var value, point fr_bw6761.Element
+// 		value.SetRandom()
+// 		point.SetRandom()
+// 		proof := kzg_bw6761.OpeningProof{
+// 			H:            G1,
+// 			ClaimedValue: value,
+// 		}
+// 		assignment, err := ValueOfOpeningProof[sw_bw6761.Scalar, sw_bw6761.G1Affine](point, proof)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bw6761")
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, G1, _ := bls24315.Generators()
+// 		var value, point fr_bls24315.Element
+// 		value.SetRandom()
+// 		point.SetRandom()
+// 		proof := kzg_bls24315.OpeningProof{
+// 			H:            G1,
+// 			ClaimedValue: value,
+// 		}
+// 		assignment, err := ValueOfOpeningProof[sw_bls24315.Scalar, sw_bls24315.G1Affine](point, proof)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bls24315")
+// }
+
+// func TestValueOfSRS(t *testing.T) {
+// 	assert := test.NewAssert(t)
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, _, G2 := bn254.Generators()
+// 		vk := kzg_bn254.VerifyingKey{
+// 			G2: [2]bn254.G2Affine{G2, G2},
+// 		}
+// 		assignment, err := ValueOfVerifyingKey[sw_bn254.G2Affine](vk)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bn254")
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, _, G2 := bls12377.Generators()
+// 		vk := kzg_bls12377.VerifyingKey{
+// 			G2: [2]bls12377.G2Affine{G2, G2},
+// 		}
+// 		assignment, err := ValueOfVerifyingKey[sw_bls12377.G2Affine](vk)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bls12377")
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, _, G2 := bls12381.Generators()
+// 		vk := kzg_bls12381.VerifyingKey{
+// 			G2: [2]bls12381.G2Affine{G2, G2},
+// 		}
+// 		assignment, err := ValueOfVerifyingKey[sw_bls12381.G2Affine](vk)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bls12381")
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, _, G2 := bw6761.Generators()
+// 		vk := kzg_bw6761.VerifyingKey{
+// 			G2: [2]bw6761.G2Affine{G2, G2},
+// 		}
+// 		assignment, err := ValueOfVerifyingKey[sw_bw6761.G2Affine](vk)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bw6761")
+// 	assert.Run(func(assert *test.Assert) {
+// 		_, _, _, G2 := bls24315.Generators()
+// 		vk := kzg_bls24315.VerifyingKey{
+// 			G2: [2]bls24315.G2Affine{G2, G2},
+// 		}
+// 		assignment, err := ValueOfVerifyingKey[sw_bls24315.G2Affine](vk)
+// 		assert.NoError(err)
+// 		_ = assignment
+// 	}, "bls24315")
+// }
