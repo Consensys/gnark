025a150a11d46c85eeea398ab590e903b2a24905
Ivo Kubjas <ivo.kubjas@consensys.net>
2023-11-02 10:54:26 +0100

    test: add reverse marshal test

--- backend/groth16/bn254/icicle/marshal_test.go
+++ backend/groth16/bn254/icicle/marshal_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend/groth16"
 	groth16_bn254 "github.com/consensys/gnark/backend/groth16/bn254"
+	icicle_bn254 "github.com/consensys/gnark/backend/groth16/bn254/icicle"
 	cs_bn254 "github.com/consensys/gnark/constraint/bn254"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/r1cs"
@@ -40,6 +41,27 @@ func TestMarshal(t *testing.T) {
 	_, err = pk.ReadFrom(buf)
 	assert.NoError(err)
 	if pk.IsDifferent(&nativePK) {
+		t.Error("marshal output difference")
+	}
+}
+
+func TestMarshal2(t *testing.T) {
+	assert := test.NewAssert(t)
+	ccs, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &circuit{})
+	assert.NoError(err)
+	tCcs := ccs.(*cs_bn254.R1CS)
+	iciPK := icicle_bn254.ProvingKey{}
+	iciVK := groth16_bn254.VerifyingKey{}
+	err = icicle_bn254.Setup(tCcs, &iciPK, &iciVK)
+	assert.NoError(err)
+
+	nativePK := groth16_bn254.ProvingKey{}
+	buf := new(bytes.Buffer)
+	_, err = iciPK.WriteTo(buf)
+	assert.NoError(err)
+	_, err = nativePK.ReadFrom(buf)
+	assert.NoError(err)
+	if iciPK.IsDifferent(&nativePK) {
 		t.Error("marshal output difference")
 	}
 }
