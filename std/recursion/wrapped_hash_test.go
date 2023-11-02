f905ad70af23b876767854485c940a48b091da75
Ivo Kubjas <ivo.kubjas@consensys.net>
2023-11-03 00:01:37 +0100

    test: implement FS tests with marshal

--- std/recursion/wrapped_hash_test.go
+++ std/recursion/wrapped_hash_test.go
@@ -13,9 +13,12 @@ import (
 	fr_bls24315 "github.com/consensys/gnark-crypto/ecc/bls24-315/fr"
 	"github.com/consensys/gnark-crypto/ecc/bn254"
 	fr_bn254 "github.com/consensys/gnark-crypto/ecc/bn254/fr"
+	bw6761 "github.com/consensys/gnark-crypto/ecc/bw6-761"
+	cryptofs "github.com/consensys/gnark-crypto/fiat-shamir"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/std/algebra"
 	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bw6761"
 	"github.com/consensys/gnark/std/algebra/native/sw_bls12377"
 	"github.com/consensys/gnark/std/algebra/native/sw_bls24315"
 	"github.com/consensys/gnark/std/recursion"
@@ -256,4 +259,114 @@ func TestHashMarshalScalar(t *testing.T) {
 		}
 		assert.CheckCircuit(circuit, test.WithCurves(ecc.BW6_633), test.WithValidAssignment(assignment), test.NoFuzzing(), test.NoSerializationChecks(), test.NoSolidityChecks())
 	})
+}
+
+type transcriptCircuit[S algebra.ScalarT, G1El algebra.G1ElementT] struct {
+	Challenges [3]string
+	Points     [3][3]G1El
+	Expected   [3]frontend.Variable
+
+	target *big.Int
+}
+
+func (c *transcriptCircuit[S, G1El]) Define(api frontend.API) error {
+	fs, err := recursion.NewTranscript(api, c.target, c.Challenges[:])
+	if err != nil {
+		return fmt.Errorf("new transcript: %w", err)
+	}
+	curve, err := algebra.GetCurve[S, G1El](api)
+	if err != nil {
+		return fmt.Errorf("get curve: %w", err)
+	}
+	for i := range c.Points {
+		for j := range c.Points[i] {
+			if err := fs.Bind(c.Challenges[i], curve.MarshalG1(c.Points[i][j])); err != nil {
+				return fmt.Errorf("bind[%d][%d] %w", i, j, err)
+			}
+		}
+	}
+	for i := range c.Expected {
+		res, err := fs.ComputeChallenge(c.Challenges[i])
+		if err != nil {
+			return fmt.Errorf("compute challenge %d: %w", i, err)
+		}
+		api.AssertIsEqual(res, c.Expected[i])
+	}
+	return nil
+}
+
+func TestTranscriptMarsha(t *testing.T) {
+	assert := test.NewAssert(t)
+	assert.Run(func(assert *test.Assert) {
+		h, err := recursion.NewShort(ecc.BW6_761.ScalarField(), ecc.BLS12_377.ScalarField())
+		assert.NoError(err)
+		challenges := [3]string{"alfa", "beta", "gamma"}
+		fs := cryptofs.NewTranscript(h, challenges[:]...)
+		var points [3][3]sw_bls12377.G1Affine
+		for i := range points {
+			for j := range points[i] {
+				var p bls12377.G1Affine
+				r, err := rand.Int(rand.Reader, ecc.BLS12_377.ScalarField())
+				assert.NoError(err)
+				p.ScalarMultiplicationBase(r)
+				points[i][j] = sw_bls12377.NewG1Affine(p)
+				if err := fs.Bind(challenges[i], p.Marshal()); err != nil {
+					t.Fatal("bind", err)
+				}
+			}
+		}
+		var expected [3]frontend.Variable
+		for i := range expected {
+			res, err := fs.ComputeChallenge(challenges[i])
+			assert.NoError(err)
+			expected[i] = res
+		}
+		circuit := &transcriptCircuit[sw_bls12377.Scalar, sw_bls12377.G1Affine]{
+			Challenges: challenges,
+			target:     ecc.BLS12_377.ScalarField(),
+		}
+		assignment := &transcriptCircuit[sw_bls12377.Scalar, sw_bls12377.G1Affine]{
+			Challenges: challenges,
+			Points:     points,
+			Expected:   expected,
+			target:     ecc.BLS12_377.ScalarField(),
+		}
+		assert.CheckCircuit(circuit, test.WithValidAssignment(assignment), test.WithCurves(ecc.BW6_761), test.NoFuzzing(), test.NoSerializationChecks())
+	}, "bw6_761")
+	assert.Run(func(assert *test.Assert) {
+		h, err := recursion.NewShort(ecc.BN254.ScalarField(), ecc.BW6_761.ScalarField())
+		assert.NoError(err)
+		challenges := [3]string{"alfa", "beta", "gamma"}
+		fs := cryptofs.NewTranscript(h, challenges[:]...)
+		var points [3][3]sw_bw6761.G1Affine
+		for i := range points {
+			for j := range points[i] {
+				var p bw6761.G1Affine
+				r, err := rand.Int(rand.Reader, ecc.BW6_761.ScalarField())
+				assert.NoError(err)
+				p.ScalarMultiplicationBase(r)
+				points[i][j] = sw_bw6761.NewG1Affine(p)
+				if err := fs.Bind(challenges[i], p.Marshal()); err != nil {
+					t.Fatal("bind", err)
+				}
+			}
+		}
+		var expected [3]frontend.Variable
+		for i := range expected {
+			res, err := fs.ComputeChallenge(challenges[i])
+			assert.NoError(err)
+			expected[i] = res
+		}
+		circuit := &transcriptCircuit[sw_bw6761.Scalar, sw_bw6761.G1Affine]{
+			Challenges: challenges,
+			target:     ecc.BW6_761.ScalarField(),
+		}
+		assignment := &transcriptCircuit[sw_bw6761.Scalar, sw_bw6761.G1Affine]{
+			Challenges: challenges,
+			Points:     points,
+			Expected:   expected,
+			target:     ecc.BW6_761.ScalarField(),
+		}
+		assert.CheckCircuit(circuit, test.WithValidAssignment(assignment), test.WithCurves(ecc.BN254), test.NoFuzzing(), test.NoSerializationChecks())
+	}, "bn254")
 }
