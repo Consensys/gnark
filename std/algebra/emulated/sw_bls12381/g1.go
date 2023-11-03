e9e4155ddacd52d11689a960f483f6e8637f64df
Ivo Kubjas <ivo.kubjas@consensys.net>
2023-11-03 13:03:56 +0100

    refactor: sw_bls12381 implement

--- std/algebra/emulated/sw_bls12381/g1.go
+++ std/algebra/emulated/sw_bls12381/g1.go
@@ -12,32 +12,31 @@ import (
 
 // G1Affine is the point in G1. It is an alias to the generic emulated affine
 // point.
-type G1Affine = sw_emulated.AffinePoint[emulated.BLS12381Fp]
+type G1Affine = sw_emulated.AffinePoint[BaseField]
 
 // Scalar is the scalar in the groups. It is an alias to the emulated element
 // defined over the scalar field of the groups.
-type Scalar = emulated.Element[emulated.BLS12381Fr]
+type Scalar = emulated.Element[ScalarField]
 
 // NewG1Affine allocates a witness from the native G1 element and returns it.
-
 func NewG1Affine(v bls12381.G1Affine) G1Affine {
 	return G1Affine{
-		X: emulated.ValueOf[emulated.BLS12381Fp](v.X),
-		Y: emulated.ValueOf[emulated.BLS12381Fp](v.Y),
+		X: emulated.ValueOf[BaseField](v.X),
+		Y: emulated.ValueOf[BaseField](v.Y),
 	}
 }
 
 type G1 struct {
-	curveF *emulated.Field[emulated.BLS12381Fp]
-	w      *emulated.Element[emulated.BLS12381Fp]
+	curveF *emulated.Field[BaseField]
+	w      *emulated.Element[BaseField]
 }
 
 func NewG1(api frontend.API) (*G1, error) {
-	ba, err := emulated.NewField[emulated.BLS12381Fp](api)
+	ba, err := emulated.NewField[BaseField](api)
 	if err != nil {
 		return nil, fmt.Errorf("new base api: %w", err)
 	}
-	w := emulated.ValueOf[emulated.BLS12381Fp]("4002409555221667392624310435006688643935503118305586438271171395842971157480381377015405980053539358417135540939436")
+	w := emulated.ValueOf[BaseField]("4002409555221667392624310435006688643935503118305586438271171395842971157480381377015405980053539358417135540939436")
 	return &G1{
 		curveF: ba,
 		w:      &w,
@@ -55,5 +54,11 @@ func (g1 *G1) phi(q *G1Affine) *G1Affine {
 
 // NewScalar allocates a witness from the native scalar and returns it.
 func NewScalar(v fr_bls12381.Element) Scalar {
-	return emulated.ValueOf[emulated.BLS12381Fr](v)
+	return emulated.ValueOf[ScalarField](v)
 }
+
+// ScalarField is the [emulated.FieldParams] impelementation of the curve scalar field.
+type ScalarField = emulated.BLS12381Fr
+
+// BaseField is the [emulated.FieldParams] impelementation of the curve base field.
+type BaseField = emulated.BLS12381Fp
