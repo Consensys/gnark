16d88dbe7b996da4103414e709b8f7ae9d5b486f
Ivo Kubjas <ivo.kubjas@consensys.net>
2023-11-20 15:24:44 +0100

    docs: add package examples

---
+++ std/recursion/plonk/nonnative_doc_test.go
@@ -0,0 +1,191 @@
+package plonk_test
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend"
+	native_plonk "github.com/consensys/gnark/backend/plonk"
+	"github.com/consensys/gnark/backend/witness"
+	"github.com/consensys/gnark/constraint"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/scs"
+	"github.com/consensys/gnark/std/algebra"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bw6761"
+	"github.com/consensys/gnark/std/math/emulated"
+	"github.com/consensys/gnark/std/recursion"
+	"github.com/consensys/gnark/std/recursion/plonk"
+	"github.com/consensys/gnark/test"
+)
+
+// InnerCircuitNative is the definition of the inner circuit we want to
+// recursively verify inside an outer circuit. The circuit proves the knowledge
+// of a factorisation of a semiprime.
+type InnerCircuitNative struct {
+	P, Q frontend.Variable
+	N    frontend.Variable `gnark:",public"`
+}
+
+func (c *InnerCircuitNative) Define(api frontend.API) error {
+	// prove that P*Q == N
+	res := api.Mul(c.P, c.Q)
+	api.AssertIsEqual(res, c.N)
+	// we must also enforce that P != 1 and Q != 1
+	api.AssertIsDifferent(c.P, 1)
+	api.AssertIsDifferent(c.Q, 1)
+	return nil
+}
+
+// computeInnerProof computes the proof for the inner circuit we want to verify
+// recursively. In this example the PLONK keys are generated on the fly, but
+// in practice should be genrated once and using MPC.
+func computeInnerProof(field, outer *big.Int) (constraint.ConstraintSystem, native_plonk.VerifyingKey, witness.Witness, native_plonk.Proof) {
+	innerCcs, err := frontend.Compile(field, scs.NewBuilder, &InnerCircuitNative{})
+	if err != nil {
+		panic(err)
+	}
+	// NB! UNSAFE! Use MPC.
+	srs, err := test.NewKZGSRS(innerCcs)
+	if err != nil {
+		panic(err)
+	}
+	innerPK, innerVK, err := native_plonk.Setup(innerCcs, srs)
+	if err != nil {
+		panic(err)
+	}
+
+	// inner proof
+	innerAssignment := &InnerCircuitNative{
+		P: 3,
+		Q: 5,
+		N: 15,
+	}
+	innerWitness, err := frontend.NewWitness(innerAssignment, field)
+	if err != nil {
+		panic(err)
+	}
+	fsProverHasher, err := recursion.NewShort(outer, field)
+	if err != nil {
+		panic(err)
+	}
+	kzgProverHasher, err := recursion.NewShort(outer, field)
+	if err != nil {
+		panic(err)
+	}
+	innerProof, err := native_plonk.Prove(innerCcs, innerPK, innerWitness,
+		backend.WithProverChallengeHashFunction(fsProverHasher),
+		backend.WithProverKZGFoldingHashFunction(kzgProverHasher),
+	)
+	if err != nil {
+		panic(err)
+	}
+	innerPubWitness, err := innerWitness.Public()
+	if err != nil {
+		panic(err)
+	}
+	fsVerifierHasher, err := recursion.NewShort(outer, field)
+	if err != nil {
+		panic(err)
+	}
+	kzgVerifierHash, err := recursion.NewShort(outer, field)
+	if err != nil {
+		panic(err)
+	}
+	err = native_plonk.Verify(innerProof, innerVK, innerPubWitness,
+		backend.WithVerifierChallengeHashFunction(fsVerifierHasher),
+		backend.WithVerifierKZGFoldingHashFunction(kzgVerifierHash),
+	)
+	if err != nil {
+		panic(err)
+	}
+	return innerCcs, innerVK, innerPubWitness, innerProof
+}
+
+// OuterCircuit is the generic outer circuit which can verify PLONK proofs
+// using field emulation or 2-chains of curves.
+type OuterCircuit[FR emulated.FieldParams, G1El algebra.G1ElementT, G2El algebra.G2ElementT, GtEl algebra.GtElementT] struct {
+	Proof        plonk.Proof[FR, G1El, G2El]
+	VerifyingKey plonk.VerifyingKey[FR, G1El, G2El]
+	InnerWitness plonk.Witness[FR]
+}
+
+func (c *OuterCircuit[FR, G1El, G2El, GtEl]) Define(api frontend.API) error {
+	verifier, err := plonk.NewVerifier[FR, G1El, G2El, GtEl](api)
+	if err != nil {
+		return fmt.Errorf("new verifier: %w", err)
+	}
+	err = verifier.AssertProof(c.VerifyingKey, c.Proof, c.InnerWitness)
+	return err
+}
+
+// Example of verifying recursively BW6-761 PLONK proof in BN254 PLONK circuit using field emulation
+func Example_emulated() {
+	// compute the proof which we want to verify recursively
+	innerCcs, innerVK, innerWitness, innerProof := computeInnerProof(ecc.BW6_761.ScalarField(), ecc.BN254.ScalarField())
+
+	// initialize the witness elements
+	circuitVk, err := plonk.ValueOfVerifyingKey[sw_bw6761.ScalarField, sw_bw6761.G1Affine, sw_bw6761.G2Affine](innerVK)
+	if err != nil {
+		panic(err)
+	}
+	circuitWitness, err := plonk.ValueOfWitness[sw_bw6761.ScalarField](innerWitness)
+	if err != nil {
+		panic(err)
+	}
+	circuitProof, err := plonk.ValueOfProof[sw_bw6761.ScalarField, sw_bw6761.G1Affine, sw_bw6761.G2Affine](innerProof)
+	if err != nil {
+		panic(err)
+	}
+
+	outerCircuit := &OuterCircuit[sw_bw6761.ScalarField, sw_bw6761.G1Affine, sw_bw6761.G2Affine, sw_bw6761.GTEl]{
+		InnerWitness: plonk.PlaceholderWitness[sw_bw6761.ScalarField](innerCcs),
+		Proof:        plonk.PlaceholderProof[sw_bw6761.ScalarField, sw_bw6761.G1Affine, sw_bw6761.G2Affine](innerProof),
+		VerifyingKey: plonk.PlaceholderVerifyingKey[sw_bw6761.ScalarField, sw_bw6761.G1Affine, sw_bw6761.G2Affine](innerVK),
+	}
+	outerAssignment := &OuterCircuit[sw_bw6761.ScalarField, sw_bw6761.G1Affine, sw_bw6761.G2Affine, sw_bw6761.GTEl]{
+		InnerWitness: circuitWitness,
+		Proof:        circuitProof,
+		VerifyingKey: circuitVk,
+	}
+	// compile the outer circuit
+	ccs, err := frontend.Compile(ecc.BN254.ScalarField(), scs.NewBuilder, outerCircuit)
+	if err != nil {
+		panic("compile failed: " + err.Error())
+	}
+
+	// NB! UNSAFE! Use MPC.
+	srs, err := test.NewKZGSRS(innerCcs)
+	if err != nil {
+		panic(err)
+	}
+	// create PLONK setup. NB! UNSAFE
+	pk, vk, err := native_plonk.Setup(ccs, srs) // UNSAFE! Use MPC
+	if err != nil {
+		panic("setup failed: " + err.Error())
+	}
+
+	// create prover witness from the assignment
+	secretWitness, err := frontend.NewWitness(outerAssignment, ecc.BN254.ScalarField())
+	if err != nil {
+		panic("secret witness failed: " + err.Error())
+	}
+
+	// create public witness from the assignment
+	publicWitness, err := secretWitness.Public()
+	if err != nil {
+		panic("public witness failed: " + err.Error())
+	}
+
+	// construct the PLONK proof of verifying PLONK proof in-circuit
+	outerProof, err := native_plonk.Prove(ccs, pk, secretWitness)
+	if err != nil {
+		panic("proving failed: " + err.Error())
+	}
+
+	// verify the Groth16 proof
+	err = native_plonk.Verify(outerProof, vk, publicWitness)
+	if err != nil {
+		panic("circuit verification failed: " + err.Error())
+	}
+}
