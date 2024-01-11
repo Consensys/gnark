8477e96ee1f985421eb4e8c86a82dceab054405d
Ivo Kubjas <ivo.kubjas@consensys.net>
2024-01-11 14:50:30 +0100

    feat: add multilinear claim

--- std/recursion/sumcheck/claimable.go
+++ std/recursion/sumcheck/claimable.go
@@ -1,6 +1,10 @@
 package sumcheck
 
 import (
+	"fmt"
+	"math/bits"
+
+	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/std/math/emulated"
 	"github.com/consensys/gnark/std/math/polynomial"
 )
@@ -22,4 +26,55 @@ type Proof[FR emulated.FieldParams] struct {
 	PartialSumPolys []polynomial.Univariate[FR]
 	// TODO: do we need to have the FinalEvalProof here? it would be good if we can move it to the claim manager.
 	FinalEvalProof EvaluationProof
+}
+
+type Function[FR emulated.FieldParams] struct {
+	ml    polynomial.Multilinear[FR]
+	claim *emulated.Element[FR]
+
+	f *emulated.Field[FR]
+	p *polynomial.Polynomial[FR]
+}
+
+func NewMultiLinearClaim[FR emulated.FieldParams](api frontend.API, ml polynomial.Multilinear[FR], claim *emulated.Element[FR]) (*Function[FR], error) {
+	f, err := emulated.NewField[FR](api)
+	if err != nil {
+		return nil, fmt.Errorf("new field: %w", err)
+	}
+	p, err := polynomial.New[FR](api)
+	if err != nil {
+		return nil, fmt.Errorf("new polynomial: %w", err)
+	}
+	return &Function[FR]{
+		ml:    ml,
+		claim: claim,
+		f:     f,
+		p:     p,
+	}, nil
+}
+
+func (fn *Function[FR]) NbClaims() int {
+	return 1
+}
+
+func (fn *Function[FR]) NbVars() int {
+	return bits.Len(uint(len(fn.ml))) - 1
+}
+
+func (fn *Function[FR]) CombinedSum(coeff *emulated.Element[FR]) *emulated.Element[FR] {
+	return fn.claim
+}
+
+func (fn *Function[FR]) Degree(i int) int {
+	// this is multlinear function - up to degree 1 in every variable
+	return 1
+}
+
+func (fn *Function[FR]) AssertEvaluation(r []*emulated.Element[FR], combinationCoeff *emulated.Element[FR], expectedValue *emulated.Element[FR], proof EvaluationProof) error {
+	val, err := fn.p.EvalMultilinear(fn.ml, r)
+	if err != nil {
+		return fmt.Errorf("eval: %w", err)
+	}
+	fn.f.AssertIsEqual(val, fn.claim)
+	return nil
 }
