d892280a562c0f806e0f57caa6193eb9f1e4414c
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2024-01-19 11:53:03 -0600

    bench shift left

--- std/compress/io_test.go
+++ std/compress/io_test.go
@@ -2,11 +2,14 @@ package compress
 
 import (
 	"crypto/rand"
+	"fmt"
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr"
 	"github.com/consensys/gnark-crypto/hash"
 	"github.com/consensys/gnark/backend"
 	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/scs"
+	"github.com/consensys/gnark/profile"
 	test_vector_utils "github.com/consensys/gnark/std/utils/test_vectors_utils"
 	"github.com/consensys/gnark/test"
 	"github.com/stretchr/testify/assert"
@@ -45,6 +48,21 @@ func TestShiftLeft(t *testing.T) {
 
 		test.NewAssert(t).CheckCircuit(&circuit, test.WithValidAssignment(&assignment), test.WithBackends(backend.PLONK), test.WithCurves(ecc.BLS12_377))
 	}
+}
+
+func BenchmarkShiftLeft(b *testing.B) {
+	const n = 128 * 1024
+
+	circuit := shiftLeftCircuit{
+		Slice:   make([]frontend.Variable, n),
+		Shifted: make([]frontend.Variable, n),
+	}
+
+	p := profile.Start()
+	cs, err := frontend.Compile(ecc.BLS12_377.ScalarField(), scs.NewBuilder, &circuit)
+	assert.NoError(b, err)
+	p.Stop()
+	fmt.Println(cs.GetNbConstraints(), "constraints")
 }
 
 type shiftLeftCircuit struct {
