a1f7ff5078fef5675276fadf288d88a388599155
bigsky77 <simonjudd2@gmail.com>
2024-02-12 07:02:15 -0700

    remove async waitgroup

--- backend/plonk/bn254/icicle/icicle.go
+++ backend/plonk/bn254/icicle/icicle.go
@@ -1304,18 +1304,18 @@ func calculateNbTasks(n int) int {
 
 // batchApply executes fn on all polynomials in x except x[id_ZS] in parallel.
 func batchApply(x []*iop.Polynomial, fn func(*iop.Polynomial, int)) {
-	var wg sync.WaitGroup
+	//	var wg sync.WaitGroup
 	for i := 0; i < len(x); i++ {
 		if i == id_ZS {
 			continue
 		}
-		wg.Add(1)
-		go func(i int) {
+		//wg.Add(1)
+		//go func(i int) {
 			fn(x[i], i)
-		wg.Done()
-		}(i)
-	}
-	wg.Wait()
+			//wg.Done()
+			//}(i)
+			}
+		//wg.Wait()
 }
 
 // p <- <p, (1, w, .., wⁿ) >
