f8737decf22a53174a7cec36c0d47e45b62eab3e
Ivo Kubjas <ivo.kubjas@consensys.net>
2024-04-01 22:38:43 +0000

    feat: add no-commit Solidity test case

--- internal/backend/circuits/commit.go
+++ internal/backend/circuits/commit.go
@@ -3,6 +3,8 @@ package circuits
 import (
 	"crypto/sha256"
 
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark-crypto/ecc/bn254"
 	"github.com/consensys/gnark/backend"
 	"github.com/consensys/gnark/frontend"
 )
@@ -28,17 +30,40 @@ func (circuit *commitCircuit) Define(api frontend.API) error {
 	return nil
 }
 
+type noCommitCircuit struct {
+	Public frontend.Variable `gnark:",public"`
+	X      frontend.Variable
+}
+
+func (circuit *noCommitCircuit) Define(api frontend.API) error {
+	api.AssertIsDifferent(circuit.Public, 0)
+	a := api.Mul(circuit.X, circuit.X)
+	for i := 0; i < 10; i++ {
+		a = api.Mul(a, circuit.X)
+	}
+	c := api.Add(a, circuit.X)
+	api.AssertIsDifferent(c, a)
+	return nil
+}
+
 func init() {
 	h1, h2 := sha256.New(), sha256.New()
-	var circuit, good, bad commitCircuit
 
-	good.X = 3
-	good.Public = 16
-
-	bad.X = 4
-	bad.Public = 0
-
-	addEntry("commit_Groth16", &circuit, &good, &bad, nil,
-		WithProverOpts(backend.WithProverHashToFieldFunction(h1)), WithVerifierOpts(backend.WithVerifierHashToFieldFunction(h2)))
-	addEntry("commit_Plonk", &circuit, &good, &bad, nil)
+	// need to have separate test cases as the hash-to-field for PLONK and Groth16 verifiers are different
+	addEntry(
+		"commit_Groth16",
+		&commitCircuit{}, &commitCircuit{Public: 16, X: 3}, &commitCircuit{Public: 0, X: 4},
+		[]ecc.ID{bn254.ID}, WithBackends(backend.GROTH16), WithProverOpts(backend.WithProverHashToFieldFunction(h1)), WithVerifierOpts(backend.WithVerifierHashToFieldFunction(h2)))
+	addEntry(
+		"commit_Plonk",
+		&commitCircuit{}, &commitCircuit{Public: 16, X: 3}, &commitCircuit{Public: 0, X: 4},
+		[]ecc.ID{bn254.ID}, WithBackends(backend.PLONK))
+	addEntry(
+		"no_commit_Groth16",
+		&noCommitCircuit{}, &noCommitCircuit{Public: 16, X: 3}, &noCommitCircuit{Public: 0, X: 4},
+		[]ecc.ID{bn254.ID}, WithBackends(backend.GROTH16), WithProverOpts(backend.WithProverHashToFieldFunction(h1)), WithVerifierOpts(backend.WithVerifierHashToFieldFunction(h2)))
+	addEntry(
+		"no_commit_Plonk",
+		&noCommitCircuit{}, &noCommitCircuit{Public: 16, X: 3}, &noCommitCircuit{Public: 0, X: 4},
+		[]ecc.ID{bn254.ID}, WithBackends(backend.PLONK))
 }
