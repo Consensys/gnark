28ce42a80c94845a1c10073c5acb850e6eaf8edf
Thomas Piellard <thomas.piellard@consensys.net>
2024-04-03 12:10:50 +0200

    feat: constants in template ok

--- backend/fflonk/bn254/verify.go
+++ backend/fflonk/bn254/verify.go
@@ -3,11 +3,13 @@ package fflonk
 import (
 	"errors"
 	"fmt"
+	"html/template"
 	"io"
 	"math/big"
 	"time"
 
 	curve "github.com/consensys/gnark-crypto/ecc/bn254"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fp"
 
 	"github.com/consensys/gnark-crypto/ecc/bn254/fflonk"
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
@@ -299,37 +301,36 @@ func deriveRandomness(fs *fiatshamir.Transcript, challenge string, points ...*cu
 //
 // Code has not been audited and is provided as-is, we make no guarantees or warranties to its safety and reliability.
 func (vk *VerifyingKey) ExportSolidity(w io.Writer) error {
-	// funcMap := template.FuncMap{
-	// 	"hex": func(i int) string {
-	// 		return fmt.Sprintf("0x%x", i)
-	// 	},
-	// 	"mul": func(a, b int) int {
-	// 		return a * b
-	// 	},
-	// 	"inc": func(i int) int {
-	// 		return i + 1
-	// 	},
-	// 	"frstr": func(x fr.Element) string {
-	// 		// we use big.Int to always get a positive string.
-	// 		// not the most efficient hack, but it works better for .sol generation.
-	// 		bv := new(big.Int)
-	// 		x.BigInt(bv)
-	// 		return bv.String()
-	// 	},
-	// 	"fpstr": func(x fp.Element) string {
-	// 		bv := new(big.Int)
-	// 		x.BigInt(bv)
-	// 		return bv.String()
-	// 	},
-	// 	"add": func(i, j int) int {
-	// 		return i + j
-	// 	},
-	// }
+	funcMap := template.FuncMap{
+		"hex": func(i int) string {
+			return fmt.Sprintf("0x%x", i)
+		},
+		"mul": func(a, b int) int {
+			return a * b
+		},
+		"inc": func(i int) int {
+			return i + 1
+		},
+		"frstr": func(x fr.Element) string {
+			// we use big.Int to always get a positive string.
+			// not the most efficient hack, but it works better for .sol generation.
+			bv := new(big.Int)
+			x.BigInt(bv)
+			return bv.String()
+		},
+		"fpstr": func(x fp.Element) string {
+			bv := new(big.Int)
+			x.BigInt(bv)
+			return bv.String()
+		},
+		"add": func(i, j int) int {
+			return i + j
+		},
+	}
 
-	// t, err := template.New("t").Funcs(funcMap).Parse(tmplSolidityVerifier)
-	// if err != nil {
-	// 	return err
-	// }
-	// return t.Execute(w, vk)
-	return nil
+	t, err := template.New("t").Funcs(funcMap).Parse(tmplSolidityVerifier)
+	if err != nil {
+		return err
+	}
+	return t.Execute(w, vk)
 }
