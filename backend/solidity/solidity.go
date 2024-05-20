2b2364102f03cbbc4a5e0cab9d4869551a400539
Thomas Piellard <thomas.piellard@consensys.net>
2024-05-20 22:40:53 +0200

    feat: addition of export options for solidity verifiers

---
+++ backend/solidity/solidity.go
@@ -0,0 +1,33 @@
+package solidity
+
+// ExportOption defines option for altering the behavior of the prover in
+// Prove, ReadAndProve and IsSolved methods. See the descriptions of functions
+// returning instances of this type for implemented options.
+type ExportOption func(*ExportConfig) error
+
+// ExportConfig is the configuration for the prover with the options applied.
+type ExportConfig struct {
+	PragmaVersion string
+}
+
+// NewExportConfig returns a default ExportConfig with given export options opts
+// applied.
+func NewExportConfig(opts ...ExportOption) (ExportConfig, error) {
+	config := ExportConfig{
+		PragmaVersion: "0.8.24",
+	}
+	for _, option := range opts {
+		if err := option(&config); err != nil {
+			return ExportConfig{}, err
+		}
+	}
+	return config, nil
+}
+
+// WithPragmaVerions changes the pragma version used in the solidit verifier.
+func WithPragmaVerions(version string) ExportOption {
+	return func(cfg *ExportConfig) error {
+		cfg.PragmaVersion = version
+		return nil
+	}
+}
