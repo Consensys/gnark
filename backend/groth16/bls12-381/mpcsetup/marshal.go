41125bc1909c00e50cbfc206556960b163d52b5a
John Guibas <jtguibas@gmail.com>
2024-09-23 16:48:30 -0700

    fix eval contribution encoding

--- backend/groth16/bls12-381/mpcsetup/marshal.go
+++ backend/groth16/bls12-381/mpcsetup/marshal.go
@@ -151,6 +151,7 @@ func (c *Phase2Evaluations) WriteTo(writer io.Writer) (int64, error) {
 		c.G1.A,
 		c.G1.B,
 		c.G2.B,
+		c.G1.VKK,
 	}
 
 	for _, v := range toEncode {
@@ -169,6 +170,7 @@ func (c *Phase2Evaluations) ReadFrom(reader io.Reader) (int64, error) {
 		&c.G1.A,
 		&c.G1.B,
 		&c.G2.B,
+		&c.G1.VKK,
 	}
 
 	for _, v := range toEncode {
