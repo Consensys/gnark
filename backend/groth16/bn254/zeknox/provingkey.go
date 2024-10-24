2dd91be13e5e62764586a6a42fd9cea193936173
Jason.Huang <20609724+doutv@users.noreply.github.com>
2024-10-24 10:20:06 +0800

    delete unused deviceInfo

--- backend/groth16/bn254/zeknox/provingkey.go
+++ backend/groth16/bn254/zeknox/provingkey.go
@@ -1,10 +1,8 @@
 package zeknox_bn254
 
 import (
-	"unsafe"
-
-	groth16_bn254 "github.com/consensys/gnark/backend/groth16/bn254"
 	"github.com/consensys/gnark-crypto/ecc/bn254"
+	groth16_bn254 "github.com/consensys/gnark/backend/groth16/bn254"
 	cs "github.com/consensys/gnark/constraint/bn254"
 	"github.com/okx/cryptography_cuda/wrappers/go/device"
 )
@@ -13,14 +11,9 @@ type deviceInfo struct {
 	G1Device struct {
 		A, B, K, Z *device.HostOrDeviceSlice[bn254.G1Affine]
 	}
-	DomainDevice struct {
-		Twiddles, TwiddlesInv     unsafe.Pointer
-		CosetTable, CosetTableInv unsafe.Pointer
-	}
 	G2Device struct {
 		B *device.HostOrDeviceSlice[bn254.G2Affine]
 	}
-	DenDevice             unsafe.Pointer
 	InfinityPointIndicesK []int
 }
 
