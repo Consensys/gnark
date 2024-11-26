59155c822a41b3200eaeffeabed11f36f16d4bd3
Ivo Kubjas <ivo.kubjas@consensys.net>
2024-11-26 13:03:29 +0000

    fix: fail early when initializing backend

--- backend/groth16/bn254/icicle/device.go
+++ backend/groth16/bn254/icicle/device.go
@@ -1,6 +1,7 @@
 package icicle
 
 import (
+	"fmt"
 	"sync"
 
 	icicle_runtime "github.com/ingonyama-zk/icicle/v3/wrappers/golang/runtime"
@@ -10,10 +11,16 @@ var onceWarmUpDevice sync.Once
 
 func warmUpDevice() {
 	onceWarmUpDevice.Do(func() {
-		icicle_runtime.LoadBackendFromEnvOrDefault()
+		err := icicle_runtime.LoadBackendFromEnvOrDefault()
+		if err != icicle_runtime.Success {
+			panic(fmt.Sprintf("ICICLE backend loading error: %s", err.AsString()))
+		}
 		device := icicle_runtime.CreateDevice("CUDA", 0)
 		icicle_runtime.RunOnDevice(&device, func(args ...any) {
-			icicle_runtime.WarmUpDevice()
+			err := icicle_runtime.WarmUpDevice()
+			if err != icicle_runtime.Success {
+				panic(fmt.Sprintf("ICICLE device warmup error: %s", err.AsString()))
+			}
 		})
 	})
 }
