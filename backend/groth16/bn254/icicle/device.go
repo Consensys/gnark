9b8187944dae332a4681aaaf5f1af5a93dcef4ca
Ivo Kubjas <ivo.kubjas@consensys.net>
2024-11-26 13:03:46 +0000

    feat: log loaded backend

--- backend/groth16/bn254/icicle/device.go
+++ backend/groth16/bn254/icicle/device.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"sync"
 
+	"github.com/consensys/gnark/logger"
 	icicle_runtime "github.com/ingonyama-zk/icicle/v3/wrappers/golang/runtime"
 )
 
@@ -11,11 +12,13 @@ var onceWarmUpDevice sync.Once
 
 func warmUpDevice() {
 	onceWarmUpDevice.Do(func() {
+		log := logger.Logger()
 		err := icicle_runtime.LoadBackendFromEnvOrDefault()
 		if err != icicle_runtime.Success {
 			panic(fmt.Sprintf("ICICLE backend loading error: %s", err.AsString()))
 		}
 		device := icicle_runtime.CreateDevice("CUDA", 0)
+		log.Debug().Int32("id", device.Id).Str("type", device.GetDeviceType()).Msg("ICICLE device created")
 		icicle_runtime.RunOnDevice(&device, func(args ...any) {
 			err := icicle_runtime.WarmUpDevice()
 			if err != icicle_runtime.Success {
