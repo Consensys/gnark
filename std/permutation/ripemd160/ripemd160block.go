ddfaffdab0174c6822f82f808bd1441a81aa5237
Ivo Kubjas <ivo.kubjas@consensys.net>
2024-12-05 16:34:39 +0000

    chore: roll loops as in paper

--- std/permutation/ripemd160/ripemd160block.go
+++ std/permutation/ripemd160/ripemd160block.go
@@ -5,8 +5,7 @@ import (
 	"github.com/consensys/gnark/std/math/uints"
 )
 
-// work buffer indices and roll amounts for one line
-var _n = [80]uint{
+var rLeft = [80]uint{
 	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
 	7, 4, 13, 1, 10, 6, 15, 3, 12, 0, 9, 5, 2, 14, 11, 8,
 	3, 10, 14, 4, 9, 15, 8, 1, 2, 7, 0, 6, 13, 11, 5, 12,
@@ -14,16 +13,7 @@ var _n = [80]uint{
 	4, 0, 5, 9, 7, 12, 2, 10, 14, 1, 3, 8, 11, 6, 15, 13,
 }
 
-var _r = [80]uint{
-	11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8,
-	7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12,
-	11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5,
-	11, 12, 14, 15, 14, 15, 9, 8, 9, 14, 5, 6, 8, 6, 5, 12,
-	9, 15, 5, 11, 6, 8, 13, 12, 5, 12, 13, 14, 11, 8, 5, 6,
-}
-
-// same for the other parallel one
-var n_ = [80]uint{
+var rRight = [80]uint{
 	5, 14, 7, 0, 9, 2, 11, 4, 13, 6, 15, 8, 1, 10, 3, 12,
 	6, 11, 3, 7, 0, 13, 5, 10, 14, 15, 8, 12, 4, 9, 1, 2,
 	15, 5, 1, 3, 7, 14, 6, 9, 11, 8, 12, 2, 10, 0, 4, 13,
@@ -31,7 +21,15 @@ var n_ = [80]uint{
 	12, 15, 10, 4, 1, 5, 8, 7, 6, 2, 13, 14, 0, 3, 9, 11,
 }
 
-var r_ = [80]uint{
+var sLeft = [80]uint{
+	11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8,
+	7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12,
+	11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5,
+	11, 12, 14, 15, 14, 15, 9, 8, 9, 14, 5, 6, 8, 6, 5, 12,
+	9, 15, 5, 11, 6, 8, 13, 12, 5, 12, 13, 14, 11, 8, 5, 6,
+}
+
+var sRight = [80]uint{
 	8, 9, 9, 11, 13, 15, 15, 5, 7, 7, 8, 11, 14, 14, 12, 6,
 	9, 13, 15, 7, 12, 8, 9, 11, 7, 7, 12, 7, 6, 15, 13, 11,
 	9, 7, 15, 11, 8, 6, 6, 14, 12, 13, 5, 14, 13, 13, 7, 5,
@@ -39,263 +37,101 @@ var r_ = [80]uint{
 	8, 5, 12, 9, 12, 5, 14, 6, 8, 13, 6, 5, 15, 13, 11, 11,
 }
 
-var _k = uints.NewU32Array([]uint32{
-	0x50a28be6,
-	0x5a827999, 0x5c4dd124,
-	0x6ed9eba1, 0x6d703ef3,
-	0x8f1bbcdc, 0x7a6d76e9,
+var kLeft = [4]uints.U32(uints.NewU32Array([]uint32{
+	0x5a827999,
+	0x6ed9eba1,
+	0x8f1bbcdc,
 	0xa953fd4e,
-})
+}))
+
+var kRight = [4]uints.U32(uints.NewU32Array([]uint32{
+	0x50a28be6,
+	0x5c4dd124,
+	0x6d703ef3,
+	0x7a6d76e9,
+}))
 
 func Permute(uapi *uints.BinaryField[uints.U32], currentHash [5]uints.U32, p [64]uints.U8) (newHash [5]uints.U32) {
 	var x [16]uints.U32
-	var alpha, beta uints.U32
 	a, b, c, d, e := currentHash[0], currentHash[1], currentHash[2], currentHash[3], currentHash[4]
 	aa, bb, cc, dd, ee := a, b, c, d, e
 	for i := 0; i < 16; i++ {
 		x[i] = uapi.PackLSB(p[4*i], p[4*i+1], p[4*i+2], p[4*i+3])
 	}
-	// round 1
-	for i := 0; i < 16; i++ {
-		// alpha = a + (b ^ c ^ d) + x[_n[i]]
-		alpha = uapi.Add(
-			a,
-			uapi.Xor(b, c, d),
-			x[_n[i]],
-		)
-		// s := int(_r[i])
-		s := int(_r[i])
-		// alpha = bits.RotateLeft32(alpha, s) + e
-		alpha = uapi.Add(
-			uapi.Lrot(alpha, s),
-			e,
-		)
-		// beta = bits.RotateLeft32(c, 10)
-		beta = uapi.Lrot(c, 10)
-		// a, b, c, d, e = e, alpha, b, beta, d
-		a, b, c, d, e = e, alpha, b, beta, d
-
-		// parallel line
-		// alpha = aa + (bb ^ (cc | ^dd)) + x[n_[i]] + 0x50a28be6
-		alpha = uapi.Add(
-			aa,
-			uapi.Xor(bb,
-				uapi.Or(
-					cc,
-					uapi.Not(dd),
-				),
-			),
-			x[n_[i]],
-			_k[0],
-		)
-		// s = int(r_[i])
-		s = int(r_[i])
-		// alpha = bits.RotateLeft32(alpha, s) + ee
-		alpha = uapi.Add(
-			uapi.Lrot(alpha, s),
-			ee,
-		)
-		// beta = bits.RotateLeft32(cc, 10)
-		beta = uapi.Lrot(cc, 10)
-		// aa, bb, cc, dd, ee = ee, alpha, bb, beta, dd
-		aa, bb, cc, dd, ee = ee, alpha, bb, beta, dd
+	for j := 0; j < 80; j++ {
+		a, b, c, d, e = round(uapi, j, true, a, b, c, d, e, x, rLeft, sLeft, kLeft)
+		aa, bb, cc, dd, ee = round(uapi, j, false, aa, bb, cc, dd, ee, x, rRight, sRight, kRight)
 	}
-
-	// round 2
-	for i := 16; i < 32; i++ {
-		// 	alpha = a + (b&c | ^b&d) + x[_n[i]] + 0x5a827999
-		alpha = uapi.Add(
-			a,
-			uapi.Or(
-				uapi.And(b, c),
-				uapi.And(uapi.Not(b), d),
-			),
-			x[_n[i]],
-			_k[1],
-		)
-		// 	s := int(_r[i])
-		s := int(_r[i])
-		// 	alpha = bits.RotateLeft32(alpha, s) + e
-		alpha = uapi.Add(
-			uapi.Lrot(alpha, s),
-			e,
-		)
-		// 	beta = bits.RotateLeft32(c, 10)
-		beta = uapi.Lrot(c, 10)
-		// 	a, b, c, d, e = e, alpha, b, beta, d
-		a, b, c, d, e = e, alpha, b, beta, d
-
-		// parallel line
-		// 	alpha = aa + (bb&dd | cc&^dd) + x[n_[i]] + 0x5c4dd124
-		alpha = uapi.Add(
-			aa,
-			uapi.Or(
-				uapi.And(bb, dd),
-				uapi.And(cc, uapi.Not(dd)),
-			),
-			x[n_[i]],
-			_k[2],
-		)
-		// 	s = int(r_[i])
-		s = int(r_[i])
-		// 	alpha = bits.RotateLeft32(alpha, s) + ee
-		alpha = uapi.Add(
-			uapi.Lrot(alpha, s),
-			ee,
-		)
-		// 	beta = bits.RotateLeft32(cc, 10)
-		beta = uapi.Lrot(cc, 10)
-		// 	aa, bb, cc, dd, ee = ee, alpha, bb, beta, dd
-		aa, bb, cc, dd, ee = ee, alpha, bb, beta, dd
-	}
-
-	// round 3
-	for i := 32; i < 48; i++ {
-		// 	alpha = a + (b | ^c ^ d) + x[_n[i]] + 0x6ed9eba1
-		alpha = uapi.Add(
-			a,
-			uapi.Xor(
-				uapi.Or(b, uapi.Not(c)),
-				d),
-			x[_n[i]],
-			_k[3],
-		)
-		// 	s := int(_r[i])
-		s := int(_r[i])
-		// 	alpha = bits.RotateLeft32(alpha, s) + e
-		alpha = uapi.Add(
-			uapi.Lrot(alpha, s),
-			e,
-		)
-		// 	beta = bits.RotateLeft32(c, 10)
-		beta = uapi.Lrot(c, 10)
-		// 	a, b, c, d, e = e, alpha, b, beta, d
-		a, b, c, d, e = e, alpha, b, beta, d
-
-		// parallel line
-		// 	alpha = aa + (bb | ^cc ^ dd) + x[n_[i]] + 0x6d703ef3
-		alpha = uapi.Add(
-			aa,
-			uapi.Xor(
-				uapi.Or(bb, uapi.Not(cc)),
-				dd),
-			x[n_[i]],
-			_k[4],
-		)
-		// 	s = int(r_[i])
-		s = int(r_[i])
-		// 	alpha = bits.RotateLeft32(alpha, s) + ee
-		alpha = uapi.Add(
-			uapi.Lrot(alpha, s),
-			ee,
-		)
-		// 	beta = bits.RotateLeft32(cc, 10)
-		beta = uapi.Lrot(cc, 10)
-		// 	aa, bb, cc, dd, ee = ee, alpha, bb, beta, dd
-		aa, bb, cc, dd, ee = ee, alpha, bb, beta, dd
-	}
-
-	// round 4
-	for i := 48; i < 64; i++ {
-		// 	alpha = a + (b&d | c&^d) + x[_n[i]] + 0x8f1bbcdc
-		alpha = uapi.Add(
-			a,
-			uapi.Or(
-				uapi.And(b, d),
-				uapi.And(c, uapi.Not(d)),
-			),
-			x[_n[i]],
-			_k[5],
-		)
-		// 	s := int(_r[i])
-		s := int(_r[i])
-		// 	alpha = bits.RotateLeft32(alpha, s) + e
-		alpha = uapi.Add(
-			uapi.Lrot(alpha, s),
-			e,
-		)
-		// 	beta = bits.RotateLeft32(c, 10)
-		beta = uapi.Lrot(c, 10)
-		// 	a, b, c, d, e = e, alpha, b, beta, d
-		a, b, c, d, e = e, alpha, b, beta, d
-
-		// parallel line
-		// 	alpha = aa + (bb&cc | ^bb&dd) + x[n_[i]] + 0x7a6d76e9
-		alpha = uapi.Add(
-			aa,
-			uapi.Or(
-				uapi.And(bb, cc),
-				uapi.And(uapi.Not(bb), dd),
-			),
-			x[n_[i]],
-			_k[6],
-		)
-		// 	s = int(r_[i])
-		s = int(r_[i])
-		// 	alpha = bits.RotateLeft32(alpha, s) + ee
-		alpha = uapi.Add(
-			uapi.Lrot(alpha, s),
-			ee,
-		)
-		// 	beta = bits.RotateLeft32(cc, 10)
-		beta = uapi.Lrot(cc, 10)
-		// 	aa, bb, cc, dd, ee = ee, alpha, bb, beta, dd
-		aa, bb, cc, dd, ee = ee, alpha, bb, beta, dd
-	}
-
-	// round 5
-	for i := 64; i < 80; i++ {
-		// 	alpha = a + (b ^ (c | ^d)) + x[_n[i]] + 0xa953fd4e
-		alpha = uapi.Add(
-			a,
-			uapi.Xor(b, uapi.Or(c, uapi.Not(d))),
-			x[_n[i]],
-			_k[7],
-		)
-		// 	s := int(_r[i])
-		s := int(_r[i])
-		// 	alpha = bits.RotateLeft32(alpha, s) + e
-		alpha = uapi.Add(
-			uapi.Lrot(alpha, s),
-			e,
-		)
-		// 	beta = bits.RotateLeft32(c, 10)
-		beta = uapi.Lrot(c, 10)
-		// 	a, b, c, d, e = e, alpha, b, beta, d
-		a, b, c, d, e = e, alpha, b, beta, d
-
-		// parallel line
-		// 	alpha = aa + (bb ^ cc ^ dd) + x[n_[i]]
-		alpha = uapi.Add(
-			aa,
-			uapi.Xor(bb, cc, dd),
-			x[n_[i]],
-		)
-		// 	s = int(r_[i])
-		s = int(r_[i])
-		// 	alpha = bits.RotateLeft32(alpha, s) + ee
-		alpha = uapi.Add(
-			uapi.Lrot(alpha, s),
-			ee,
-		)
-		// 	beta = bits.RotateLeft32(cc, 10)
-		beta = uapi.Lrot(cc, 10)
-		// 	aa, bb, cc, dd, ee = ee, alpha, bb, beta, dd
-		aa, bb, cc, dd, ee = ee, alpha, bb, beta, dd
-	}
-
-	// combine results
-	// dd += c + md.s[1]
-	// md.s[1] = md.s[2] + d + ee
-	// md.s[2] = md.s[3] + e + aa
-	// md.s[3] = md.s[4] + a + bb
-	// md.s[4] = md.s[0] + b + cc
-	// md.s[0] = dd
 	newHash[0] = uapi.Add(currentHash[1], c, dd)
 	newHash[1] = uapi.Add(currentHash[2], d, ee)
 	newHash[2] = uapi.Add(currentHash[3], e, aa)
 	newHash[3] = uapi.Add(currentHash[4], a, bb)
 	newHash[4] = uapi.Add(currentHash[0], b, cc)
+	return
+}
 
+func f(uapi *uints.BinaryField[uints.U32], j int, x, y, z uints.U32) uints.U32 {
+	if j < 16 {
+		// x ^ y ^ z
+		return uapi.Xor(x, y, z)
+	}
+	if j < 32 {
+		// (x & y) | (~x & z)
+		return uapi.Or(uapi.And(x, y), uapi.And(uapi.Not(x), z))
+	}
+	if j < 48 {
+		// (x | ~y) ^ z
+		return uapi.Xor(
+			uapi.Or(x, uapi.Not(y)),
+			z,
+		)
+	}
+	if j < 64 {
+		// (x & z) | (y & ~z)
+		return uapi.Or(
+			uapi.And(x, z),
+			uapi.And(y, uapi.Not(z)),
+		)
+	}
+	// x ^ (y | ~z)
+	return uapi.Xor(
+		x,
+		uapi.Or(y, uapi.Not(z)),
+	)
+}
+
+func round(uapi *uints.BinaryField[uints.U32], j int, isLeft bool, A, B, C, D, E uints.U32, X_i [16]uints.U32, r, s [80]uint, K [4]uints.U32) (AA, BB, CC, DD, EE uints.U32) {
+	var tmp1 uints.U32
+	jj := j
+	jjj := j / 16
+	if !isLeft {
+		jj = 79 - j
+	} else {
+		jjj = jjj - 1
+	}
+	ff := f(uapi, jj, B, C, D)
+	if (isLeft && j < 16) || (!isLeft && j >= 64) {
+		tmp1 = uapi.Add(
+			A,
+			ff,
+			X_i[r[j]],
+		)
+	} else {
+		tmp1 = uapi.Add(
+			A,
+			ff,
+			X_i[r[j]],
+			K[jjj],
+		)
+	}
+	T := uapi.Add(
+		uapi.Lrot(tmp1, int(s[j])),
+		E,
+	)
+	AA = E
+	BB = T
+	CC = B
+	DD = uapi.Lrot(C, 10)
+	EE = D
 	return
 }
