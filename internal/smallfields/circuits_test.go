cd640016f6ebe7d03153ea39d885d349304ec430
Ivo Kubjas <ivo.kubjas@consensys.net>
2024-12-10 00:25:59 +0000

    XXX tests

--- internal/smallfields/circuits_test.go
+++ internal/smallfields/circuits_test.go
@@ -1,10 +1,13 @@
 package smallfields_test
 
 import (
+	"crypto/rand"
 	"fmt"
 	"math/big"
 	"testing"
 
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark-crypto/ecc/bn254"
 	"github.com/consensys/gnark-crypto/field/babybear"
 	"github.com/consensys/gnark-crypto/field/goldilocks"
 	"github.com/consensys/gnark-crypto/field/koalabear"
@@ -12,9 +15,13 @@ import (
 	"github.com/consensys/gnark/frontend/cs/r1cs"
 	"github.com/consensys/gnark/frontend/cs/scs"
 	"github.com/consensys/gnark/internal/smallfields/tinyfield"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
 	"github.com/consensys/gnark/std/math/emulated"
 	"github.com/consensys/gnark/std/math/emulated/emparams"
 	"github.com/consensys/gnark/test"
+
+	babybearcs "github.com/consensys/gnark/constraint/babybear"
+	bls12377cs "github.com/consensys/gnark/constraint/bls12-377"
 )
 
 type NativeCircuit struct {
@@ -84,11 +91,11 @@ type smallBN struct {
 }
 
 func (smallBN) BitsPerLimb() uint {
-	return 11
+	return 10
 }
 
 func (smallBN) NbLimbs() uint {
-	return 24
+	return 26
 }
 
 type EmulatedCircuit[T emulated.FieldParams] struct {
@@ -109,15 +116,139 @@ func (c *EmulatedCircuit[T]) Define(api frontend.API) error {
 func TestEmulatedCircuit(t *testing.T) {
 	assert := test.NewAssert(t)
 
-	err := test.IsSolved(&EmulatedCircuit[emparams.BN254Fp]{}, &EmulatedCircuit[emparams.BN254Fp]{A: emulated.ValueOf[emparams.BN254Fp](2), B: emulated.ValueOf[emparams.BN254Fp](4)}, goldilocks.Modulus())
-	assert.Error(err)
+	a, err := rand.Int(rand.Reader, emparams.BN254Fp{}.Modulus())
+	assert.NoError(err)
+	b := new(big.Int).Mul(a, a)
+	b.Mod(b, emparams.BN254Fp{}.Modulus())
 
-	err = test.IsSolved(&EmulatedCircuit[smallBN]{}, &EmulatedCircuit[smallBN]{A: emulated.ValueOf[smallBN](2), B: emulated.ValueOf[smallBN](4)}, goldilocks.Modulus())
+	err = test.IsSolved(&EmulatedCircuit[emparams.BN254Fp]{}, &EmulatedCircuit[emparams.BN254Fp]{A: emulated.ValueOf[emparams.BN254Fp](a), B: emulated.ValueOf[emparams.BN254Fp](b)}, ecc.BN254.ScalarField())
 	assert.NoError(err)
 
-	err = test.IsSolved(&EmulatedCircuit[emparams.BN254Fp]{}, &EmulatedCircuit[emparams.BN254Fp]{A: emulated.ValueOf[emparams.BN254Fp](2), B: emulated.ValueOf[emparams.BN254Fp](4)}, babybear.Modulus())
-	assert.Error(err)
-
-	err = test.IsSolved(&EmulatedCircuit[smallBN]{}, &EmulatedCircuit[smallBN]{A: emulated.ValueOf[smallBN](2), B: emulated.ValueOf[smallBN](4)}, babybear.Modulus())
+	err = test.IsSolved(&EmulatedCircuit[emparams.BN254Fp]{}, &EmulatedCircuit[emparams.BN254Fp]{A: emulated.ValueOf[emparams.BN254Fp](a), B: emulated.ValueOf[emparams.BN254Fp](b)}, goldilocks.Modulus())
 	assert.NoError(err)
+
+	err = test.IsSolved(&EmulatedCircuit[smallBN]{}, &EmulatedCircuit[smallBN]{A: emulated.ValueOf[smallBN](a), B: emulated.ValueOf[smallBN](b)}, goldilocks.Modulus())
+	assert.NoError(err)
+
+	err = test.IsSolved(&EmulatedCircuit[emparams.BN254Fp]{}, &EmulatedCircuit[emparams.BN254Fp]{A: emulated.ValueOf[emparams.BN254Fp](a), B: emulated.ValueOf[emparams.BN254Fp](b)}, babybear.Modulus())
+	assert.NoError(err)
+
+	err = test.IsSolved(&EmulatedCircuit[smallBN]{}, &EmulatedCircuit[smallBN]{A: emulated.ValueOf[smallBN](a), B: emulated.ValueOf[smallBN](b)}, babybear.Modulus())
+	assert.NoError(err)
+}
+
+func TestCompileEmulatedCircuit(t *testing.T) {
+	assert := test.NewAssert(t)
+	f := babybear.Modulus()
+
+	circuit := &EmulatedCircuit[smallBN]{}
+	assignemnt := &EmulatedCircuit[smallBN]{A: emulated.ValueOf[smallBN](2), B: emulated.ValueOf[smallBN](4)}
+
+	ccs, err := frontend.CompileU32(f, scs.NewBuilder, circuit)
+	assert.NoError(err)
+
+	w, err := frontend.NewWitness(assignemnt, f)
+	assert.NoError(err)
+
+	res, err := ccs.Solve(w)
+	assert.NoError(err)
+
+	tres, ok := res.(*babybearcs.SparseR1CSSolution)
+	assert.True(ok)
+
+	fmt.Println(tres.L.String())
+	fmt.Println(tres.R.String())
+	fmt.Println(tres.O.String())
+
+	ccs2, err := frontend.Compile(f, r1cs.NewBuilder, circuit)
+	assert.NoError(err)
+
+	res2, err := ccs2.Solve(w)
+	assert.NoError(err)
+
+	tres2, ok := res2.(*babybearcs.R1CSSolution)
+	assert.True(ok)
+
+	fmt.Println(tres2.W.String())
+	fmt.Println(tres2.A.String())
+	fmt.Println(tres2.B.String())
+	fmt.Println(tres2.C.String())
+}
+
+type PairCircuit struct {
+	InG1 sw_bn254.G1Affine
+	InG2 sw_bn254.G2Affine
+	Res  sw_bn254.GTEl
+}
+
+func (c *PairCircuit) Define(api frontend.API) error {
+	pairing, err := sw_bn254.NewPairing(api)
+	if err != nil {
+		return fmt.Errorf("new pairing: %w", err)
+	}
+	pairing.AssertIsOnG1(&c.InG1)
+	pairing.AssertIsOnG2(&c.InG2)
+	res, err := pairing.Pair([]*sw_bn254.G1Affine{&c.InG1}, []*sw_bn254.G2Affine{&c.InG2})
+	if err != nil {
+		return fmt.Errorf("pair: %w", err)
+	}
+	pairing.AssertIsEqual(res, &c.Res)
+	return nil
+}
+
+func TestPairTestSolve(t *testing.T) {
+	assert := test.NewAssert(t)
+	p, q := randomG1G2Affines()
+	res, err := bn254.Pair([]bn254.G1Affine{p}, []bn254.G2Affine{q})
+	assert.NoError(err)
+	witness := PairCircuit{
+		InG1: sw_bn254.NewG1Affine(p),
+		InG2: sw_bn254.NewG2Affine(q),
+		Res:  sw_bn254.NewGTEl(res),
+	}
+	err = test.IsSolved(&PairCircuit{}, &witness, babybear.Modulus())
+	assert.NoError(err)
+
+	// ccs, err := frontend.Compile(babybear.Modulus(), scs.NewBuilder, &PairCircuit{})
+	// assert.NoError(err)
+	// _ = ccs
+
+	// w, err := frontend.NewWitness(&witness, babybear.Modulus())
+	// assert.NoError(err)
+
+	// sol, err := ccs.Solve(w)
+	// assert.NoError(err)
+
+	// tres, ok := sol.(*babybearcs.SparseR1CSSolution)
+	// assert.True(ok)
+	// fmt.Println(tres.L.Len())
+
+	ccs2, err := frontend.Compile(ecc.BLS12_377.ScalarField(), scs.NewBuilder, &PairCircuit{})
+	_ = ccs2
+	assert.NoError(err)
+	w2, err := frontend.NewWitness(&witness, ecc.BLS12_377.ScalarField())
+	assert.NoError(err)
+	sol2, err := ccs2.Solve(w2)
+	assert.NoError(err)
+	tres, ok := sol2.(*bls12377cs.SparseR1CSSolution)
+	assert.True(ok)
+	fmt.Println(tres.L.Len())
+}
+
+func randomG1G2Affines() (bn254.G1Affine, bn254.G2Affine) {
+	_, _, G1AffGen, G2AffGen := bn254.Generators()
+	mod := bn254.ID.ScalarField()
+	s1, err := rand.Int(rand.Reader, mod)
+	if err != nil {
+		panic(err)
+	}
+	s2, err := rand.Int(rand.Reader, mod)
+	if err != nil {
+		panic(err)
+	}
+	var p bn254.G1Affine
+	p.ScalarMultiplication(&G1AffGen, s1)
+	var q bn254.G2Affine
+	q.ScalarMultiplication(&G2AffGen, s2)
+	return p, q
 }
