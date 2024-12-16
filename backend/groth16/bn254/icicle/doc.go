9154785015cc6b8b028416d9a9c7767188dc6327
Ivo Kubjas <ivo.kubjas@consensys.net>
2024-12-16 10:58:50 +0000

    Revert "Update ICICLE integration to use v3 ICICLE (#1318)"
    This reverts commit b04a6884de8de776f2d3803ec9f81892a8ae5886.
    
    Build fails when dependencies not installed.

--- backend/groth16/bn254/icicle/doc.go
+++ backend/groth16/bn254/icicle/doc.go
@@ -1,2 +1,2 @@
 // Package icicle_bn254 implements ICICLE acceleration for BN254 Groth16 backend.
-package icicle
+package icicle_bn254
--- backend/groth16/bn254/icicle/provingkey.go
+++ backend/groth16/bn254/icicle/provingkey.go
@@ -1,26 +1,25 @@
-package icicle
+package icicle_bn254
 
 import (
-	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+	"unsafe"
+
 	groth16_bn254 "github.com/consensys/gnark/backend/groth16/bn254"
 	cs "github.com/consensys/gnark/constraint/bn254"
-	icicle_core "github.com/ingonyama-zk/icicle/v3/wrappers/golang/core"
 )
 
 type deviceInfo struct {
-	CosetGenerator [fr.Limbs * 2]uint32
-	G1Device       struct {
-		A, B, K, Z icicle_core.DeviceSlice
+	G1Device struct {
+		A, B, K, Z unsafe.Pointer
+	}
+	DomainDevice struct {
+		Twiddles, TwiddlesInv     unsafe.Pointer
+		CosetTable, CosetTableInv unsafe.Pointer
 	}
 	G2Device struct {
-		B icicle_core.DeviceSlice
+		B unsafe.Pointer
 	}
-	DenDevice icicle_core.DeviceSlice
-
-	CommitmentKeysDevice struct {
-		Basis         []icicle_core.DeviceSlice
-		BasisExpSigma []icicle_core.DeviceSlice // we compute in batch
-	}
+	DenDevice             unsafe.Pointer
+	InfinityPointIndicesK []int
 }
 
 type ProvingKey struct {
@@ -28,17 +27,10 @@ type ProvingKey struct {
 	*deviceInfo
 }
 
-func NewProvingKey() *ProvingKey {
-	warmUpDevice()
-	return &ProvingKey{}
-}
-
 func Setup(r1cs *cs.R1CS, pk *ProvingKey, vk *groth16_bn254.VerifyingKey) error {
-	warmUpDevice()
 	return groth16_bn254.Setup(r1cs, &pk.ProvingKey, vk)
 }
 
 func DummySetup(r1cs *cs.R1CS, pk *ProvingKey) error {
-	warmUpDevice()
 	return groth16_bn254.DummySetup(r1cs, &pk.ProvingKey)
 }
