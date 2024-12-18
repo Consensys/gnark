74de36a62779e8098e47051235ad972a13111b10
Arya Tabaie <15056835+Tabaie@users.noreply.github.com>
2024-12-18 16:18:51 -0600

    feat a couple of helper functions

--- internal/utils/test_utils/test_utils.go
+++ internal/utils/test_utils/test_utils.go
@@ -1,6 +1,12 @@
 package test_utils
 
-import "log"
+import (
+	"bytes"
+	"github.com/stretchr/testify/require"
+	"io"
+	"log"
+	"testing"
+)
 
 var ConditionalLoggerEnabled bool
 
@@ -14,4 +20,35 @@ func ConditionalLogf(format string, v ...any) {
 	if ConditionalLoggerEnabled {
 		log.Printf(format, v...)
 	}
+}
+
+// Range (n, startingPoints...) = [startingPoints[0], startingPoints[0]+1, ..., startingPoints[0]+n-1, startingPoints[1], startingPoints[1]+1, ...,]
+// or [0, 1, ..., n-1] if startingPoints is empty
+func Range(n int, startingPoints ...int) []int {
+	if len(startingPoints) == 0 {
+		startingPoints = []int{0}
+	}
+	res := make([]int, n*len(startingPoints))
+
+	for i := range startingPoints {
+		for j := range n {
+			res[i*n+j] = startingPoints[i] + j
+		}
+	}
+
+	return res
+}
+
+func CopyThruSerialization(t *testing.T, dst, src interface {
+	io.ReaderFrom
+	io.WriterTo
+}) {
+	var bb bytes.Buffer
+
+	n, err := src.WriteTo(&bb)
+	require.NoError(t, err)
+	require.Equal(t, int64(bb.Len()), n)
+	n, err = dst.ReadFrom(bytes.NewReader(bb.Bytes()))
+	require.NoError(t, err)
+	require.Equal(t, int64(bb.Len()), n)
 }
