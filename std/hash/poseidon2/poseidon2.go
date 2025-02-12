d6fcd5d92e9239b265865cf477f43eebeb50e52e
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-02-12 23:34:43 +0000

    refactor: rename constructor to align with gnark-crypto

--- std/hash/poseidon2/poseidon2.go
+++ std/hash/poseidon2/poseidon2.go
@@ -8,7 +8,9 @@ import (
 	poseidon2 "github.com/consensys/gnark/std/permutation/poseidon2"
 )
 
-func NewPoseidon2(api frontend.API) (hash.FieldHasher, error) {
+// NewMerkleDamgardHasher returns a Poseidon2 hasher using the Merkle-Damgard
+// construction with the default parameters.
+func NewMerkleDamgardHasher(api frontend.API) (hash.FieldHasher, error) {
 	f, err := poseidon2.NewPoseidon2(api)
 	if err != nil {
 		return nil, fmt.Errorf("could not create poseidon2 hasher: %w", err)
--- std/hash/poseidon2/poseidon2_test.go
+++ std/hash/poseidon2/poseidon2_test.go
@@ -1,12 +1,13 @@
 package poseidon2
 
 import (
+	"testing"
+
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr/poseidon2"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/test"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func TestPoseidon2Hash(t *testing.T) {
@@ -19,7 +20,7 @@ func TestPoseidon2Hash(t *testing.T) {
 	res := h.Sum(nil)
 
 	test.SingleFunction(ecc.BLS12_377, func(api frontend.API) []frontend.Variable {
-		hsh, err := NewPoseidon2(api)
+		hsh, err := NewMerkleDamgardHasher(api)
 		require.NoError(t, err)
 		hsh.Write(0, 1, 2, 3, 4)
 		return []frontend.Variable{hsh.Sum()}
