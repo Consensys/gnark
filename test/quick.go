2fcd4a920c1fe17ea21d11b27029c131fdfcd762
Arya Tabaie <15056835+Tabaie@users.noreply.github.com>
2025-02-14 16:31:33 -0600

    refactor: mature SingleFunctionTest

--- test/quick.go
+++ test/quick.go
@@ -4,48 +4,47 @@ import (
 	"crypto/rand"
 	"encoding/binary"
 	"errors"
-	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/frontend"
 	"github.com/stretchr/testify/require"
+	"sync"
 	"testing"
 )
 
-var snarkFunctionStore = make(map[uint64]func(frontend.API) []frontend.Variable) // todo make thread safe
+var snarkFunctionStore sync.Map
+
 type snarkFunctionTestCircuit struct {
-	Outs   []frontend.Variable
-	funcId uint64 // this workaround is necessary because deepEquals fails on objects with function fields
+	funcId     uint64            // this workaround is necessary because deepEquals fails on objects with function fields
+	DummyInput frontend.Variable // to keep the Plonk backend from crashing
 }
 
 func (c *snarkFunctionTestCircuit) Define(api frontend.API) error {
-	outs := snarkFunctionStore[c.funcId](api)
-	delete(snarkFunctionStore, c.funcId)
 
-	// todo replace with SliceEquals
-	if len(outs) != len(c.Outs) {
-		return errors.New("SingleFunction: unexpected number of output")
+	f, ok := snarkFunctionStore.Load(c.funcId)
+	if !ok {
+		return errors.New("function not found")
 	}
-	for i := range outs {
-		api.AssertIsEqual(outs[i], c.Outs[i])
+
+	F, ok := f.(func(frontend.API) error)
+	if !ok {
+		panic("unexpected entry type")
 	}
-	return nil
+
+	return F(api)
 }
 
-// SingleFunction returns a test function that can run a simple circuit consisting of function f, and match its output with outs
-func SingleFunction(curve ecc.ID, f func(frontend.API) []frontend.Variable, outs ...frontend.Variable) func(*testing.T) {
-
+// Function returns a test function that can run a simple circuit consisting of function f
+func Function(f func(frontend.API) error, opts ...TestingOption) func(*testing.T) {
 	return func(t *testing.T) {
-		c := snarkFunctionTestCircuit{
-			Outs: make([]frontend.Variable, len(outs)),
-		}
-		var b [8]byte
+		var (
+			c snarkFunctionTestCircuit
+			b [8]byte
+		)
 		_, err := rand.Read(b[:])
 		require.NoError(t, err)
 		c.funcId = binary.BigEndian.Uint64(b[:])
-		snarkFunctionStore[c.funcId] = f
+		snarkFunctionStore.Store(c.funcId, f)
 
-		a := snarkFunctionTestCircuit{
-			Outs: outs,
-		}
-		require.NoError(t, IsSolved(&c, &a, curve.ScalarField()))
+		NewAssert(t).SolvingSucceeded(&c, &snarkFunctionTestCircuit{DummyInput: 0}, opts...)
+		snarkFunctionStore.Delete(c.funcId)
 	}
 }
