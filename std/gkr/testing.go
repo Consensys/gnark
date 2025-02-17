883c46f4485d93d7be16abfb0c88934b96546ffb
Arya Tabaie <15056835+Tabaie@users.noreply.github.com>
2025-02-16 21:56:31 -0600

    docs: incorporate @ivokub's suggestions in SolveAll doc

--- std/gkr/testing.go
+++ std/gkr/testing.go
@@ -23,9 +23,9 @@ import (
 	"math/big"
 )
 
-// SolveInTestEngine solves the circuit.
-// The output is the values of all variables, across all instances.
-// NOTE: It only works with the test engine.
+// SolveInTestEngine solves the defined circuit directly inside the SNARK circuit. This means that the method does not compute the GKR proof of the circuit and does not embed the GKR proof verifier inside a SNARK.
+// The output is the values of all variables, across all instances; i.e. indexed variable-first, instance-second.
+// This method only works under the test engine and should only be called to debug a GKR circuit, as the GKR prover's errors can be obscure.
 func (api *API) SolveInTestEngine(parentApi frontend.API) [][]frontend.Variable {
 	res := make([][]frontend.Variable, len(api.toStore.Circuit))
 	degreeTestedGates := make(map[string]struct{})
