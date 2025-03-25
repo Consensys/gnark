a7ef40d1bd9b4544c826bcaa1a6316fdfdd07912
Thomas Piellard <thomas.piellard@consensys.net>
2025-03-25 15:06:41 +0100

    feat: corrected name

--- std/evmprecompiles/16-blsmaptog1.go
+++ std/evmprecompiles/16-blsmaptog1.go
@@ -7,10 +7,10 @@ import (
 	"github.com/consensys/gnark/std/math/emulated"
 )
 
-// ECMapToG1 implements[BLS12_MAP_FP_TO_G1] precompile contract at adress 0x10.
+// ECMapToG1BLS implements[BLS12_MAP_FP_TO_G1] precompile contract at adress 0x10.
 //
-// [ECMapToG1]: https://eips.ethereum.org/EIPS/eip-2537
-func ECMapToG1(api frontend.API, u *emulated.Element[emulated.BLS12381Fp]) *sw_emulated.AffinePoint[emulated.BLS12381Fp] {
+// [ECMapToG1BLS]: https://eips.ethereum.org/EIPS/eip-2537
+func ECMapToG1BLS(api frontend.API, u *emulated.Element[emulated.BLS12381Fp]) *sw_emulated.AffinePoint[emulated.BLS12381Fp] {
 
 	res, err := sw_bls12381.MapToG1(api, u)
 	if err != nil {
