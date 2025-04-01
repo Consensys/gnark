0e517ded642edfb5aaa1e9cca2a99cc200d56495
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-04-01 00:28:16 +0000

    test: remove non-native test over small field (other PR)

--- internal/smallfields/circuits_test.go
+++ internal/smallfields/circuits_test.go
@@ -3,10 +3,11 @@ package smallfields_test
 import (
 	"testing"
 
+	"github.com/consensys/gnark-crypto/field/babybear"
 	"github.com/consensys/gnark-crypto/field/goldilocks"
+	"github.com/consensys/gnark-crypto/field/koalabear"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/std/math/emulated"
-	"github.com/consensys/gnark/std/math/emulated/emparams"
 	"github.com/consensys/gnark/test"
 )
 
@@ -41,26 +42,10 @@ func TestNativeCircuit(t *testing.T) {
 
 	err := test.IsSolved(&NativeCircuit{}, &NativeCircuit{A: 2, B: 4}, goldilocks.Modulus())
 	assert.NoError(err)
-}
 
-type smallBN struct {
-	emparams.BN254Fp
-}
+	err = test.IsSolved(&NativeCircuit{}, &NativeCircuit{A: 2, B: 4}, babybear.Modulus())
+	assert.NoError(err)
 
-func (smallBN) BitsPerLimb() uint {
-	return 16
-}
-
-func (smallBN) NbLimbs() uint {
-	return 16
-}
-
-func TestEmulatedCircuit(t *testing.T) {
-	assert := test.NewAssert(t)
-
-	err := test.IsSolved(&EmulatedCircuit[emparams.BN254Fp]{}, &EmulatedCircuit[emparams.BN254Fp]{A: emulated.ValueOf[emparams.BN254Fp](2), B: emulated.ValueOf[emparams.BN254Fp](4)}, goldilocks.Modulus())
-	assert.Error(err)
-
-	err = test.IsSolved(&EmulatedCircuit[smallBN]{}, &EmulatedCircuit[smallBN]{A: emulated.ValueOf[smallBN](2), B: emulated.ValueOf[smallBN](4)}, goldilocks.Modulus())
+	err = test.IsSolved(&NativeCircuit{}, &NativeCircuit{A: 2, B: 4}, koalabear.Modulus())
 	assert.NoError(err)
 }
