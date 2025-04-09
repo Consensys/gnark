b2c738b9422029fe4b76e966e564f9eecfd12f3d
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-04-09 08:01:38 +0000

    XXX: work refactor hash to g2

--- std/algebra/emulated/sw_bls12381/hash_to_g2.go
+++ std/algebra/emulated/sw_bls12381/hash_to_g2.go
@@ -4,6 +4,8 @@ import (
 	"math/big"
 	"slices"
 
+	bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381"
+	"github.com/consensys/gnark-crypto/ecc/bls12-381/hash_to_curve"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/std/algebra/emulated/fields_bls12381"
 	"github.com/consensys/gnark/std/hash/tofield"
@@ -372,4 +374,113 @@ func clearCofactor(g2 *G2, fp *emulated.Field[emparams.BLS12381Fp], p *G2Affine)
 	Q := g2.sub(t3, p)
 	// 11. return Q
 	return Q
+}
+
+func (g2 *G2) evalFixedPolynomial(monic bool, coefficients []bls12381.E2, x *fields_bls12381.E2) *fields_bls12381.E2 {
+	emuCoefficients := make([]*fields_bls12381.E2, len(coefficients))
+	for i := 0; i < len(coefficients); i++ {
+		emuCoefficients[i] = &fields_bls12381.E2{
+			A0: emulated.ValueOf[emparams.BLS12381Fp](coefficients[i].A0),
+			A1: emulated.ValueOf[emparams.BLS12381Fp](coefficients[i].A1),
+		}
+	}
+	var res *fields_bls12381.E2
+	if monic {
+		res = g2.Add(emuCoefficients[0], x)
+	} else {
+		res = emuCoefficients[len(emuCoefficients)-1]
+	}
+
+	for i := len(emuCoefficients) - 2; i >= 0; i-- {
+		res = g2.Mul(res, x)
+		res = g2.Add(res, emuCoefficients[i])
+	}
+
+	return res
+}
+
+func (g2 *G2) isogeny(p *G2Affine) *G2Affine {
+	isogenyMap := hash_to_curve.G2IsogenyMap()
+	ydenom := g2.evalFixedPolynomial(true, isogenyMap[3], &p.P.X)
+	xdenom := g2.evalFixedPolynomial(true, isogenyMap[1], &p.P.X)
+	y := g2.evalFixedPolynomial(false, isogenyMap[2], &p.P.X)
+	y = g2.Mul(y, &p.P.Y)
+	x := g2.evalFixedPolynomial(false, isogenyMap[0], &p.P.X)
+	x = g2.DivUnchecked(x, xdenom)
+	y = g2.DivUnchecked(y, ydenom)
+	return &G2Affine{P: g2AffP{X: *x, Y: *y}}
+}
+
+func (g2 *G2) sgn0(x *fields_bls12381.E2) frontend.Variable {
+	x0Bits := g2.fp.ToBitsCanonical(&x.A0)
+	x1Bits := g2.fp.ToBitsCanonical(&x.A1)
+	sign0 := x0Bits[0]
+	zero0 := g2.api.IsZero(sign0)
+	sign1 := x1Bits[0]
+	tv := g2.api.And(zero0, sign1)
+	s := g2.api.Or(sign0, tv)
+	return s
+}
+
+func (g2 *G2) MapToCurve2(u *fields_bls12381.E2) *G2Affine {
+	// SSWU Steps:
+	// 1.  tv1 = u^2
+	tv1 := g2.Ext2.Square(u)
+	// 2.  tv1 = Z * tv1
+	tv1 = g2.Ext2.Mul(m.Z, tv1)
+	// 3.  tv2 = tv1^2
+	tv2 := g2.Ext2.Square(tv1)
+	// 4.  tv2 = tv2 + tv1
+	tv2 = g2.Ext2.Add(tv2, tv1)
+	// 5.  tv3 = tv2 + 1
+	tv3 := g2.Ext2.Add(tv2, g2.ext2.One())
+	// 6.  tv3 = B * tv3
+	tv3 = g2.Ext2.Mul(&m.B, tv3)
+	// 7.  tv4 = CMOV(Z, -tv2, tv2 != 0)
+	s1 := g2.Ext2.IsZero(tv2)
+	tv4 := g2.Ext2.Select(s1, &m.Z, m.ext2.Neg(tv2))
+	// 8.  tv4 = A * tv4
+	tv4 = g2.Ext2.Mul(&m.A, tv4)
+	// 9.  tv2 = tv3^2
+	tv2 = g2.Ext2.Square(tv3)
+	// 10. tv6 = tv4^2
+	tv6 := g2.Ext2.Square(tv4)
+	// 11. tv5 = A * tv6
+	tv5 := g2.Ext2.Mul(&m.A, tv6)
+	// 12. tv2 = tv2 + tv5
+	tv2 = g2.Ext2.Add(tv2, tv5)
+	// 13. tv2 = tv2 * tv3
+	tv2 = g2.Ext2.Mul(tv2, tv3)
+	// 14. tv6 = tv6 * tv4
+	tv6 = g2.Ext2.Mul(tv6, tv4)
+	// 15. tv5 = B * tv6
+	tv5 = g2.Ext2.Mul(&m.B, tv6)
+	// 16. tv2 = tv2 + tv5
+	tv2 = g2.Ext2.Add(tv2, tv5)
+	// 17.   x = tv1 * tv3
+	x := g2.Ext2.Mul(tv1, tv3)
+	// 18. (is_gx1_square, y1) = sqrt_ratio(tv2, tv6)
+	isGx1Square, y1 := m.sqrtRatio(tv2, tv6)
+	// 19.   y = tv1 * u
+	y := g2.Ext2.Mul(tv1, &u)
+	// 20.   y = y * y1
+	y = g2.Ext2.Mul(y, y1)
+	// 21.   x = CMOV(x, tv3, is_gx1_square)
+	x = g2.Ext2.Select(isGx1Square, tv3, x)
+	// 22.   y = CMOV(y, y1, is_gx1_square)
+	y = g2.Ext2.Select(isGx1Square, y1, y)
+	// 23.  e1 = sgn0(u) == sgn0(y)
+	sgn0U := g2.sgn0(&u)
+	sgn0Y := g2.sgn0(y)
+	diff := g2.api.Sub(sgn0U, sgn0Y)
+	e1 := g2.api.IsZero(diff)
+	// 24.   y = CMOV(-y, y, e1)
+	yNeg := g2.Ext2.Neg(y)
+	y = g2.Ext2.Select(e1, y, yNeg)
+	// 25.   x = x / tv4
+	x = g2.Ext2.DivUnchecked(x, tv4)
+	// 26. return (x, y)
+	return &G2Affine{
+		P: g2AffP{X: *x, Y: *y},
+	}
 }
