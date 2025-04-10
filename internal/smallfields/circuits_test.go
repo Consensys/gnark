4f18e5342818ce7d3af873e7fe4e11f37da8758f
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-04-10 09:38:56 +0000

    test: add tests for small field compile and solve

--- internal/smallfields/circuits_test.go
+++ internal/smallfields/circuits_test.go
@@ -1,13 +1,17 @@
 package smallfields_test
 
 import (
+	"fmt"
+	"math/big"
 	"testing"
 
 	"github.com/consensys/gnark-crypto/field/babybear"
 	"github.com/consensys/gnark-crypto/field/goldilocks"
 	"github.com/consensys/gnark-crypto/field/koalabear"
 	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/gnark/std/math/emulated"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"github.com/consensys/gnark/frontend/cs/scs"
+	"github.com/consensys/gnark/internal/smallfields/tinyfield"
 	"github.com/consensys/gnark/test"
 )
 
@@ -22,30 +26,53 @@ func (circuit *NativeCircuit) Define(api frontend.API) error {
 	return nil
 }
 
-type EmulatedCircuit[T emulated.FieldParams] struct {
-	A emulated.Element[T] `gnark:",public"`
-	B emulated.Element[T] `gnark:",secret"`
+var testCases = []struct {
+	name            string
+	modulus         *big.Int
+	supportsCompile bool
+}{
+	{"goldilocks", goldilocks.Modulus(), false},
+	{"tinyfield", tinyfield.Modulus(), false},
+	{"babybear", babybear.Modulus(), true},
+	{"koalabear", koalabear.Modulus(), true},
 }
 
-func (circuit *EmulatedCircuit[T]) Define(api frontend.API) error {
-	f, err := emulated.NewField[T](api)
-	if err != nil {
-		return err
-	}
-	res := f.Mul(&circuit.A, &circuit.A)
-	f.AssertIsEqual(res, &circuit.B)
-	return nil
-}
-
-func TestNativeCircuit(t *testing.T) {
+func TestNativeCircuitTestSolve(t *testing.T) {
 	assert := test.NewAssert(t)
+	for _, tc := range testCases {
+		assert.Run(func(assert *test.Assert) {
+			err := test.IsSolved(&NativeCircuit{}, &NativeCircuit{A: 2, B: 4}, tc.modulus)
+			assert.NoError(err)
+		}, tc.name)
+	}
+}
 
-	err := test.IsSolved(&NativeCircuit{}, &NativeCircuit{A: 2, B: 4}, goldilocks.Modulus())
-	assert.NoError(err)
+func TestNativeCircuitCompileAndSolve(t *testing.T) {
+	assert := test.NewAssert(t)
+	for _, tc := range testCases {
+		if !tc.supportsCompile {
+			continue
+		}
+		assert.Run(func(assert *test.Assert) {
+			ccs, err := frontend.CompileU32(tc.modulus, r1cs.NewBuilder, &NativeCircuit{})
+			assert.NoError(err)
+			assignment := &NativeCircuit{A: 2, B: 4}
+			wit, err := frontend.NewWitness(assignment, tc.modulus)
+			assert.NoError(err)
+			solution, err := ccs.Solve(wit)
+			assert.NoError(err)
+			_ = solution
 
-	err = test.IsSolved(&NativeCircuit{}, &NativeCircuit{A: 2, B: 4}, babybear.Modulus())
-	assert.NoError(err)
-
-	err = test.IsSolved(&NativeCircuit{}, &NativeCircuit{A: 2, B: 4}, koalabear.Modulus())
-	assert.NoError(err)
+		}, fmt.Sprintf("ccs=r1cs/field=%s", tc.name))
+		assert.Run(func(assert *test.Assert) {
+			ccs, err := frontend.CompileU32(tc.modulus, scs.NewBuilder, &NativeCircuit{})
+			assert.NoError(err)
+			assignment := &NativeCircuit{A: 2, B: 4}
+			wit, err := frontend.NewWitness(assignment, tc.modulus)
+			assert.NoError(err)
+			solution, err := ccs.Solve(wit)
+			assert.NoError(err)
+			_ = solution
+		}, fmt.Sprintf("ccs=scs/field=%s", tc.name))
+	}
 }
