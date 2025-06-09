1894cfafae5c97778c8eef7eb89637f0a8221049
Tabaie <arya.pourtabatabaie@gmail.com>
2025-06-09 17:49:28 -0500

    feat: gkrmimc for sbox degree 5

---
+++ std/permutation/gkr-mimc/gkr-mimc.go
@@ -0,0 +1,143 @@
+package gkr_mimc
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	bls12377 "github.com/consensys/gnark-crypto/ecc/bls12-377/fr/mimc"
+	bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381/fr/mimc"
+	bls24315 "github.com/consensys/gnark-crypto/ecc/bls24-315/fr/mimc"
+	bls24317 "github.com/consensys/gnark-crypto/ecc/bls24-317/fr/mimc"
+	bn254 "github.com/consensys/gnark-crypto/ecc/bn254/fr/mimc"
+	bw6633 "github.com/consensys/gnark-crypto/ecc/bw6-633/fr/mimc"
+	bw6761 "github.com/consensys/gnark-crypto/ecc/bw6-761/fr/mimc"
+	"github.com/consensys/gnark/constraint/solver/gkrgates"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/internal/utils"
+	"github.com/consensys/gnark/std/gkrapi"
+	"github.com/consensys/gnark/std/gkrapi/gkr"
+)
+
+// mimcCompressor implements a compression function by applying
+// the Miyaguchi–Preneel transformation to the MiMC encryption function.
+type mimcCompressor struct {
+	gkrCircuit    *gkrapi.Circuit
+	in0, in1, out gkr.Variable
+}
+
+func newGkrCompressor(api frontend.API) (*mimcCompressor, error) {
+	gkrApi := gkrapi.New()
+
+	in0 := gkrApi.NewInput()
+	in1 := gkrApi.NewInput()
+
+	y := in1
+
+	curve := utils.FieldToCurve(api.Compiler().Field())
+	params, _, err := getParams(curve) // params is only used for its length
+	if err != nil {
+		return nil, err
+	}
+	if err = RegisterGates(curve); err != nil {
+		return nil, err
+	}
+	gateNamer := newGateNamer(curve)
+
+	for i := range len(params) - 1 {
+		y = gkrApi.NamedGate(gateNamer.round(i), in0, y)
+	}
+
+	y = gkrApi.NamedGate(gateNamer.round(len(params)-1), in0, y, in1)
+
+	return &mimcCompressor{
+		gkrCircuit: gkrApi.Compile(api, "poseidon2"),
+		in0:        in0,
+		in1:        in1,
+		out:        y,
+	}, nil
+}
+
+func RegisterGates(curves ...ecc.ID) error {
+	for _, curve := range curves {
+		constants, deg, err := getParams(curve)
+		if err != nil {
+			return err
+		}
+		gateNamer := newGateNamer(curve)
+		var lastLayerSBox, nonLastLayerSBox func(*big.Int) gkr.GateFunction
+		switch deg {
+		case 5:
+			lastLayerSBox = addPow5Add
+			nonLastLayerSBox = addPow5
+		default:
+			return fmt.Errorf("s-Box of degree %d not supported", deg)
+		}
+
+		for i := range len(constants) - 1 {
+			if _, err = gkrgates.Register(nonLastLayerSBox(&constants[i]), 2, gkrgates.WithName(gateNamer.round(i)), gkrgates.WithUnverifiedDegree(deg), gkrgates.WithCurves(curve)); err != nil {
+				return fmt.Errorf("failed to register keyed GKR gate for round %d of MiMC on curve %s: %w", i, curve, err)
+			}
+		}
+
+		if _, err = gkrgates.Register(lastLayerSBox(&constants[len(constants)-1]), 3, gkrgates.WithName(gateNamer.round(len(constants)-1)), gkrgates.WithUnverifiedDegree(deg), gkrgates.WithCurves(curve)); err != nil {
+			return fmt.Errorf("failed to register keyed GKR gate for round %d of MiMC on curve %s: %w", len(constants)-1, curve, err)
+		}
+	}
+	return nil
+}
+
+// getParams returns the parameters for the MiMC encryption function for the given curve.
+// It also returns the degree of the s-Box
+func getParams(curve ecc.ID) ([]big.Int, int, error) {
+	switch curve {
+	case ecc.BN254:
+		return bn254.GetConstants(), 5, nil
+	case ecc.BLS12_381:
+		return bls12381.GetConstants(), 5, nil
+	case ecc.BLS12_377:
+		return bls12377.GetConstants(), 17, nil
+	case ecc.BLS24_315:
+		return bls24315.GetConstants(), 5, nil
+	case ecc.BLS24_317:
+		return bls24317.GetConstants(), 7, nil
+	case ecc.BW6_633:
+		return bw6633.GetConstants(), 5, nil
+	case ecc.BW6_761:
+		return bw6761.GetConstants(), 5, nil
+	default:
+		return nil, -1, fmt.Errorf("unsupported curve ID: %s", curve)
+	}
+}
+
+type gateNamer string
+
+func newGateNamer(o fmt.Stringer) gateNamer {
+	return gateNamer("MiMC-" + o.String() + "-round-")
+}
+func (n gateNamer) round(i int) gkr.GateName {
+	return gkr.GateName(fmt.Sprintf("%s%d", string(n), i))
+}
+
+func addPow5(key *big.Int) gkr.GateFunction {
+	return func(api gkr.GateAPI, in ...frontend.Variable) frontend.Variable {
+		if len(in) != 2 {
+			panic("expected two input")
+		}
+		s := api.Add(in[0], in[1], key)
+		t := api.Mul(s, s)
+		return api.Mul(t, t, s)
+	}
+}
+
+// addPow5Add: (in[0]+in[1]+key)⁵ + in[0] + in[2]
+func addPow5Add(key *big.Int) gkr.GateFunction {
+	return func(api gkr.GateAPI, in ...frontend.Variable) frontend.Variable {
+		if len(in) != 3 {
+			panic("expected three input")
+		}
+		s := api.Add(in[0], in[1], key)
+		t := api.Mul(s, s)
+		return api.Add(api.Mul(t, t, s), in[0], in[2])
+	}
+}
