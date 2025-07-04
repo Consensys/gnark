f38a6ec578682c2fd38d9815964272f3c7273db3
Red <45277734+ltp456@users.noreply.github.com>
2025-07-04 17:59:20 +0800

    ECDSA Signature: Add New Method IsVerified (#1454)
    Co-authored-by: Weiji Guo <weiji.g@gmail.com>
    Co-authored-by: Ivo Kubjas <ivo.kubjas@consensys.net>

--- std/signature/ecdsa/ecdsa_test.go
+++ std/signature/ecdsa/ecdsa_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark-crypto/ecc/secp256k1/ecdsa"
+	fr_secp256k1 "github.com/consensys/gnark-crypto/ecc/secp256k1/fr"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/std/algebra/emulated/sw_emulated"
 	"github.com/consensys/gnark/std/math/emulated"
@@ -157,4 +158,138 @@ func ExamplePublicKey_Verify_create() {
 
 	// can continue in the PublicKey Verify example
 	_, _, _, _, _ = sig.R, sig.S, msg, pubx, puby
+}
+
+type EcdsaIsValidCircuit[T, S emulated.FieldParams] struct {
+	Sig     Signature[S]
+	Msg     emulated.Element[S]
+	Pub     PublicKey[T, S]
+	IsValid frontend.Variable `gnark:",public"`
+}
+
+func (c *EcdsaIsValidCircuit[T, S]) Define(api frontend.API) error {
+	verified := c.Pub.IsValid(api, sw_emulated.GetCurveParams[T](), &c.Msg, &c.Sig)
+	api.AssertIsEqual(verified, c.IsValid)
+	return nil
+}
+
+func TestEcdsaPublicKeyIsValid(t *testing.T) {
+	assert := test.NewAssert(t)
+	// generate parameters
+	privKey, err := ecdsa.GenerateKey(rand.Reader)
+	assert.NoError(err, "failed to generate ecdsa key")
+	publicKey := privKey.PublicKey
+
+	// sign
+	msg := []byte("testing ECDSA (pre-hashed)")
+	sigBin, err := privKey.Sign(msg, nil)
+	assert.NoError(err, "failed to sign message")
+
+	// check that the signature is correct
+	flag, err := publicKey.Verify(sigBin, msg, nil)
+	assert.NoError(err, "failed to verify signature")
+	assert.True(flag, "can't verify signature")
+
+	// unmarshal signature
+	var sig ecdsa.Signature
+	sig.SetBytes(sigBin)
+	r, s := new(big.Int), new(big.Int)
+	r.SetBytes(sig.R[:32])
+	s.SetBytes(sig.S[:32])
+
+	hash := ecdsa.HashToInt(msg)
+
+	circuit := EcdsaIsValidCircuit[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{}
+
+	assert.Run(func(assert *test.Assert) {
+		witness := EcdsaIsValidCircuit[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{
+			Sig: Signature[emulated.Secp256k1Fr]{
+				R: emulated.ValueOf[emulated.Secp256k1Fr](r),
+				S: emulated.ValueOf[emulated.Secp256k1Fr](s),
+			},
+			Msg: emulated.ValueOf[emulated.Secp256k1Fr](hash),
+			Pub: PublicKey[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{
+				X: emulated.ValueOf[emulated.Secp256k1Fp](privKey.PublicKey.A.X),
+				Y: emulated.ValueOf[emulated.Secp256k1Fp](privKey.PublicKey.A.Y),
+			},
+			IsValid: 1,
+		}
+		err = test.IsSolved(&circuit, &witness, ecc.BN254.ScalarField())
+		assert.NoError(err, "solving failed")
+	}, "case=valid")
+
+	assert.Run(func(assert *test.Assert) {
+		r, err := rand.Int(rand.Reader, fr_secp256k1.Modulus())
+		assert.NoError(err, "failed to generate random r")
+		witness := EcdsaIsValidCircuit[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{
+			Sig: Signature[emulated.Secp256k1Fr]{
+				R: emulated.ValueOf[emulated.Secp256k1Fr](r),
+				S: emulated.ValueOf[emulated.Secp256k1Fr](s),
+			},
+			Msg: emulated.ValueOf[emulated.Secp256k1Fr](hash),
+			Pub: PublicKey[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{
+				X: emulated.ValueOf[emulated.Secp256k1Fp](privKey.PublicKey.A.X),
+				Y: emulated.ValueOf[emulated.Secp256k1Fp](privKey.PublicKey.A.Y),
+			},
+			IsValid: 0,
+		}
+		err = test.IsSolved(&circuit, &witness, ecc.BN254.ScalarField())
+		assert.NoError(err, "solving failed")
+	}, "case=invalid-r")
+
+	assert.Run(func(assert *test.Assert) {
+		s, err := rand.Int(rand.Reader, fr_secp256k1.Modulus())
+		assert.NoError(err, "failed to generate random r")
+		witness := EcdsaIsValidCircuit[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{
+			Sig: Signature[emulated.Secp256k1Fr]{
+				R: emulated.ValueOf[emulated.Secp256k1Fr](r),
+				S: emulated.ValueOf[emulated.Secp256k1Fr](s),
+			},
+			Msg: emulated.ValueOf[emulated.Secp256k1Fr](hash),
+			Pub: PublicKey[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{
+				X: emulated.ValueOf[emulated.Secp256k1Fp](privKey.PublicKey.A.X),
+				Y: emulated.ValueOf[emulated.Secp256k1Fp](privKey.PublicKey.A.Y),
+			},
+			IsValid: 0,
+		}
+		err = test.IsSolved(&circuit, &witness, ecc.BN254.ScalarField())
+		assert.NoError(err, "solving failed")
+	}, "case=invalid-s")
+
+	assert.Run(func(assert *test.Assert) {
+		hash := ecdsa.HashToInt([]byte("invalid message"))
+		witness := EcdsaIsValidCircuit[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{
+			Sig: Signature[emulated.Secp256k1Fr]{
+				R: emulated.ValueOf[emulated.Secp256k1Fr](r),
+				S: emulated.ValueOf[emulated.Secp256k1Fr](s),
+			},
+			Msg: emulated.ValueOf[emulated.Secp256k1Fr](hash),
+			Pub: PublicKey[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{
+				X: emulated.ValueOf[emulated.Secp256k1Fp](privKey.PublicKey.A.X),
+				Y: emulated.ValueOf[emulated.Secp256k1Fp](privKey.PublicKey.A.Y),
+			},
+			IsValid: 0,
+		}
+		err = test.IsSolved(&circuit, &witness, ecc.BN254.ScalarField())
+		assert.NoError(err, "solving failed")
+	}, "case=invalid-msg")
+
+	assert.Run(func(assert *test.Assert) {
+		privKey, err := ecdsa.GenerateKey(rand.Reader)
+		assert.NoError(err, "failed to generate ecdsa key")
+		witness := EcdsaIsValidCircuit[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{
+			Sig: Signature[emulated.Secp256k1Fr]{
+				R: emulated.ValueOf[emulated.Secp256k1Fr](r),
+				S: emulated.ValueOf[emulated.Secp256k1Fr](s),
+			},
+			Msg: emulated.ValueOf[emulated.Secp256k1Fr](hash),
+			Pub: PublicKey[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{
+				X: emulated.ValueOf[emulated.Secp256k1Fp](privKey.PublicKey.A.X),
+				Y: emulated.ValueOf[emulated.Secp256k1Fp](privKey.PublicKey.A.Y),
+			},
+			IsValid: 0,
+		}
+		err = test.IsSolved(&circuit, &witness, ecc.BN254.ScalarField())
+		assert.NoError(err, "solving failed")
+	}, "case=invalid-pub")
 }
