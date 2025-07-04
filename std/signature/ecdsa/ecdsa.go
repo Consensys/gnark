51a11e4ac9671125eea163b3a731c70f201b04f3
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-07-04 08:41:01 +0000

    chore: docs and method names

--- std/signature/ecdsa/ecdsa.go
+++ std/signature/ecdsa/ecdsa.go
@@ -19,18 +19,22 @@ type PublicKey[Base, Scalar emulated.FieldParams] sw_emulated.AffinePoint[Base]
 //
 // We assume that the message msg is already hashed to the scalar field.
 func (pk PublicKey[T, S]) Verify(api frontend.API, params sw_emulated.CurveParams, msg *emulated.Element[S], sig *Signature[S]) {
-	qxBits, rbits := pk.checkParams(api, params, msg, sig)
+	qxBits, rbits := pk.prepareVerification(api, params, msg, sig)
 	for i := range rbits {
 		api.AssertIsEqual(rbits[i], qxBits[i])
 	}
 }
 
-// IsVerified returns whether the signature sig verifies for the message msg and public
-// key pk. The curve parameters params define the elliptic curve.
+// IsValid returns a boolean indicating if the signature sig is valid for public
+// key pk and the message (return value 1 for valid signature and 0 for invalid
+// signature). It is equivalent to [PublicKey.Verify] but allows for more
+// flexible usage in circuits which could allow processing invalid signatures.
+//
+// The curve parameters params define the elliptic curve.
+//
 // We assume that the message msg is already hashed to the scalar field.
-// If the signature is valid, it returns 1; otherwise, it returns  0
-func (pk PublicKey[T, S]) IsVerified(api frontend.API, params sw_emulated.CurveParams, msg *emulated.Element[S], sig *Signature[S]) frontend.Variable {
-	qxBits, rbits := pk.checkParams(api, params, msg, sig)
+func (pk PublicKey[T, S]) IsValid(api frontend.API, params sw_emulated.CurveParams, msg *emulated.Element[S], sig *Signature[S]) frontend.Variable {
+	qxBits, rbits := pk.prepareVerification(api, params, msg, sig)
 	verified := frontend.Variable(1)
 	for i := range rbits {
 		res := api.IsZero(api.Sub(rbits[i], qxBits[i]))
@@ -40,7 +44,9 @@ func (pk PublicKey[T, S]) IsVerified(api frontend.API, params sw_emulated.CurveP
 
 }
 
-func (pk PublicKey[T, S]) checkParams(api frontend.API, params sw_emulated.CurveParams, msg *emulated.Element[S], sig *Signature[S]) ([]frontend.Variable, []frontend.Variable) {
+// prepareVerification computes Q = [r/s]PK + [m/s]G and returns the bits of Q.x
+// and r. The verifier should check that the bits are equal.
+func (pk PublicKey[T, S]) prepareVerification(api frontend.API, params sw_emulated.CurveParams, msg *emulated.Element[S], sig *Signature[S]) ([]frontend.Variable, []frontend.Variable) {
 	cr, err := sw_emulated.New[T, S](api, params)
 	if err != nil {
 		panic(err)
