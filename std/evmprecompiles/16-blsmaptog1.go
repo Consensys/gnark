8ec0dca6e894861409b086bc0db9f5013b54c6f0
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-07-19 12:09:01 +0300

    refactor: BLS EVM precompile integration (#1515)

--- std/evmprecompiles/16-blsmaptog1.go
+++ std/evmprecompiles/16-blsmaptog1.go
@@ -1,6 +1,8 @@
 package evmprecompiles
 
 import (
+	"fmt"
+
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/std/algebra/emulated/sw_bls12381"
 	"github.com/consensys/gnark/std/algebra/emulated/sw_emulated"
@@ -10,16 +12,16 @@ import (
 // ECMapToG1BLS implements [BLS12_MAP_FP_TO_G1] precompile contract at address 0x10.
 //
 // [ECMapToG1BLS]: https://eips.ethereum.org/EIPS/eip-2537
-func ECMapToG1BLS(api frontend.API, u *emulated.Element[emulated.BLS12381Fp]) *sw_emulated.AffinePoint[emulated.BLS12381Fp] {
+func ECMapToG1BLS(api frontend.API, P *emulated.Element[emulated.BLS12381Fp], expected *sw_emulated.AffinePoint[emulated.BLS12381Fp]) error {
 	g, err := sw_bls12381.NewG1(api)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("new G1: %w", err)
 	}
-	res, err := g.MapToG1(u)
+	res, err := g.MapToG1(P)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("map to G1: %w", err)
 	}
+	g.AssertIsEqual(res, expected)
 
-	return res
-
+	return nil
 }
