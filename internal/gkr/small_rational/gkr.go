c6b830c3fdf9137884b4015b4bdff72e3658884a
Tabaie <arya.pourtabatabaie@gmail.com>
2025-09-22 14:14:37 -0500

    build: generify exp changes

--- internal/gkr/small_rational/gkr.go
+++ internal/gkr/small_rational/gkr.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"fmt"
 	"math/big"
+	"math/bits"
 	"strconv"
 	"sync"
 
@@ -795,19 +796,18 @@ func (api *gateAPI) Println(a ...frontend.Variable) {
 }
 
 func (api *gateAPI) Exp(i frontend.Variable, e uint8) frontend.Variable {
-	var res *small_rational.SmallRational
-	x := api.cast(i)
-
-	if e%2 == 0 {
-		res = api.cast(1)
-	} else {
-		*res = *x
+	if e == 0 {
+		return 1
 	}
+	n := bits.Len8(e) - 1
+	e = bits.Reverse8(e) >> (8 - n)
 
-	e /= 2
+	res := api.newElement()
+	x := api.cast(i)
+	*res = *x
 
 	// square and multiply
-	for e != 0 {
+	for n != 0 {
 		res.Mul(res, res)
 
 		if e%2 != 0 {
@@ -815,6 +815,7 @@ func (api *gateAPI) Exp(i frontend.Variable, e uint8) frontend.Variable {
 		}
 
 		e /= 2
+		n--
 	}
 
 	return res
