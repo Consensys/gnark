c9dd1c3ae68f18e2da229088833fa624a98518cc
Tabaie <arya.pourtabatabaie@gmail.com>
2025-10-27 14:52:36 -0500

    style: run goimports

--- std/hash/hash.go
+++ std/hash/hash.go
@@ -115,9 +115,9 @@ type merkleDamgardHasher struct {
 
 // NewMerkleDamgardHasher range-extends a 2-1 one-way hash compression function into a hash by way of the Merkle-Damgård construction.
 // Parameters:
-//  - api: constraint builder
-//  - f: 2-1 hash compression (one-way) function
-//  - initialState: the initialization vector (IV) in the Merkle-Damgård chain. It must be a value whose preimage is not known.
+//   - api: constraint builder
+//   - f: 2-1 hash compression (one-way) function
+//   - initialState: the initialization vector (IV) in the Merkle-Damgård chain. It must be a value whose preimage is not known.
 func NewMerkleDamgardHasher(api frontend.API, f Compressor, initialState frontend.Variable) FieldHasher {
 	return &merkleDamgardHasher{
 		state: initialState,
@@ -143,12 +143,12 @@ func (h *merkleDamgardHasher) Sum() frontend.Variable {
 
 // SumMerkleDamgardDynamicLength computes the Merkle-Damgård hash of the input data, truncated at the given length.
 // Parameters:
-// 	- api: constraint builder
-//  - f: 2-1 hash compression (one-way) function
-//  - initialState: the initialization vector (IV) in the Merkle-Damgård chain. It must be a value whose preimage is not known.
-//  - length: length of the prefix of data to be hashed. The verifier will not accept a value outside the range {0, 1, ..., len(data)}.
-//            The gnark prover will refuse to attempt to generate such an unsuccessful proof.
-//  - data: the values a prefix of which is to be hashed.
+//   - api: constraint builder
+//   - f: 2-1 hash compression (one-way) function
+//   - initialState: the initialization vector (IV) in the Merkle-Damgård chain. It must be a value whose preimage is not known.
+//   - length: length of the prefix of data to be hashed. The verifier will not accept a value outside the range {0, 1, ..., len(data)}.
+//     The gnark prover will refuse to attempt to generate such an unsuccessful proof.
+//   - data: the values a prefix of which is to be hashed.
 func SumMerkleDamgardDynamicLength(api frontend.API, f Compressor, initialState frontend.Variable, length frontend.Variable, data []frontend.Variable) frontend.Variable {
 	resT := logderivlookup.New(api)
 	state := initialState
