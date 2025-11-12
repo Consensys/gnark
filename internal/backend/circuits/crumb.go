124b693963732edbbb7295ff51313ffec0f51d5b
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-11-12 11:36:01 +0100

    test: refactor crumb test to standard path (#1648)

---
+++ internal/backend/circuits/crumb.go
@@ -0,0 +1,32 @@
+package circuits
+
+import (
+	"math/rand/v2"
+
+	"github.com/consensys/gnark/frontend"
+)
+
+type isCrumbCircuit struct {
+	C []frontend.Variable
+}
+
+func (circuit *isCrumbCircuit) Define(api frontend.API) error {
+	for _, x := range circuit.C {
+		api.AssertIsCrumb(x)
+	}
+	return nil
+}
+
+func init() {
+	c := []frontend.Variable{0, 1, 2, 3}
+	good := []frontend.Circuit{
+		&isCrumbCircuit{C: c},
+	}
+	addNewEntry("isCrumb/case=good", &isCrumbCircuit{C: make([]frontend.Variable, len(c))}, good, nil, nil)
+	var bad []frontend.Circuit
+	for n := 0; n < 20; n++ {
+		x := rand.IntN(65531) + 4 //#nosec G404 weak rng OK for test
+		bad = append(bad, &isCrumbCircuit{C: []frontend.Variable{x}})
+	}
+	addNewEntry("isCrumb/case=bad", &isCrumbCircuit{C: []frontend.Variable{nil}}, nil, bad, nil)
+}
