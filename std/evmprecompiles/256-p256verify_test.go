3bc281942599ac48d3200d015e3b4dcbd75f1d2f
Youssef El Housni <youssef.housni21@gmail.com>
2025-11-14 11:17:23 -0500

    test: add test vectors

--- std/evmprecompiles/256-p256verify_test.go
+++ std/evmprecompiles/256-p256verify_test.go
@@ -1,7 +1,12 @@
 package evmprecompiles
 
 import (
+	"bytes"
 	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"math/big"
 	"testing"
 
@@ -68,4 +73,73 @@ func TestP256VerifyCircuit(t *testing.T) {
 	}
 	err = test.IsSolved(&circuit, &witness, ecc.BN254.ScalarField())
 	assert.NoError(err)
+}
+
+func TestP256VerifyCircuitWithEIPVectors(t *testing.T) {
+	assert := test.NewAssert(t)
+	data, err := ioutil.ReadFile("test_vectors/p256verify_vectors_clean.json")
+	if err != nil {
+		t.Fatalf("read vectors.json: %v", err)
+	}
+
+	var vecs []vector
+	if err := json.Unmarshal(data, &vecs); err != nil {
+		t.Fatalf("unmarshal: %v", err)
+	}
+	for i, v := range vecs {
+		h, r, s, qx, qy := splitInput160(v.Input)
+		verified := expectedBool(v.Expected)
+		expected := frontend.Variable(0)
+		if verified {
+			expected = 1
+		}
+		witness := p256verifyCircuit{
+			MsgHash:  emulated.ValueOf[emulated.P256Fr](*h),
+			R:        emulated.ValueOf[emulated.P256Fr](*r),
+			S:        emulated.ValueOf[emulated.P256Fr](*s),
+			Qx:       emulated.ValueOf[emulated.P256Fp](*qx),
+			Qy:       emulated.ValueOf[emulated.P256Fp](*qy),
+			Expected: expected,
+		}
+
+		circuit := p256verifyCircuit{}
+
+		t.Run(fmt.Sprintf("vector_%03d_%s", i, v.Name), func(t *testing.T) {
+			err := test.IsSolved(&circuit, &witness, ecc.BN254.ScalarField())
+			assert.NoError(err)
+		})
+	}
+}
+
+// --- utils
+type vector struct {
+	Name     string `json:"Name,omitempty"`
+	Input    string `json:"Input"`
+	Expected string `json:"Expected"`
+}
+
+func splitInput160(hexInput string) (h, r, s, qx, qy *big.Int) {
+	raw, err := hex.DecodeString(hexInput)
+	if err != nil {
+		panic(err)
+	}
+	if len(raw) != 160 {
+		return nil, nil, nil, nil, nil
+	}
+	h = new(big.Int).SetBytes(raw[0:32])
+	r = new(big.Int).SetBytes(raw[32:64])
+	s = new(big.Int).SetBytes(raw[64:96])
+	qx = new(big.Int).SetBytes(raw[96:128])
+	qy = new(big.Int).SetBytes(raw[128:160])
+	return
+}
+
+func expectedBool(s string) bool {
+	raw, err := hex.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	one := make([]byte, 32)
+	one[31] = 1
+	return bytes.Equal(raw, one)
 }
