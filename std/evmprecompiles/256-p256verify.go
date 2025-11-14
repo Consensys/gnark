847ba743ac7fc6b3f960c8bc9be2e36a1b00865e
Youssef El Housni <youssef.housni21@gmail.com>
2025-11-14 10:21:40 -0500

    docs: add comments P256Verify

--- std/evmprecompiles/256-p256verify.go
+++ std/evmprecompiles/256-p256verify.go
@@ -1,8 +1,6 @@
 package evmprecompiles
 
 import (
-	"fmt"
-
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/std/algebra/emulated/sw_emulated"
 	"github.com/consensys/gnark/std/math/emulated"
@@ -11,7 +9,8 @@ import (
 
 // P256Verify implements [P256Verify] precompile contract at address 0x100.
 //
-// ...
+// This circuit performs ECDSA signature verification over the secp256r1
+// elliptic curve (also known as P-256 or prime256v1).
 //
 // [P256Verify]: https://eips.ethereum.org/EIPS/eip-7951
 func P256Verify(api frontend.API,
@@ -19,22 +18,12 @@ func P256Verify(api frontend.API,
 	r, s emulated.Element[emulated.P256Fr],
 	qx, qy emulated.Element[emulated.P256Fp],
 ) frontend.Variable {
-	// Input validation
-	// 1. input_length == 160 ==> enforced by emulated.P256Fr, and emulated.P256Fp?
-	// 2. 0 < r < n and 0 < s < n ==> enforced by IsValid()
-	// 3. (qx, qy) is a valid point on the curve P256
-	curve, err := sw_emulated.New[emulated.P256Fp, emulated.P256Fr](api, sw_emulated.GetP256Params())
-	if err != nil {
-		panic(fmt.Sprintf("new curve: %v", err))
-	}
-	q := sw_emulated.AffinePoint[emulated.P256Fp]{
-		X: qx,
-		Y: qy,
-	}
-	curve.AssertIsOnCurve(&q) // todo: return bit
-	// 4. (qx, qy) != O
-	// impelemnt in sw_emulated
-
+	// Input validation:
+	// 1. input_length == 160 ==> checked by the arithmetization
+	// 2. 0 < r < n and 0 < s < n ==> checked by the arithmetization/ECDATA and enforced in `IsValid()`
+	// 3. 0 ≤ qx < p and 0 ≤ qy < p ==> checked by the arithmetization/ECDATA
+	// 4. (qx, qy) is a valid point on the curve P256 ==> checked by the arithmetization/ECDATA
+	// 5. (qx, qy) is not (0,0) ==> checked by the arithmetization/ECDATA
 	pk := ecdsa.PublicKey[emulated.P256Fp, emulated.P256Fr]{
 		X: qx,
 		Y: qy,
@@ -43,8 +32,6 @@ func P256Verify(api frontend.API,
 		R: r,
 		S: s,
 	}
-
 	verified := pk.IsValid(api, sw_emulated.GetCurveParams[emulated.P256Fp](), &msgHash, &sig)
-
 	return verified
 }
