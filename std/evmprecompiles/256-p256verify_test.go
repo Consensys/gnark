24237682959dabf72348925d52fed6780ffd69a9
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-11-16 03:10:02 +0000

    test: implement utility functions for mocking arithmetization

--- std/evmprecompiles/256-p256verify_test.go
+++ std/evmprecompiles/256-p256verify_test.go
@@ -2,16 +2,22 @@ package evmprecompiles
 
 import (
 	"bytes"
+	cryptoecdsa "crypto/ecdsa"
+	"crypto/elliptic"
 	"crypto/rand"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"math/big"
 	"os"
 	"testing"
 
 	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark-crypto/ecc/secp256r1"
 	"github.com/consensys/gnark-crypto/ecc/secp256r1/ecdsa"
+	fp_secp256r1 "github.com/consensys/gnark-crypto/ecc/secp256r1/fp"
+	fr_secp256r1 "github.com/consensys/gnark-crypto/ecc/secp256r1/fr"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/std/math/emulated"
 	"github.com/consensys/gnark/test"
@@ -26,7 +32,7 @@ type p256verifyCircuit struct {
 }
 
 func (c *p256verifyCircuit) Define(api frontend.API) error {
-	res := P256Verify(api, c.MsgHash, c.R, c.S, c.Qx, c.Qy)
+	res := P256Verify(api, &c.MsgHash, &c.R, &c.S, &c.Qx, &c.Qy)
 	api.AssertIsEqual(c.Expected, res)
 	return nil
 }
@@ -87,7 +93,10 @@ func TestP256VerifyCircuitWithEIPVectors(t *testing.T) {
 		t.Fatalf("unmarshal: %v", err)
 	}
 	for i, v := range vecs {
-		h, r, s, qx, qy := splitInput160(v.Input)
+		h, r, s, qx, qy, err := splitInput160(v.Input)
+		if err != nil {
+			t.Fatalf("splitInput160: %v", err)
+		}
 		verified := expectedBool(v.Expected)
 		expected := frontend.Variable(0)
 		if verified {
@@ -118,13 +127,83 @@ type vector struct {
 	Expected string `json:"Expected"`
 }
 
-func splitInput160(hexInput string) (h, r, s, qx, qy *big.Int) {
-	raw, err := hex.DecodeString(hexInput)
+// mockedArithmetization performs the checks what the arithmetization would
+// perform. It returns a boolean indicating if the signature is valid or not and
+// and an error if the test case would fail already at the arithmetization
+// level.
+func mockedArithmetization(testcase *vector) (isValid bool, h, r, s, qx, qy *big.Int, err error) {
+	// arithmetization checks:
+	// * Input length: Input MUST be exactly 160 bytes !!!!
+	// * Signature component bounds: Both r and s MUST satisfy 0 < r < n and 0 < s < n !!!
+	// * Public key bounds: Both qx and qy MUST satisfy 0 ≤ qx < p and 0 ≤ qy < p !!!
+	// * Point validity: The point (qx, qy) MUST satisfy the curve equation qy^2 ≡ qx^3 + a*qx + b (mod p) !!!
+	// * Point not at infinity: The point (qx, qy) MUST NOT be the point at infinity (represented as (0, 0)) !!!
+
+	// 1. first check the input length:
+	h, r, s, qx, qy, err = splitInput160(testcase.Input)
 	if err != nil {
-		panic(err)
+		err = fmt.Errorf("input length check failed: %w", err)
+		return
+	}
+	// 2. check signature component bounds
+	modFr := fr_secp256r1.Modulus()
+	if r.Cmp(big.NewInt(0)) != 1 || r.Cmp(modFr) != -1 {
+		err = errors.New("r out of bounds")
+		return
+	}
+	if s.Cmp(big.NewInt(0)) != 1 || s.Cmp(modFr) != -1 {
+		err = errors.New("s out of bounds")
+		return
+	}
+	// 3. check public key bounds
+	modFp := fp_secp256r1.Modulus()
+	if qx.Cmp(big.NewInt(0)) == -1 || qx.Cmp(modFp) != -1 {
+		err = errors.New("qx out of bounds")
+		return
+	}
+	if qy.Cmp(big.NewInt(0)) == -1 || qy.Cmp(modFp) != -1 {
+		err = errors.New("qy out of bounds")
+		return
+	}
+	// 4. check that point is on the curve
+	var P secp256r1.G1Affine
+	P.X.SetBigInt(qx)
+	P.Y.SetBigInt(qy)
+	if !P.IsOnCurve() {
+		err = errors.New("point not on curve")
+		return
+	}
+	// 5. check that point is not at infinity
+	if P.IsInfinity() {
+		err = errors.New("point at infinity")
+		return
+	}
+	// if we reached this point, all arithmetization checks passed. Now check
+	// signature validity
+	pk := cryptoecdsa.PublicKey{
+		Curve: elliptic.P256(),
+		X:     qx,
+		Y:     qy,
+	}
+	msgBytes := h.Bytes()
+	ok := cryptoecdsa.Verify(&pk, msgBytes, r, s)
+	if !ok {
+		return
+	}
+	isValid = true
+	return
+}
+
+func splitInput160(hexInput string) (h, r, s, qx, qy *big.Int, err error) {
+	var raw []byte
+	raw, err = hex.DecodeString(hexInput)
+	if err != nil {
+		// invalid hex encoding
+		return
 	}
 	if len(raw) != 160 {
-		return nil, nil, nil, nil, nil
+		// invalid length
+		return nil, nil, nil, nil, nil, errors.New("invalid input length")
 	}
 	h = new(big.Int).SetBytes(raw[0:32])
 	r = new(big.Int).SetBytes(raw[32:64])
