876dedffa2ce7ecbeda88f2b09c5fed5a500c680
Ivo Kubjas <ivo.kubjas@consensys.net>
2026-01-14 22:35:48 +0000

    chore: go generate

--- backend/accelerated/icicle/groth16/bw6-761/icicle.go
+++ backend/accelerated/icicle/groth16/bw6-761/icicle.go
@@ -778,6 +778,7 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, cfg *icic
 	if opt.HashToFieldFn == nil {
 		opt.HashToFieldFn = hash_to_field.New([]byte(constraint.CommitmentDst))
 	}
+	pk.PinToGPU = cfg.PinToGPU
 	log := logger.Logger().With().Str("curve", r1cs.CurveID().String()).Str("acceleration", "icicle").Int("nbConstraints", r1cs.GetNbConstraints()).Str("backend", "groth16").Logger()
 
 	device := icicle_runtime.CreateDevice(cfg.Backend.String(), cfg.DeviceID)
