e2ed62d395f22d1709bc7d535309a70c6acde891
Shramee Srivastav <shramee.srivastav@gmail.com>
2026-03-24 22:16:50 +0530

    feat: polynomial ring multiplication hint
    With serialisation and deserialisation for emulated element polynomials

--- std/math/emulated/field_polyring.go
+++ std/math/emulated/field_polyring.go
@@ -3,6 +3,9 @@ package emulated
 import (
 	"fmt"
 	"math/big"
+
+	"github.com/consensys/gnark/frontend"
+	limbs "github.com/consensys/gnark/std/internal/limbcomposition"
 )
 
 // Represents an element of a polynomial ring over the emulated field.
@@ -49,6 +52,166 @@ type polyRingMulCheck[T FieldParams] struct {
 	q      Poly[T]   // quotient
 }
 
+// CallPolyRingMulHint computes a polynomial product check in a polynomial ring,
+// returns the remainder (reduced result) and the quotient. The computation
+// is performed inside a hint, so it is the callers responsibility to perform
+// the deferred multiplication check.
+func (f *Field[T]) CallPolyRingMulHint(inputs []Poly[T], mod Poly[T]) (quo, rem Poly[T], err error) {
+	nbLimbs, nbBits := int(f.fParams.NbLimbs()), f.fParams.BitsPerLimb()
+
+	// total number of terms for all input polynomials
+	nbTerms := 0
+	// loop through inputs to compute the number of terms
+	for i := range inputs {
+		// add degree of each input polynomial
+		nbTerms += len(inputs[i])
+	}
+
+	// metadata for hint inputs
+	nbPoly := len(inputs)
+	nbTermsLimbs := nbTerms * nbLimbs
+	nbModTermsLimbs := len(mod) * nbLimbs
+
+	// metadata for outputs
+	modDegree := len(mod) - 1
+	nbRemLimbs := modDegree * nbLimbs
+	totalDegree := nbTerms - nbPoly
+	// q degree is total degree minus the degree of the modulus polynomial
+	qDegree := totalDegree - modDegree
+	nbQTermsLimbs := (1 + qDegree) * nbLimbs
+
+	// Polynomials serialised as nbTerms|...terms. hintInputs contains serialisation of,
+	// in order: nbBits|nbLimbs|nbPoly|fieldMod|...inputs|modPoly, where inputs and mod are serialised as polynomials
+	hintInputs := make([]frontend.Variable, 0, 4+(nbPoly+nbTermsLimbs)+(1+nbModTermsLimbs))
+
+	hintInputs = append(hintInputs, nbBits, nbLimbs, nbPoly, *f.fParams.Modulus())
+
+	// loop through inputs to compute the number of terms
+	for _, inputPoly := range inputs {
+		// serialised as nbTerms|...terms
+		// add degree of each input polynomial
+		hintInputs = append(hintInputs, len(inputPoly))
+		// append each term from inputPoly polynomial
+		for _, coeff := range inputPoly {
+			hintInputs = append(hintInputs, coeff.Limbs...)
+		}
+	}
+
+	hintInputs = append(hintInputs, len(mod))
+	// append mod terms
+	for _, coeff := range mod {
+		hintInputs = append(hintInputs, coeff.Limbs...)
+	}
+
+	// call
+	ret, err := f.api.NewHint(polyRingMulHint, 1+nbQTermsLimbs+1+nbRemLimbs, hintInputs...)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// unpack quotient: skip nbQTerms header, then read (1+qDegree) terms of nbLimbs each
+	quo = make(Poly[T], 1+qDegree)
+	retPtr := 1 // skip nbQTerms
+	for i := range quo {
+		termLimbs := ret[retPtr : retPtr+nbLimbs]
+		retPtr += nbLimbs
+		quo[i] = f.packLimbs(termLimbs, false)
+	}
+
+	// unpack remainder: skip nbRemTerms header, then read (len(mod)-1) terms of nbLimbs each
+	retPtr++ // skip nbRemTerms
+	rem = make(Poly[T], len(mod)-1)
+	for i := range rem {
+		termLimbs := ret[retPtr : retPtr+nbLimbs]
+		retPtr += nbLimbs
+		rem[i] = f.packLimbs(termLimbs, true)
+	}
+
+	return quo, rem, nil
+}
+
+// polyRingMulHint computes the multivariate evaluation as a hint. Should not be
+// called directly, but rather through [Field.callPolyRingMulHint] method which
+// handles the input packing and output unpacking.
+func polyRingMulHint(mod *big.Int, inputs, outputs []*big.Int) error {
+	nbBits := int(inputs[0].Int64())
+	nbLimbs := int(inputs[1].Int64())
+	nbPoly := int(inputs[2].Int64())
+
+	// extract the input polynomials
+	inputsPolys := make([][]*big.Int, nbPoly)
+	ptr := 3
+	fieldMod := new(big.Int).Set(inputs[ptr])
+	ptr++
+
+	for i := 0; i < nbPoly; i++ {
+		nbTerms := int(inputs[ptr].Int64())
+		ptr++
+		inputsPolys[i] = make([]*big.Int, nbTerms)
+		for j := 0; j < nbTerms; j++ {
+			coeffLimbs := inputs[ptr : ptr+nbLimbs]
+			ptr += nbLimbs
+			val := new(big.Int)
+			if err := limbs.Recompose(coeffLimbs, uint(nbBits), val); err != nil {
+				return fmt.Errorf("recompose input[%d][%d]: %w", i, j, err)
+			}
+			inputsPolys[i][j] = val
+		}
+	}
+
+	nbTerms := int(inputs[ptr].Int64())
+	ptr++
+	modPoly := make([]*big.Int, nbTerms)
+	for j := 0; j < nbTerms; j++ {
+		coeffLimbs := inputs[ptr : ptr+nbLimbs]
+		ptr += nbLimbs
+		val := new(big.Int)
+		if err := limbs.Recompose(coeffLimbs, uint(nbBits), val); err != nil {
+			return fmt.Errorf("recompose mod polynomial[%d]: %w", j, err)
+		}
+		modPoly[j] = val
+	}
+
+	// multiply inputs and divide by modPoly to obtain quotient and remainder
+	q, r, err := polyRingMul(fieldMod, inputsPolys, modPoly)
+	if err != nil {
+		return fmt.Errorf("polyRingMul: %w", err)
+	}
+
+	// serialize outputs: nbQTerms | q limbs... | nbRemTerms | r limbs...
+	nbBitsU := uint(nbBits)
+	outptr := 0
+	outputs[outptr].SetInt64(int64(len(q)))
+	outptr++
+	for _, coeff := range q {
+		coeffLimbs := make([]*big.Int, nbLimbs)
+		for i := range coeffLimbs {
+			coeffLimbs[i] = new(big.Int)
+		}
+		if err := limbs.Decompose(coeff, nbBitsU, coeffLimbs); err != nil {
+			return fmt.Errorf("decompose quotient coeff: %w", err)
+		}
+		copy(outputs[outptr:outptr+nbLimbs], coeffLimbs)
+		outptr += nbLimbs
+	}
+	outputs[outptr].SetInt64(int64(len(r)))
+	outptr++
+	for _, coeff := range r {
+		coeffLimbs := make([]*big.Int, nbLimbs)
+		for i := range coeffLimbs {
+			coeffLimbs[i] = new(big.Int)
+		}
+		if err := limbs.Decompose(coeff, nbBitsU, coeffLimbs); err != nil {
+			return fmt.Errorf("decompose remainder coeff: %w", err)
+		}
+		copy(outputs[outptr:outptr+nbLimbs], coeffLimbs)
+		outptr += nbLimbs
+	}
+
+	return nil
+}
+
 // polyRingMul multiplies all polynomials in inputs and divides the product
 // by modPoly using polynomial Euclidean division. Coefficients are reduced
 // modulo fieldMod (the emulated field prime).
@@ -59,9 +222,6 @@ func polyRingMul(fieldMod *big.Int, inputs [][]*big.Int, modPoly []*big.Int) (q,
 	if len(modPoly) == 0 {
 		return nil, nil, fmt.Errorf("polyRingMul: modulus polynomial is empty")
 	}
-
-	fmt.Printf("inputs: %v\n", inputs)
-	fmt.Printf("modPoly: %v\n", modPoly)
 
 	// multiply all polynomials in inputs
 	product := make([]*big.Int, len(inputs[0]))
@@ -144,10 +304,6 @@ func polyRingMul(fieldMod *big.Int, inputs [][]*big.Int, modPoly []*big.Int) (q,
 			remainder[i] = new(big.Int)
 		}
 	}
-
-	fmt.Printf("product: %v\n", product)
-	fmt.Printf("quotient: %v\n", quotient)
-	fmt.Printf("remainder: %v\n", remainder)
 
 	return quotient, remainder, nil
 }
